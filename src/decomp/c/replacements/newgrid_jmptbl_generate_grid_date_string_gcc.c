ddd8178220bc333dec183ff9a787abefef00ad22
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-02 05:01:46 -0500

    Decompilation checkpoint: 1772445706

---
+++ src/decomp/c/replacements/newgrid_jmptbl_generate_grid_date_string_gcc.c
@@ -0,0 +1,10 @@
+#include "esq_types.h"
+
+void GENERATE_GRID_DATE_STRING(void) __attribute__((noinline));
+
+void NEWGRID_JMPTBL_GENERATE_GRID_DATE_STRING(void) __attribute__((noinline, used));
+
+void NEWGRID_JMPTBL_GENERATE_GRID_DATE_STRING(void)
+{
+    GENERATE_GRID_DATE_STRING();
+}
