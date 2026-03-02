83469cc823c75cca392a35bc45a6f4b80d03c674
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-02 08:35:13 -0500

    Decompilation checkpoint: 1772458512

---
+++ src/decomp/c/replacements/parseini_jmptbl_unknown36_finalizerequest_gcc.c
@@ -0,0 +1,10 @@
+#include "esq_types.h"
+
+void UNKNOWN36_FinalizeRequest(void) __attribute__((noinline));
+
+void PARSEINI_JMPTBL_UNKNOWN36_FinalizeRequest(void) __attribute__((noinline, used));
+
+void PARSEINI_JMPTBL_UNKNOWN36_FinalizeRequest(void)
+{
+    UNKNOWN36_FinalizeRequest();
+}
