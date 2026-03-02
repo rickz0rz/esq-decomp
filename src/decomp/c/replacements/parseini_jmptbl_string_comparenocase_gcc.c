eaa99edab1736521c4b00c84cf063eff5d54adbf
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-02 08:13:44 -0500

    Decompilation checkpoint: 1772457224

---
+++ src/decomp/c/replacements/parseini_jmptbl_string_comparenocase_gcc.c
@@ -0,0 +1,10 @@
+#include "esq_types.h"
+
+void STRING_CompareNoCase(void) __attribute__((noinline));
+
+void PARSEINI_JMPTBL_STRING_CompareNoCase(void) __attribute__((noinline, used));
+
+void PARSEINI_JMPTBL_STRING_CompareNoCase(void)
+{
+    STRING_CompareNoCase();
+}
