038f939d7d71cb38fab61088d2cfb4e998da9f58
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-02 06:35:41 -0500

    Decompilation checkpoint: 1772451341

---
+++ src/decomp/c/replacements/parseini_jmptbl_esqfunc_rebuildpwbrushlistfromtagtablefromtagtable_gcc.c
@@ -0,0 +1,10 @@
+#include "esq_types.h"
+
+void ESQFUNC_RebuildPwBrushListFromTagTable(void) __attribute__((noinline));
+
+void PARSEINI_JMPTBL_ESQFUNC_RebuildPwBrushListFromTagTableFromTagTable(void) __attribute__((noinline, used));
+
+void PARSEINI_JMPTBL_ESQFUNC_RebuildPwBrushListFromTagTableFromTagTable(void)
+{
+    ESQFUNC_RebuildPwBrushListFromTagTable();
+}
