34005d0566a4a82be74f256ef401cb4d7f177878
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-05 16:19:30 -0500

    Decompilation checkpoint: 1772745570

---
+++ src/decomp/sas_c/esqfunc_process_ui_frame_tick.c
@@ -0,0 +1,101 @@
+typedef signed long LONG;
+typedef signed short WORD;
+typedef signed char BYTE;
+typedef unsigned short UWORD;
+
+extern WORD GCOMMAND_DriveProbeRequestedFlag;
+extern LONG ESQDISP_DisplayActiveFlag;
+extern WORD Global_UIBusyFlag;
+extern WORD CLEANUP_PendingAlertFlag;
+extern LONG ESQDISP_SecondaryPersistRequestFlag;
+extern WORD CTASKS_IffTaskDoneFlag;
+extern UWORD ESQFUNC_IffTaskGateFlags;
+extern LONG Global_REF_LONG_DF0_LOGO_LST_DATA;
+extern WORD ESQIFF_ExternalAssetFlags;
+extern BYTE ED_DiagGraphModeChar;
+extern LONG Global_REF_LONG_GFX_G_ADS_DATA;
+extern LONG WDISP_WeatherStatusBrushListHead;
+extern LONG PARSEINI_BannerBrushResourceHead;
+extern LONG ESQIFF_LogoBrushListCount;
+extern LONG ESQIFF_GAdsBrushListCount;
+extern BYTE ESQDISP_StatusRefreshPendingFlag;
+extern BYTE GCOMMAND_HighlightHoldoffTickCount;
+
+extern void ESQFUNC_JMPTBL_DISKIO_ProbeDrivesAndAssignPaths(void);
+extern void ESQDISP_PollInputModeAndRefreshSelection(void);
+extern void ESQDISP_ProcessGridMessagesIfIdle(void);
+extern void ED_DispatchEscMenuState(void);
+extern void ESQFUNC_JMPTBL_SCRIPT_HandleSerialCtrlCmd(void);
+extern void ESQFUNC_JMPTBL_CLEANUP_ProcessAlerts(void);
+extern void ESQFUNC_CommitSecondaryStateAndPersist(void);
+extern void ESQFUNC_JMPTBL_TEXTDISP_ResetSelectionAndRefresh(void);
+extern void ESQIFF_PlayNextExternalAssetFrame(WORD arg);
+extern void ESQIFF_QueueIffBrushLoad(WORD arg);
+extern void ESQIFF_ServiceExternalAssetSourceState(WORD arg);
+extern void ESQFUNC_JMPTBL_TEXTDISP_TickDisplayState(void);
+extern void ESQDISP_RefreshStatusIndicatorsFromCurrentMask(void);
+
+void ESQFUNC_ProcessUiFrameTick(void)
+{
+    if (GCOMMAND_DriveProbeRequestedFlag != 0) {
+        ESQFUNC_JMPTBL_DISKIO_ProbeDrivesAndAssignPaths();
+    }
+
+    if (ESQDISP_DisplayActiveFlag == 1) {
+        ESQDISP_PollInputModeAndRefreshSelection();
+    }
+
+    if (Global_UIBusyFlag == 0) {
+        ESQDISP_ProcessGridMessagesIfIdle();
+    }
+
+    ED_DispatchEscMenuState();
+
+    if (Global_UIBusyFlag == 0) {
+        ESQFUNC_JMPTBL_SCRIPT_HandleSerialCtrlCmd();
+    }
+
+    if (CLEANUP_PendingAlertFlag != 0) {
+        ESQFUNC_JMPTBL_CLEANUP_ProcessAlerts();
+
+        if (ESQDISP_SecondaryPersistRequestFlag != 0) {
+            ESQDISP_SecondaryPersistRequestFlag = 0;
+            ESQFUNC_CommitSecondaryStateAndPersist();
+        }
+
+        if (CTASKS_IffTaskDoneFlag != 0) {
+            if ((ESQFUNC_IffTaskGateFlags & 2) != 0 && Global_UIBusyFlag == 0) {
+                ESQFUNC_IffTaskGateFlags &= (UWORD)~2;
+                ESQFUNC_JMPTBL_TEXTDISP_ResetSelectionAndRefresh();
+            } else if ((ESQFUNC_IffTaskGateFlags & 1) != 0 && Global_UIBusyFlag == 0) {
+                ESQFUNC_IffTaskGateFlags &= (UWORD)~1;
+                ESQIFF_PlayNextExternalAssetFrame(1);
+            }
+
+            if (Global_REF_LONG_DF0_LOGO_LST_DATA == 0 && Global_UIBusyFlag == 0) {
+                ESQIFF_ExternalAssetFlags &= (UWORD)0xFFFD;
+            }
+
+            if (ED_DiagGraphModeChar != 'N' && Global_REF_LONG_GFX_G_ADS_DATA == 0 && Global_UIBusyFlag == 0) {
+                ESQIFF_ExternalAssetFlags &= (UWORD)0xFFFE;
+            }
+
+            if (WDISP_WeatherStatusBrushListHead == 0 && PARSEINI_BannerBrushResourceHead != 0) {
+                ESQIFF_QueueIffBrushLoad(0);
+            }
+
+            if (ESQIFF_LogoBrushListCount < 1) {
+                ESQIFF_ServiceExternalAssetSourceState(0);
+            } else if (ED_DiagGraphModeChar != 'N' && ESQIFF_GAdsBrushListCount < 2 && Global_UIBusyFlag == 0) {
+                ESQIFF_ServiceExternalAssetSourceState(1);
+            }
+        }
+    }
+
+    ESQFUNC_JMPTBL_TEXTDISP_TickDisplayState();
+
+    if (ESQDISP_StatusRefreshPendingFlag != 0 && GCOMMAND_HighlightHoldoffTickCount == 0) {
+        ESQDISP_StatusRefreshPendingFlag = 0;
+        ESQDISP_RefreshStatusIndicatorsFromCurrentMask();
+    }
+}
---
+++ src/decomp/scripts/semantic_filter_sasc_esqfunc_process_ui_frame_tick.awk
@@ -0,0 +1,63 @@
+BEGIN {
+    has_entry = 0
+    has_probe = 0
+    has_poll = 0
+    has_grid = 0
+    has_serial = 0
+    has_alerts = 0
+    has_commit = 0
+    has_reset_refresh = 0
+    has_next_frame = 0
+    has_queue_load = 0
+    has_service_asset = 0
+    has_tick_display = 0
+    has_refresh_status = 0
+    has_return = 0
+}
+
+function trim(s, t) {
+    t = s
+    sub(/;.*/, "", t)
+    sub(/^[ \t]+/, "", t)
+    sub(/[ \t]+$/, "", t)
+    return t
+}
+
+{
+    line = trim($0)
+    if (line == "") next
+    gsub(/[ \t]+/, " ", line)
+    uline = toupper(line)
+
+    if (uline ~ /^ESQFUNC_PROCESSUIFRAMETICK:/ || uline ~ /^ESQFUNC_PROCESSUIFRAMETIC[A-Z0-9_]*:/) has_entry = 1
+    if (uline ~ /ESQFUNC_JMPTBL_DISKIO_PROBEDRIVESANDASSIGNPATHS/ || uline ~ /ESQFUNC_JMPTBL_DISKIO_PROBEDRIVESAN/ || uline ~ /ESQFUNC_JMPTBL_DISKIO_PROBEDRIVE/) has_probe = 1
+    if (uline ~ /ESQDISP_POLLINPUTMODEANDREFRESHSELECTION/ || uline ~ /ESQDISP_POLLINPUTMODEANDREFRESHS/) has_poll = 1
+    if (uline ~ /ESQDISP_PROCESSGRIDMESSAGESIFIDLE/ || uline ~ /ESQDISP_PROCESSGRIDMESSAGESIFI/) has_grid = 1
+    if (uline ~ /ESQFUNC_JMPTBL_SCRIPT_HANDLESERIALCTRLCMD/ || uline ~ /ESQFUNC_JMPTBL_SCRIPT_HANDLESERIAL/ || uline ~ /ESQFUNC_JMPTBL_SCRIPT_HANDLESERI/) has_serial = 1
+    if (uline ~ /ESQFUNC_JMPTBL_CLEANUP_PROCESSALERTS/ || uline ~ /ESQFUNC_JMPTBL_CLEANUP_PROCESSALE/ || uline ~ /ESQFUNC_JMPTBL_CLEANUP_PROCESSAL/) has_alerts = 1
+    if (uline ~ /ESQFUNC_COMMITSECONDARYSTATEANDPERSIST/ || uline ~ /ESQFUNC_COMMITSECONDARYSTATEANDPE/ || uline ~ /ESQFUNC_COMMITSECONDARYSTATEANDP/) has_commit = 1
+    if (uline ~ /ESQFUNC_JMPTBL_TEXTDISP_RESETSELECTIONANDREFRESH/ || uline ~ /ESQFUNC_JMPTBL_TEXTDISP_RESETSEL/) has_reset_refresh = 1
+    if (uline ~ /ESQIFF_PLAYNEXTEXTERNALASSETFRAME/ || uline ~ /ESQIFF_PLAYNEXTEXTERNALASSETF/) has_next_frame = 1
+    if (uline ~ /ESQIFF_QUEUEIFFBRUSHLOAD/ || uline ~ /ESQIFF_QUEUEIFFBRUSHLOA/) has_queue_load = 1
+    if (uline ~ /ESQIFF_SERVICEEXTERNALASSETSOURCESTATE/ || uline ~ /ESQIFF_SERVICEEXTERNALASSETSO/) has_service_asset = 1
+    if (uline ~ /ESQFUNC_JMPTBL_TEXTDISP_TICKDISPLAYSTATE/ || uline ~ /ESQFUNC_JMPTBL_TEXTDISP_TICKDISPL/ || uline ~ /ESQFUNC_JMPTBL_TEXTDISP_TICKDISP/) has_tick_display = 1
+    if (uline ~ /ESQDISP_REFRESHSTATUSINDICATORSFROMCURRENTMASK/ || uline ~ /ESQDISP_REFRESHSTATUSINDICATORSFRO/ || uline ~ /ESQDISP_REFRESHSTATUSINDICATORSF/) has_refresh_status = 1
+    if (uline ~ /^RTS$/) has_return = 1
+}
+
+END {
+    print "HAS_ENTRY=" has_entry
+    print "HAS_PROBE=" has_probe
+    print "HAS_POLL=" has_poll
+    print "HAS_GRID=" has_grid
+    print "HAS_SERIAL=" has_serial
+    print "HAS_ALERTS=" has_alerts
+    print "HAS_COMMIT=" has_commit
+    print "HAS_RESET_REFRESH=" has_reset_refresh
+    print "HAS_NEXT_FRAME=" has_next_frame
+    print "HAS_QUEUE_LOAD=" has_queue_load
+    print "HAS_SERVICE_ASSET=" has_service_asset
+    print "HAS_TICK_DISPLAY=" has_tick_display
+    print "HAS_REFRESH_STATUS=" has_refresh_status
+    print "HAS_RETURN=" has_return
+}
