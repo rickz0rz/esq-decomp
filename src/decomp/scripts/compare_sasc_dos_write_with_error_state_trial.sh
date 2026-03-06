36d2da3d1bfb6dc68b9856c01db0fe0221133da3
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-06 18:09:00 -0500

    Decompilation checkpoint: 1772838540

--- src/decomp/scripts/compare_sasc_dos_write_with_error_state_trial.sh
+++ src/decomp/scripts/compare_sasc_dos_write_with_error_state_trial.sh
@@ -9,13 +9,27 @@ SASC_DIR="src/decomp/sas_c"
 SASC_DIS="${SASC_DIR}/${SASC_SRC}.dis"
 ORIG_ASM="src/modules/submodules/unknown17.s"
 OUT_DIR="build/decomp/sasc_trial"
+ENTRY_ORIG="DOS_WriteWithErrorState"
 
 mkdir -p "$OUT_DIR"
 
 ./sc-build-with-dis.sh "$SASC_SRC" >"${OUT_DIR}/sc_build_dos_write_with_error_state.log" 2>&1
 
-awk '$0 ~ /^DOS_WriteWithErrorState:$/ {in_func=1} in_func { if ($0 ~ /^;!======/) exit; print }' "$ORIG_ASM" >"${OUT_DIR}/dos_write_with_error_state.original.s"
-awk '$0 ~ /^DOS_WriteWithErrorState:$/ {in_func=1} in_func { if ($0 ~ /^__const:$/) exit; print }' "$SASC_DIS" >"${OUT_DIR}/dos_write_with_error_state.sasc.dis.s"
+awk -v e="^${ENTRY_ORIG}:$" '
+  $0 ~ e {in_func=1}
+  in_func {
+    if ($0 ~ /^;!======/) exit
+    print
+  }
+' "$ORIG_ASM" >"${OUT_DIR}/dos_write_with_error_state.original.s"
+
+awk -v e="^${ENTRY_ORIG}:$" '
+  $0 ~ e {in_func=1}
+  in_func {
+    if ($0 ~ /^__const:$/) exit
+    print
+  }
+' "$SASC_DIS" >"${OUT_DIR}/dos_write_with_error_state.sasc.dis.s"
 
 normalize() {
   sed -E \
