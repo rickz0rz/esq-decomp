49dbca22bc1e07b35aa98c0dafab8b80f5658bba
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-06 17:56:40 -0500

    Decompilation checkpoint: 1772837800

--- src/decomp/scripts/compare_sasc_buffer_ensure_allocated_trial.sh
+++ src/decomp/scripts/compare_sasc_buffer_ensure_allocated_trial.sh
@@ -9,13 +9,27 @@ SASC_DIR="src/decomp/sas_c"
 SASC_DIS="${SASC_DIR}/${SASC_SRC}.dis"
 ORIG_ASM="src/modules/submodules/unknown31.s"
 OUT_DIR="build/decomp/sasc_trial"
+ENTRY_ORIG="BUFFER_EnsureAllocated"
 
 mkdir -p "$OUT_DIR"
 
 ./sc-build-with-dis.sh "$SASC_SRC" >"${OUT_DIR}/sc_build_buffer_ensure_allocated.log" 2>&1
 
-awk '$0 ~ /^BUFFER_EnsureAllocated:$/ {in_func=1} in_func { if ($0 ~ /^;!======/) exit; print }' "$ORIG_ASM" >"${OUT_DIR}/buffer_ensure_allocated.original.s"
-awk '$0 ~ /^BUFFER_EnsureAllocated:$/ {in_func=1} in_func { if ($0 ~ /^__const:$/) exit; print }' "$SASC_DIS" >"${OUT_DIR}/buffer_ensure_allocated.sasc.dis.s"
+awk -v e="^${ENTRY_ORIG}:$" '
+  $0 ~ e {in_func=1}
+  in_func {
+    if ($0 ~ /^;!======/) exit
+    print
+  }
+' "$ORIG_ASM" >"${OUT_DIR}/buffer_ensure_allocated.original.s"
+
+awk -v e="^${ENTRY_ORIG}:$" '
+  $0 ~ e {in_func=1}
+  in_func {
+    if ($0 ~ /^__const:$/) exit
+    print
+  }
+' "$SASC_DIS" >"${OUT_DIR}/buffer_ensure_allocated.sasc.dis.s"
 
 normalize() {
   sed -E \
--- src/decomp/scripts/compare_sasc_buffer_flush_all_and_close_with_code_trial.sh
+++ src/decomp/scripts/compare_sasc_buffer_flush_all_and_close_with_code_trial.sh
@@ -9,13 +9,27 @@ SASC_DIR="src/decomp/sas_c"
 SASC_DIS="${SASC_DIR}/${SASC_SRC}.dis"
 ORIG_ASM="src/modules/submodules/unknown16.s"
 OUT_DIR="build/decomp/sasc_trial"
+ENTRY_ORIG="BUFFER_FlushAllAndCloseWithCode"
 
 mkdir -p "$OUT_DIR"
 
 ./sc-build-with-dis.sh "$SASC_SRC" >"${OUT_DIR}/sc_build_buffer_flush_all_and_close_with_code.log" 2>&1
 
-awk '$0 ~ /^BUFFER_FlushAllAndCloseWithCode:$/ {in_func=1} in_func { if ($0 ~ /^;!======/) exit; print }' "$ORIG_ASM" >"${OUT_DIR}/buffer_flush_all_and_close_with_code.original.s"
-awk '$0 ~ /^BUFFER_FlushAllAndCloseWithCode:$/ {in_func=1} in_func { if ($0 ~ /^__const:$/) exit; print }' "$SASC_DIS" >"${OUT_DIR}/buffer_flush_all_and_close_with_code.sasc.dis.s"
+awk -v e="^${ENTRY_ORIG}:$" '
+  $0 ~ e {in_func=1}
+  in_func {
+    if ($0 ~ /^;!======/) exit
+    print
+  }
+' "$ORIG_ASM" >"${OUT_DIR}/buffer_flush_all_and_close_with_code.original.s"
+
+awk -v e="^${ENTRY_ORIG}:$" '
+  $0 ~ e {in_func=1}
+  in_func {
+    if ($0 ~ /^__const:$/) exit
+    print
+  }
+' "$SASC_DIS" >"${OUT_DIR}/buffer_flush_all_and_close_with_code.sasc.dis.s"
 
 normalize() {
   sed -E \
