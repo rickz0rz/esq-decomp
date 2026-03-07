b27b420b29921ad4fe65a05df99827a6399f8ca7
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 11:34:55 -0500

    Decompilation checkpoint: 1772901295

--- src/decomp/sas_c/brush_stream_font_chunk.c
+++ src/decomp/sas_c/brush_stream_font_chunk.c
@@ -11,17 +11,17 @@ enum {
 extern void *Global_REF_DOS_LIBRARY_2;
 LONG _LVORead(LONG fh, void *buf, LONG len);
 
-LONG BRUSH_StreamFontChunk(LONG fh, LONG byte_count, LONG max_bytes, UBYTE *dst, void *state)
+LONG BRUSH_StreamFontChunk(LONG fh, LONG byteCount, LONG maxBytes, UBYTE *dst, void *state)
 {
     LONG bytesRemaining;
 
     (void)Global_REF_DOS_LIBRARY_2;
-    if (byte_count > max_bytes) {
+    if (byteCount > maxBytes) {
         return BRUSH_STREAM_STATUS_ERROR;
     }
 
-    *(LONG *)((UBYTE *)state + BRUSH_STATE_FONT_CHUNK_SIZE_OFFSET) = byte_count;
-    bytesRemaining = byte_count;
+    *(LONG *)((UBYTE *)state + BRUSH_STATE_FONT_CHUNK_SIZE_OFFSET) = byteCount;
+    bytesRemaining = byteCount;
 
     while (bytesRemaining > BRUSH_STREAM_CHUNK_SIZE) {
         if (_LVORead(fh, dst, BRUSH_STREAM_CHUNK_SIZE) != BRUSH_STREAM_CHUNK_SIZE) {
