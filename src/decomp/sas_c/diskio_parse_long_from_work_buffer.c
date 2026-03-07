4b00ae06459e322fba7342b0c9d18b118a801f29
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 08:08:34 -0500

    Decompilation checkpoint: 1772888913

--- src/decomp/sas_c/diskio_parse_long_from_work_buffer.c
+++ src/decomp/sas_c/diskio_parse_long_from_work_buffer.c
@@ -11,12 +11,12 @@ extern LONG GROUP_AG_JMPTBL_PARSE_ReadSignedLongSkipClass3_Alt(UBYTE *text);
 
 LONG DISKIO_ParseLongFromWorkBuffer(void)
 {
-    UBYTE *text;
+    UBYTE *numericText;
 
-    text = DISKIO_ConsumeCStringFromWorkBuffer();
-    if (text == (UBYTE *)DISKIO_WORKBUF_SENTINEL_ERROR) {
+    numericText = DISKIO_ConsumeCStringFromWorkBuffer();
+    if (numericText == (UBYTE *)DISKIO_WORKBUF_SENTINEL_ERROR) {
         return DISKIO_PARSE_RESULT_FAIL;
     }
 
-    return GROUP_AG_JMPTBL_PARSE_ReadSignedLongSkipClass3_Alt(text);
+    return GROUP_AG_JMPTBL_PARSE_ReadSignedLongSkipClass3_Alt(numericText);
 }
