0d7acbc2cbff15a9bce060612faf67d29f3a018d
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 07:39:43 -0500

    Decompilation checkpoint: 1772887183

--- src/decomp/sas_c/displib_commit_current_line_pen_and_advance.c
+++ src/decomp/sas_c/displib_commit_current_line_pen_and_advance.c
@@ -1,6 +1,10 @@
 typedef unsigned short UWORD;
 typedef unsigned long ULONG;
 
+enum {
+    DISPTEXT_LINE_EMPTY_LENGTH = 0
+};
+
 extern volatile UWORD DISPTEXT_TargetLineIndex;
 extern volatile UWORD DISPTEXT_CurrentLineIndex;
 extern volatile UWORD DISPTEXT_LineLengthTable[21];
@@ -10,7 +14,7 @@ void DISPLIB_CommitCurrentLinePenAndAdvance(ULONG pen)
 {
     UWORD lineIndex = DISPTEXT_CurrentLineIndex;
 
-    if (DISPTEXT_LineLengthTable[lineIndex] != 0) {
+    if (DISPTEXT_LineLengthTable[lineIndex] != DISPTEXT_LINE_EMPTY_LENGTH) {
         DISPTEXT_CurrentLineIndex = (UWORD)(lineIndex + 1U);
     }
 
