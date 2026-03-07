a2d15d134bfbbee9bd77ad43bf8331bc93b8d545
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 07:42:16 -0500

    Decompilation checkpoint: 1772887336

--- src/decomp/sas_c/displib_normalize_value_by_step.c
+++ src/decomp/sas_c/displib_normalize_value_by_step.c
@@ -1,10 +1,12 @@
 long DISPLIB_NormalizeValueByStep(short value, short lowerBound, short step)
 {
+    const short upperWrapBound = step;
+
     while (value < lowerBound) {
         value = (short)(value + step);
     }
 
-    while (value > step) {
+    while (value > upperWrapBound) {
         value = (short)(value - step);
     }
 
