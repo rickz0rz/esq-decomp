e94bf4942bfa198d4c5832dca18c041b4f44e902
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 11:47:52 -0500

    Decompilation checkpoint: 1772902072

--- src/decomp/sas_c/brush_select_brush_slot.c
+++ src/decomp/sas_c/brush_select_brush_slot.c
@@ -45,8 +45,8 @@ LONG BRUSH_SelectBrushSlot(
     LONG dstY;
     LONG spanX;
     LONG spanY;
-    LONG clip_w;
-    LONG clip_h;
+    LONG clipW;
+    LONG clipH;
     LONG alignModeX;
     LONG alignModeY;
 
@@ -55,25 +55,25 @@ LONG BRUSH_SelectBrushSlot(
     }
 
     spanX = srcX1 - srcX0 + BRUSH_SPAN_INCLUSIVE_DELTA;
-    clip_w = *(LONG *)(brush + BRUSH_CLIP_W_OFFSET);
-    if (clip_w > spanX) {
+    clipW = *(LONG *)(brush + BRUSH_CLIP_W_OFFSET);
+    if (clipW > spanX) {
         dstX = srcX0;
         alignModeX = *(LONG *)(brush + BRUSH_ALIGN_X_MODE_OFFSET);
         if (alignModeX == ALIGN_MODE_RIGHT_BOTTOM) {
-            dstX = clip_w - (srcX1 - srcX0) - BRUSH_SPAN_INCLUSIVE_DELTA;
+            dstX = clipW - (srcX1 - srcX0) - BRUSH_SPAN_INCLUSIVE_DELTA;
         } else if (alignModeX == ALIGN_MODE_CENTER) {
-            dstX = half_toward_zero(clip_w) - half_toward_zero(spanX);
+            dstX = half_toward_zero(clipW) - half_toward_zero(spanX);
         } else {
             dstX = *(LONG *)(brush + BRUSH_DST_X_OFFSET);
         }
     } else {
-        if (clip_w < spanX) {
+        if (clipW < spanX) {
             dstX = *(LONG *)(brush + BRUSH_DST_X_OFFSET);
             alignModeX = *(LONG *)(brush + BRUSH_ALIGN_X_MODE_OFFSET);
             if (alignModeX == ALIGN_MODE_RIGHT_BOTTOM) {
-                srcX = srcX1 - clip_w + BRUSH_SPAN_INCLUSIVE_DELTA;
+                srcX = srcX1 - clipW + BRUSH_SPAN_INCLUSIVE_DELTA;
             } else if (alignModeX == ALIGN_MODE_CENTER) {
-                srcX = srcX0 + half_toward_zero(spanX) - half_toward_zero(clip_w);
+                srcX = srcX0 + half_toward_zero(spanX) - half_toward_zero(clipW);
             } else {
                 srcX = srcX0;
             }
@@ -84,25 +84,25 @@ LONG BRUSH_SelectBrushSlot(
     }
 
     spanY = srcY1 - srcY0 + BRUSH_SPAN_INCLUSIVE_DELTA;
-    clip_h = *(LONG *)(brush + BRUSH_CLIP_H_OFFSET);
-    if (clip_h > spanY) {
+    clipH = *(LONG *)(brush + BRUSH_CLIP_H_OFFSET);
+    if (clipH > spanY) {
         dstY = srcY0;
         alignModeY = *(LONG *)(brush + BRUSH_ALIGN_Y_MODE_OFFSET);
         if (alignModeY == ALIGN_MODE_RIGHT_BOTTOM) {
-            dstY = clip_h - (srcY1 - srcY0) - BRUSH_SPAN_INCLUSIVE_DELTA;
+            dstY = clipH - (srcY1 - srcY0) - BRUSH_SPAN_INCLUSIVE_DELTA;
         } else if (alignModeY == ALIGN_MODE_CENTER) {
-            dstY = half_toward_zero(clip_h) - half_toward_zero(spanY);
+            dstY = half_toward_zero(clipH) - half_toward_zero(spanY);
         } else {
             dstY = *(LONG *)(brush + BRUSH_DST_Y_OFFSET);
         }
     } else {
-        if (clip_h < spanY) {
+        if (clipH < spanY) {
             dstY = *(LONG *)(brush + BRUSH_DST_Y_OFFSET);
             alignModeY = *(LONG *)(brush + BRUSH_ALIGN_Y_MODE_OFFSET);
             if (alignModeY == ALIGN_MODE_RIGHT_BOTTOM) {
-                srcY = srcY1 - clip_h + BRUSH_SPAN_INCLUSIVE_DELTA;
+                srcY = srcY1 - clipH + BRUSH_SPAN_INCLUSIVE_DELTA;
             } else if (alignModeY == ALIGN_MODE_CENTER) {
-                srcY = srcY0 + half_toward_zero(spanY) - half_toward_zero(clip_h);
+                srcY = srcY0 + half_toward_zero(spanY) - half_toward_zero(clipH);
             } else {
                 srcY = srcY0;
             }
@@ -112,14 +112,14 @@ LONG BRUSH_SelectBrushSlot(
         }
     }
 
-    clip_w = srcX1 - srcX0 + BRUSH_SPAN_INCLUSIVE_DELTA;
-    if (clip_w > *(LONG *)(brush + BRUSH_CLIP_W_OFFSET)) {
-        clip_w = *(LONG *)(brush + BRUSH_CLIP_W_OFFSET);
+    clipW = srcX1 - srcX0 + BRUSH_SPAN_INCLUSIVE_DELTA;
+    if (clipW > *(LONG *)(brush + BRUSH_CLIP_W_OFFSET)) {
+        clipW = *(LONG *)(brush + BRUSH_CLIP_W_OFFSET);
     }
 
-    clip_h = srcY1 - srcY0 + BRUSH_SPAN_INCLUSIVE_DELTA;
-    if (clip_h > *(LONG *)(brush + BRUSH_CLIP_H_OFFSET)) {
-        clip_h = *(LONG *)(brush + BRUSH_CLIP_H_OFFSET);
+    clipH = srcY1 - srcY0 + BRUSH_SPAN_INCLUSIVE_DELTA;
+    if (clipH > *(LONG *)(brush + BRUSH_CLIP_H_OFFSET)) {
+        clipH = *(LONG *)(brush + BRUSH_CLIP_H_OFFSET);
     }
 
     if (forcedDstY > BRUSH_FORCED_DST_Y_MIN) {
@@ -133,8 +133,8 @@ LONG BRUSH_SelectBrushSlot(
         dstRp,
         dstX,
         dstY,
-        clip_w,
-        clip_h,
+        clipW,
+        clipH,
         BLIT_MINTERM_COPY
     );
 }
