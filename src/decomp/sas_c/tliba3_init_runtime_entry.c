e8aeccc178910940e641a35711e6189f549c29a2
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 05:18:00 -0500

    Decompilation checkpoint: 1772878680

--- src/decomp/sas_c/tliba3_init_runtime_entry.c
+++ src/decomp/sas_c/tliba3_init_runtime_entry.c
@@ -3,6 +3,24 @@ typedef unsigned long ULONG;
 typedef unsigned short UWORD;
 typedef unsigned char UBYTE;
 
+enum {
+    VM_RUNTIME_STRIDE = 154,
+    VM_REGBASE_OFFSET = 0,
+    VM_WIDTH_OFFSET = 2,
+    VM_HEIGHT_OFFSET = 4,
+    VM_X_OFFSET = 6,
+    VM_Y_OFFSET = 8,
+    VM_RPORT_COPY_OFFSET = 10,
+    VM_BITMAP_PTR_SLOT_OFFSET = 14,
+    VM_BITMAP_OFFSET = 110,
+    VM_PLANE_PTR_BASE_OFFSET = 118,
+    VM_PLANE_PTR_STRIDE = 4,
+    VM_PLANE_PTR_COUNT = 6,
+    VM_RESERVED0_OFFSET = 150,
+    VM_RESERVED1_OFFSET = 152,
+    VM_RPORT_COPY_LAST_INDEX = 24
+};
+
 extern UBYTE TLIBA3_VmArrayRuntimeTable[];
 extern void *Global_REF_RASTPORT_1;
 extern ULONG WDISP_DisplayContextPlanePointer0[];
@@ -23,35 +41,36 @@ void TLIBA3_InitRuntimeEntry(
     UBYTE *vm;
     LONG i;
 
-    vm = TLIBA3_VmArrayRuntimeTable + MATH_Mulu32(idx, 154);
+    vm = TLIBA3_VmArrayRuntimeTable + MATH_Mulu32(idx, VM_RUNTIME_STRIDE);
 
-    *(UWORD *)(vm + 0) = (UWORD)regBase;
-    *(UWORD *)(vm + 2) = (UWORD)width;
-    *(UWORD *)(vm + 4) = (UWORD)height;
-    *(UWORD *)(vm + 6) = (UWORD)x;
-    *(UWORD *)(vm + 8) = (UWORD)y;
+    *(UWORD *)(vm + VM_REGBASE_OFFSET) = (UWORD)regBase;
+    *(UWORD *)(vm + VM_WIDTH_OFFSET) = (UWORD)width;
+    *(UWORD *)(vm + VM_HEIGHT_OFFSET) = (UWORD)height;
+    *(UWORD *)(vm + VM_X_OFFSET) = (UWORD)x;
+    *(UWORD *)(vm + VM_Y_OFFSET) = (UWORD)y;
 
     {
         ULONG *src = (ULONG *)Global_REF_RASTPORT_1;
-        ULONG *dst = (ULONG *)(vm + 10);
-        for (i = 0; i <= 24; ++i) {
+        ULONG *dst = (ULONG *)(vm + VM_RPORT_COPY_OFFSET);
+        for (i = 0; i <= VM_RPORT_COPY_LAST_INDEX; ++i) {
             dst[i] = src[i];
         }
     }
 
-    *(ULONG *)(vm + 14) = (ULONG)(vm + 110);
+    *(ULONG *)(vm + VM_BITMAP_PTR_SLOT_OFFSET) = (ULONG)(vm + VM_BITMAP_OFFSET);
 
     _LVOInitBitMap(
         Global_REF_GRAPHICS_LIBRARY,
-        vm + 110,
+        vm + VM_BITMAP_OFFSET,
         (LONG)(UBYTE)depth,
         (LONG)(UWORD)width,
         (LONG)(UWORD)height);
 
-    for (i = 0; i < 6; ++i) {
-        *(ULONG *)(vm + 118 + (i * 4)) = WDISP_DisplayContextPlanePointer0[i];
+    for (i = 0; i < VM_PLANE_PTR_COUNT; ++i) {
+        *(ULONG *)(vm + VM_PLANE_PTR_BASE_OFFSET + (i * VM_PLANE_PTR_STRIDE)) =
+            WDISP_DisplayContextPlanePointer0[i];
     }
 
-    *(UWORD *)(vm + 150) = 0;
-    *(UWORD *)(vm + 152) = 0;
+    *(UWORD *)(vm + VM_RESERVED0_OFFSET) = 0;
+    *(UWORD *)(vm + VM_RESERVED1_OFFSET) = 0;
 }
