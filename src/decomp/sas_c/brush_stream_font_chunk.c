1b5a08a40d4008a202df93092e0a2e5e20f641ef
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 11:28:06 -0500

    Decompilation checkpoint: 1772900886

--- src/decomp/sas_c/brush_stream_font_chunk.c
+++ src/decomp/sas_c/brush_stream_font_chunk.c
@@ -13,7 +13,7 @@ LONG _LVORead(LONG fh, void *buf, LONG len);
 
 LONG BRUSH_StreamFontChunk(LONG fh, LONG byte_count, LONG max_bytes, UBYTE *dst, void *state)
 {
-    LONG remaining;
+    LONG bytesRemaining;
 
     (void)Global_REF_DOS_LIBRARY_2;
     if (byte_count > max_bytes) {
@@ -21,17 +21,17 @@ LONG BRUSH_StreamFontChunk(LONG fh, LONG byte_count, LONG max_bytes, UBYTE *dst,
     }
 
     *(LONG *)((UBYTE *)state + BRUSH_STATE_FONT_CHUNK_SIZE_OFFSET) = byte_count;
-    remaining = byte_count;
+    bytesRemaining = byte_count;
 
-    while (remaining > BRUSH_STREAM_CHUNK_SIZE) {
+    while (bytesRemaining > BRUSH_STREAM_CHUNK_SIZE) {
         if (_LVORead(fh, dst, BRUSH_STREAM_CHUNK_SIZE) != BRUSH_STREAM_CHUNK_SIZE) {
             return BRUSH_STREAM_STATUS_ERROR;
         }
         dst += BRUSH_STREAM_CHUNK_SIZE;
-        remaining -= BRUSH_STREAM_CHUNK_SIZE;
+        bytesRemaining -= BRUSH_STREAM_CHUNK_SIZE;
     }
 
-    if (_LVORead(fh, dst, remaining) != remaining) {
+    if (_LVORead(fh, dst, bytesRemaining) != bytesRemaining) {
         return BRUSH_STREAM_STATUS_ERROR;
     }
 
