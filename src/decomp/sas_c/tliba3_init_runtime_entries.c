59c0e86fb777dbdfc899aad8e79a6580016da9a8
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 05:19:56 -0500

    Decompilation checkpoint: 1772878796

--- src/decomp/sas_c/tliba3_init_runtime_entries.c
+++ src/decomp/sas_c/tliba3_init_runtime_entries.c
@@ -4,6 +4,22 @@ typedef signed short WORD;
 enum {
     GRAPHICS_WORD_OFFSET = 206,
     RUNTIME_DIW_MASK = 2,
+    VM_X_0 = 0,
+    VM_X_8 = 8,
+    VM_X_16 = 16,
+    VM_X_44 = 44,
+    VM_X_360 = 360,
+    VM_Y_0 = 0,
+    VM_WIDTH_320 = 320,
+    VM_WIDTH_352 = 352,
+    VM_WIDTH_640 = 640,
+    VM_WIDTH_696 = 696,
+    VM_HEIGHT_120 = 120,
+    VM_HEIGHT_240 = 240,
+    VM_HEIGHT_296 = 296,
+    VM_DEPTH_1 = 1,
+    VM_DEPTH_4 = 4,
+    VM_DEPTH_5 = 5,
     REGBASE_C304 = 0xC304,
     REGBASE_8304 = 0x8304,
     REGBASE_4304 = 0x4304,
@@ -33,29 +49,29 @@ void TLIBA3_InitRuntimeEntries(void)
     d7 &= RUNTIME_DIW_MASK;
 
     d1 = (LONG)(WORD)(d7 + REGBASE_C304);
-    TLIBA3_InitRuntimeEntry(0, d1, 352, 240, 360, 0, 4);
+    TLIBA3_InitRuntimeEntry(0, d1, VM_WIDTH_352, VM_HEIGHT_240, VM_X_360, VM_Y_0, VM_DEPTH_4);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_C304);
-    TLIBA3_InitRuntimeEntry(1, d1, 352, 240, 16, 0, 4);
+    TLIBA3_InitRuntimeEntry(1, d1, VM_WIDTH_352, VM_HEIGHT_240, VM_X_16, VM_Y_0, VM_DEPTH_4);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_C304);
-    TLIBA3_InitRuntimeEntry(2, d1, 696, 240, 8, 0, 4);
+    TLIBA3_InitRuntimeEntry(2, d1, VM_WIDTH_696, VM_HEIGHT_240, VM_X_8, VM_Y_0, VM_DEPTH_4);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_8304);
-    TLIBA3_InitRuntimeEntry(3, d1, 696, 240, 0, 0, 1);
+    TLIBA3_InitRuntimeEntry(3, d1, VM_WIDTH_696, VM_HEIGHT_240, VM_X_0, VM_Y_0, VM_DEPTH_1);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_C304);
-    TLIBA3_InitRuntimeEntry(4, d1, 640, 240, 44, 0, 4);
+    TLIBA3_InitRuntimeEntry(4, d1, VM_WIDTH_640, VM_HEIGHT_240, VM_X_44, VM_Y_0, VM_DEPTH_4);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_4304);
-    TLIBA3_InitRuntimeEntry(5, d1, 320, 240, 44, 0, 5);
+    TLIBA3_InitRuntimeEntry(5, d1, VM_WIDTH_320, VM_HEIGHT_240, VM_X_44, VM_Y_0, VM_DEPTH_5);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_C300);
-    TLIBA3_InitRuntimeEntry(6, d1, 640, 120, 44, 0, 4);
+    TLIBA3_InitRuntimeEntry(6, d1, VM_WIDTH_640, VM_HEIGHT_120, VM_X_44, VM_Y_0, VM_DEPTH_4);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_4300);
-    TLIBA3_InitRuntimeEntry(7, d1, 320, 120, 44, 0, 5);
+    TLIBA3_InitRuntimeEntry(7, d1, VM_WIDTH_320, VM_HEIGHT_120, VM_X_44, VM_Y_0, VM_DEPTH_5);
 
     d1 = (LONG)(WORD)(d7 + REGBASE_C304);
-    TLIBA3_InitRuntimeEntry(8, d1, 320, 296, 16, 0, 4);
+    TLIBA3_InitRuntimeEntry(8, d1, VM_WIDTH_320, VM_HEIGHT_296, VM_X_16, VM_Y_0, VM_DEPTH_4);
 }
