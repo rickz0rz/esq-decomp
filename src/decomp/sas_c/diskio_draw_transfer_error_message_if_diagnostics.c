a98910c3e8ef54c7cc2dbb341ba59938f662cce2
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 07:51:30 -0500

    Decompilation checkpoint: 1772887890

--- src/decomp/sas_c/diskio_draw_transfer_error_message_if_diagnostics.c
+++ src/decomp/sas_c/diskio_draw_transfer_error_message_if_diagnostics.c
@@ -1,6 +1,15 @@
 typedef signed long LONG;
 typedef signed short WORD;
 
+enum {
+    DISKIO_DIAG_INACTIVE = 0,
+    DISKIO_REASON_INDEX_OFFSET = 1,
+    DISKIO_ERROR_TEXT_X = 40,
+    DISKIO_ERROR_TEXT_Y = 240,
+    DISKIO_ERROR_TEXT_PEN = 4,
+    DISKIO_DEFAULT_TEXT_PEN = 1
+};
+
 extern WORD ED_DiagnosticsScreenActive;
 extern void *Global_REF_RASTPORT_1;
 extern void *Global_REF_GRAPHICS_LIBRARY;
@@ -11,15 +20,15 @@ extern void DISPLIB_DisplayTextAtPosition(void *rastPort, LONG x, LONG y, const 
 
 void DISKIO_DrawTransferErrorMessageIfDiagnostics(LONG reasonCode)
 {
-    if (ED_DiagnosticsScreenActive == 0) {
+    if (ED_DiagnosticsScreenActive == DISKIO_DIAG_INACTIVE) {
         return;
     }
 
-    _LVOSetAPen(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, 4);
+    _LVOSetAPen(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, DISKIO_ERROR_TEXT_PEN);
     DISPLIB_DisplayTextAtPosition(
         Global_REF_RASTPORT_1,
-        40,
-        240,
-        CTASKS_TerminationReasonPtrTable[reasonCode - 1]);
-    _LVOSetAPen(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, 1);
+        DISKIO_ERROR_TEXT_X,
+        DISKIO_ERROR_TEXT_Y,
+        CTASKS_TerminationReasonPtrTable[reasonCode - DISKIO_REASON_INDEX_OFFSET]);
+    _LVOSetAPen(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, DISKIO_DEFAULT_TEXT_PEN);
 }
