8aea6499a6f7c6c76968d1fd77feb7d4cbb4b01d
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 11:51:59 -0500

    Decompilation checkpoint: 1772902319

--- src/decomp/sas_c/brush_find_brush_by_predicate.c
+++ src/decomp/sas_c/brush_find_brush_by_predicate.c
@@ -7,13 +7,13 @@ enum {
 
 LONG GROUP_AA_JMPTBL_STRING_CompareNoCase(void *a, void *b);
 
-void *BRUSH_FindBrushByPredicate(void *key, void *listHeadPtr)
+void *BRUSH_FindBrushByPredicate(void *searchKey, void *listHeadPtr)
 {
     unsigned char *nodeCursor;
 
     nodeCursor = *(unsigned char **)listHeadPtr;
     while (nodeCursor != (unsigned char *)BRUSH_NULL) {
-        if (GROUP_AA_JMPTBL_STRING_CompareNoCase((void *)nodeCursor, key) == BRUSH_NULL) {
+        if (GROUP_AA_JMPTBL_STRING_CompareNoCase((void *)nodeCursor, searchKey) == BRUSH_NULL) {
             return (void *)nodeCursor;
         }
         nodeCursor = *(unsigned char **)(nodeCursor + BRUSH_NODE_NEXT_OFFSET);
