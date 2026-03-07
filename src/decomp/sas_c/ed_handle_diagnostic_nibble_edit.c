c9350cb86d5cdcb325faf6b9eaf1c01e151d5106
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-06 19:37:04 -0500

    Decompilation checkpoint: 1772843824

---
+++ src/decomp/sas_c/ed_handle_diagnostic_nibble_edit.c
@@ -0,0 +1,113 @@
+typedef signed long LONG;
+typedef unsigned char UBYTE;
+
+extern UBYTE ED_LastKeyCode;
+extern LONG ED_TempCopyOffset;
+extern LONG ED_StateRingIndex;
+extern UBYTE ED_StateRingTable[];
+extern UBYTE ED_LastMenuInputChar;
+
+extern UBYTE GCOMMAND_PresetFallbackValue0[];
+extern UBYTE GCOMMAND_PresetFallbackValue1[];
+extern UBYTE GCOMMAND_PresetFallbackValue2[];
+
+extern void ED_DrawESCMenuBottomHelp(void);
+extern void ED1_JMPTBL_ESQSHARED4_LoadDefaultPaletteToCopper_NoOp(void);
+extern void ED_DrawDiagnosticRegisterValues(void);
+
+static void ED_IncNibble(UBYTE *base)
+{
+    UBYTE prev = *base;
+    *base = (UBYTE)(*base + 1);
+    if (prev >= 15) {
+        *base = 0;
+    }
+}
+
+static void ED_DecNibble(UBYTE *base)
+{
+    *base = (UBYTE)(*base - 1);
+    if (*base > 15) {
+        *base = 15;
+    }
+}
+
+static LONG ED_DiagIndex3(void)
+{
+    LONG idx = ED_TempCopyOffset;
+    return (idx << 2) - idx;
+}
+
+void ED_HandleDiagnosticNibbleEdit(void)
+{
+    LONG idx3;
+
+    switch ((LONG)ED_LastKeyCode) {
+        case 13:
+        case 27:
+            ED_DrawESCMenuBottomHelp();
+            return;
+
+        case 66:
+            idx3 = ED_DiagIndex3();
+            ED_IncNibble(&GCOMMAND_PresetFallbackValue2[idx3]);
+            break;
+
+        case 71:
+            idx3 = ED_DiagIndex3();
+            ED_IncNibble(&GCOMMAND_PresetFallbackValue1[idx3]);
+            break;
+
+        case 82:
+            idx3 = ED_DiagIndex3();
+            ED_IncNibble(&GCOMMAND_PresetFallbackValue0[idx3]);
+            break;
+
+        case 98:
+            idx3 = ED_DiagIndex3();
+            ED_DecNibble(&GCOMMAND_PresetFallbackValue2[idx3]);
+            break;
+
+        case 103:
+            idx3 = ED_DiagIndex3();
+            ED_DecNibble(&GCOMMAND_PresetFallbackValue1[idx3]);
+            break;
+
+        case 114:
+            idx3 = ED_DiagIndex3();
+            ED_DecNibble(&GCOMMAND_PresetFallbackValue0[idx3]);
+            break;
+
+        case 155: {
+            LONG ringOff = (ED_StateRingIndex << 2) + ED_StateRingIndex;
+            UBYTE menuChar = ED_StateRingTable[ringOff + 1];
+            ED_LastMenuInputChar = menuChar;
+
+            if (menuChar == 68) {
+                --ED_TempCopyOffset;
+                if (ED_TempCopyOffset < 0) {
+                    ED_TempCopyOffset = 39;
+                }
+            } else {
+                ++ED_TempCopyOffset;
+                if (ED_TempCopyOffset == 40) {
+                    ED_TempCopyOffset = 0;
+                }
+            }
+            break;
+        }
+
+        default:
+            ++ED_TempCopyOffset;
+            if (ED_TempCopyOffset == 40) {
+                ED_TempCopyOffset = 0;
+            }
+            break;
+    }
+
+    if (ED_TempCopyOffset >= 0 && ED_TempCopyOffset < 40) {
+        ED1_JMPTBL_ESQSHARED4_LoadDefaultPaletteToCopper_NoOp();
+    }
+
+    ED_DrawDiagnosticRegisterValues();
+}
---
+++ src/decomp/scripts/semantic_filter_sasc_ed_handle_diagnostic_nibble_edit.awk
@@ -0,0 +1,24 @@
+BEGIN{h_help=0;h_index3=0;h_inc=0;h_dec=0;h_state_ring=0;h_palette=0;h_draw_regs=0;h_rts=0}
+function t(s, x){x=s;sub(/;.*/,"",x);sub(/^[ \t]+/,"",x);sub(/[ \t]+$/,"",x);gsub(/[ \t]+/," ",x);return toupper(x)}
+{
+    l=t($0)
+    if(l=="")next
+    if(l~/(JSR|BSR).*ED_DRAWESCMENUBOTTOMHELP/)h_help=1
+    if(l~/LSL\.L #\$?2,D0/ || l~/SUB\.L ED_TEMPCOPYOFFSET,D0/ || l~/(JSR|BSR).*ED_DIAGINDEX3/)h_index3=1
+    if(l~/ADDQ\.B #\$?1,\(A1\)/ || l~/(JSR|BSR).*ED_INCNIBBLE/)h_inc=1
+    if(l~/SUBQ\.B #\$?1,\(A1\)/ || l~/(JSR|BSR).*ED_DECNIBBLE/)h_dec=1
+    if(l~/ED_STATERINGINDEX/ || l~/ED_STATERINGTABLE/ || l~/ED_LASTMENUINPUTCHAR/)h_state_ring=1
+    if(l~/(JSR|BSR).*ED1_JMPTBL_ESQSHARED4_LOADDEFAULTPALETTETOCOPPER_NO_OP/ || l~/(JSR|BSR).*ED1_JMPTBL_ESQSHARED4_LOADDEFAULT/ || l~/(JSR|BSR).*ED1_JMPTBL_ESQSHARED4_LOADDEFAUL/)h_palette=1
+    if(l~/(JSR|BSR).*ED_DRAWDIAGNOSTICREGISTERVALUES/)h_draw_regs=1
+    if(l=="RTS")h_rts=1
+}
+END{
+    print "HAS_HELP_CASE="h_help
+    print "HAS_INDEX_TIMES3="h_index3
+    print "HAS_INC_PATH="h_inc
+    print "HAS_DEC_PATH="h_dec
+    print "HAS_RING_ADJUST_PATH="h_state_ring
+    print "HAS_PALETTE_REFRESH="h_palette
+    print "HAS_DRAW_REGS="h_draw_regs
+    print "HAS_RTS="h_rts
+}
