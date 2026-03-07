ab0edfb5710cce427b34028768ac7c161c6f7951
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 08:15:18 -0500

    Decompilation checkpoint: 1772889318

--- src/decomp/sas_c/datetime_adjust_month_index.c
+++ src/decomp/sas_c/datetime_adjust_month_index.c
@@ -10,14 +10,14 @@ enum {
 
 LONG DATETIME_AdjustMonthIndex(void *ctx)
 {
-    UBYTE *p = (UBYTE *)ctx;
-    LONG month = (LONG)(WORD)(*(WORD *)(p + DATETIME_MONTH_OFFSET));
-    LONG rem = month % DATETIME_MONTHS_PER_YEAR;
+    UBYTE *ctxBytes = (UBYTE *)ctx;
+    LONG month = (LONG)(WORD)(*(WORD *)(ctxBytes + DATETIME_MONTH_OFFSET));
+    LONG normalizedMonth = month % DATETIME_MONTHS_PER_YEAR;
 
-    if (*(WORD *)(p + DATETIME_SIGNFLAG_OFFSET) != 0) {
-        rem += DATETIME_MONTHS_PER_YEAR;
+    if (*(WORD *)(ctxBytes + DATETIME_SIGNFLAG_OFFSET) != 0) {
+        normalizedMonth += DATETIME_MONTHS_PER_YEAR;
     }
 
-    *(WORD *)(p + DATETIME_MONTH_OFFSET) = (WORD)rem;
-    return rem;
+    *(WORD *)(ctxBytes + DATETIME_MONTH_OFFSET) = (WORD)normalizedMonth;
+    return normalizedMonth;
 }
