f9743b9c3ecd5a0165f84de65f052c0fd868e6e4
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 06:51:56 -0500

    Decompilation checkpoint: 1772884316

--- src/decomp/sas_c/coi_free_entry_resources.c
+++ src/decomp/sas_c/coi_free_entry_resources.c
@@ -1,6 +1,11 @@
 typedef unsigned char UBYTE;
 typedef long LONG;
 
+enum {
+    COI_AUX_DEALLOC_LINE = 815,
+    COI_AUX_DEALLOC_BYTES = 42
+};
+
 extern UBYTE Global_STR_COI_C_3[];
 
 void COI_FreeSubEntryTableEntries(void *entry);
@@ -22,7 +27,11 @@ void COI_FreeEntryResources(void *entry)
     COI_ClearAnimObjectStrings((void *)e);
 
     if (aux != (UBYTE *)0) {
-        GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(Global_STR_COI_C_3, 815, (void *)aux, 42);
+        GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(
+            Global_STR_COI_C_3,
+            COI_AUX_DEALLOC_LINE,
+            (void *)aux,
+            COI_AUX_DEALLOC_BYTES);
     }
 
     *(void **)(e + 48) = (void *)0;
--- src/decomp/sas_c/ctasks_start_iff_task_process.c
+++ src/decomp/sas_c/ctasks_start_iff_task_process.c
@@ -2,6 +2,22 @@ typedef unsigned short UWORD;
 typedef unsigned long ULONG;
 typedef long LONG;
 
+enum {
+    CTASKS_FLAG_FALSE = 0,
+    CTASKS_TASKSTATE_LOGO = 4,
+    CTASKS_TASKSTATE_GADS = 5,
+    CTASKS_TASKSTATE_IFF = 6,
+    CTASKS_TASKPROC_ALLOC_LINE = 159,
+    CTASKS_TASKPROC_STRUCT_SIZE = 14,
+    CTASKS_MEMF_PUBLIC_CLEAR = 0x10001UL,
+    CTASKS_TASKLIST_SIZE_OFFSET = 0,
+    CTASKS_TASKLIST_MAGIC_OFFSET = 8,
+    CTASKS_TASKLIST_ENTRY_OFFSET = 10,
+    CTASKS_TASKLIST_MAGIC = 20217,
+    CTASKS_TASKLIST_SEG_BPTR_SHIFT = 2,
+    CTASKS_TASKLIST_SEG_BPTR_ADD = 4
+};
+
 extern UWORD CTASKS_IffTaskDoneFlag;
 extern UWORD CTASKS_IffTaskState;
 extern UWORD ESQIFF_AssetSourceSelect;
@@ -21,19 +37,6 @@ LONG _LVOCreateProc(void);
 
 void CTASKS_StartIffTaskProcess(void)
 {
-    const UWORD FLAG_FALSE = 0;
-    const UWORD TASKSTATE_IFF = 6;
-    const UWORD TASKSTATE_LOGO = 4;
-    const UWORD TASKSTATE_GADS = 5;
-    const LONG TASKPROC_ALLOC_LINE = 159;
-    const LONG TASKPROC_STRUCT_SIZE = 14;
-    const ULONG MEMF_PUBLIC_CLEAR = 0x10001UL;
-    const LONG TASKLIST_SIZE_OFFSET = 0;
-    const LONG TASKLIST_MAGIC_OFFSET = 8;
-    const LONG TASKLIST_ENTRY_OFFSET = 10;
-    const UWORD TASKLIST_MAGIC = 20217;
-    const LONG TASKLIST_SEG_BPTR_SHIFT = 2;
-    const LONG TASKLIST_SEG_BPTR_ADD = 4;
     LONG task;
     LONG list_ptr;
     LONG seg_bptr;
@@ -42,14 +45,14 @@ void CTASKS_StartIffTaskProcess(void)
         _LVOForbid();
         task = _LVOFindTask();
         _LVOPermit();
-    } while (task != FLAG_FALSE);
+    } while (task != CTASKS_FLAG_FALSE);
 
-    CTASKS_IffTaskDoneFlag = FLAG_FALSE;
-    if ((UWORD)(CTASKS_IffTaskState - TASKSTATE_IFF) != 0) {
-        if (ESQIFF_AssetSourceSelect != FLAG_FALSE) {
-            CTASKS_IffTaskState = TASKSTATE_LOGO;
+    CTASKS_IffTaskDoneFlag = CTASKS_FLAG_FALSE;
+    if ((UWORD)(CTASKS_IffTaskState - CTASKS_TASKSTATE_IFF) != 0) {
+        if (ESQIFF_AssetSourceSelect != CTASKS_FLAG_FALSE) {
+            CTASKS_IffTaskState = CTASKS_TASKSTATE_LOGO;
         } else {
-            CTASKS_IffTaskState = TASKSTATE_GADS;
+            CTASKS_IffTaskState = CTASKS_TASKSTATE_GADS;
         }
     }
 
