ed02aee6d14a390aec7f0b0c1674dd6402d0be93
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 04:47:44 -0500

    Decompilation checkpoint: 1772876864

--- src/decomp/sas_c/brush_clone_brush_record.c
+++ src/decomp/sas_c/brush_clone_brush_record.c
@@ -4,6 +4,11 @@ typedef unsigned long ULONG;
 typedef long LONG;
 
 enum {
+    BRUSH_NULL = 0,
+    BRUSH_PENDING_ALERT_SET = 1,
+    BRUSH_PLANE_COUNT = 5,
+    BRUSH_PLANE_PAIR_COPY_COUNT = 4,
+    BRUSH_RASTPORT_COPY_BYTES = 96,
     BRUSH_RECORD_SIZE = 372,
     MEMF_PUBLIC_CLEAR = 0x10001UL
 };
@@ -37,21 +42,21 @@ void *BRUSH_CloneBrushRecord(void *src_rec)
         1248,
         BRUSH_RECORD_SIZE,
         MEMF_PUBLIC_CLEAR);
-    if (dst == (UBYTE *)0) {
-        return (void *)0;
+    if (dst == (UBYTE *)BRUSH_NULL) {
+        return (void *)BRUSH_NULL;
     }
 
     s = src;
     d = dst;
     do {
         *d++ = *s;
-    } while (*s++ != 0);
+    } while (*s++ != BRUSH_NULL);
 
-    for (i = 0; i <= 4; i++) {
+    for (i = BRUSH_NULL; i < BRUSH_PLANE_COUNT; i++) {
         *(ULONG *)(dst + 176 + (i * 4)) = *(ULONG *)(src + 128 + (i * 4));
     }
     *(ULONG *)(dst + 196) = *(ULONG *)(src + 148);
-    *(ULONG *)(dst + 368) = 0;
+    *(ULONG *)(dst + 368) = BRUSH_NULL;
 
     _LVOInitBitMap(dst + 136, (UBYTE)dst[184], (UWORD)*(UWORD *)(dst + 176), (UWORD)*(UWORD *)(dst + 178));
 
@@ -64,7 +69,7 @@ void *BRUSH_CloneBrushRecord(void *src_rec)
     *(ULONG *)(dst + 356) = *(ULONG *)(src + 222);
     *(ULONG *)(dst + 360) = *(ULONG *)(src + 226);
 
-    for (i = 0; i < 4; i++) {
+    for (i = BRUSH_NULL; i < BRUSH_PLANE_PAIR_COPY_COUNT; i++) {
         *(ULONG *)(dst + 200 + (i * 8) + 0) = *(ULONG *)(src + 152 + (i * 8) + 0);
         *(ULONG *)(dst + 200 + (i * 8) + 4) = *(ULONG *)(src + 152 + (i * 8) + 4);
     }
@@ -73,23 +78,23 @@ void *BRUSH_CloneBrushRecord(void *src_rec)
     d = dst + 33;
     do {
         *d++ = *s;
-    } while (*s++ != 0);
+    } while (*s++ != BRUSH_NULL);
 
     *(ULONG *)(dst + 364) = *(ULONG *)(src + 230);
 
-    if (*(ULONG *)(src + 214) != 0) {
+    if (*(ULONG *)(src + 214) != BRUSH_NULL) {
         *(ULONG *)(dst + 348) = *(ULONG *)(src + 214);
     } else {
         *(ULONG *)(dst + 348) = (UWORD)*(UWORD *)(dst + 176);
     }
 
-    if (*(ULONG *)(src + 218) != 0) {
+    if (*(ULONG *)(src + 218) != BRUSH_NULL) {
         *(ULONG *)(dst + 352) = *(ULONG *)(src + 218);
     } else {
         *(ULONG *)(dst + 352) = (UWORD)*(UWORD *)(dst + 178);
     }
 
-    for (i = 0; i < (LONG)(UBYTE)dst[184] && i < 5; i++) {
+    for (i = BRUSH_NULL; i < (LONG)(UBYTE)dst[184] && i < BRUSH_PLANE_COUNT; i++) {
         void *p;
         LONG plane_off = i << 2;
         p = GROUP_AA_JMPTBL_GRAPHICS_AllocRaster(
@@ -99,15 +104,15 @@ void *BRUSH_CloneBrushRecord(void *src_rec)
             (UWORD)*(UWORD *)(dst + 176),
             (UWORD)*(UWORD *)(dst + 178));
         *(void **)(dst + 0x90 + plane_off) = p;
-        if (p == (void *)0) {
+        if (p == (void *)BRUSH_NULL) {
             _LVOForbid();
-            if (BRUSH_PendingAlertCode == 0) {
+            if (BRUSH_PendingAlertCode == BRUSH_NULL) {
                 const UBYTE *snap_s = dst;
                 UBYTE *snap_d = BRUSH_SnapshotHeader;
-                BRUSH_PendingAlertCode = 1;
+                BRUSH_PendingAlertCode = BRUSH_PENDING_ALERT_SET;
                 do {
                     *snap_d++ = *snap_s;
-                } while (*snap_s++ != 0);
+                } while (*snap_s++ != BRUSH_NULL);
             }
             _LVOPermit();
             break;
@@ -117,7 +122,7 @@ void *BRUSH_CloneBrushRecord(void *src_rec)
     if (i == (LONG)(UBYTE)dst[184]) {
         _LVOInitRastPort(dst + 36);
         *(void **)(dst + 40) = dst + 136;
-        for (i = 0; i < 96; i++) {
+        for (i = BRUSH_NULL; i < BRUSH_RASTPORT_COPY_BYTES; i++) {
             dst[232 + i] = src[32 + i];
         }
     }
