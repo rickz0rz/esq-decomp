06904e0ee4890ea39896d3bf8921806305044ee7
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-07 06:35:41 -0500

    Decompilation checkpoint: 1772883341

--- src/decomp/sas_c/ed_draw_special_functions_menu.c
+++ src/decomp/sas_c/ed_draw_special_functions_menu.c
@@ -14,13 +14,20 @@ extern LONG DISPLIB_DisplayTextAtPosition(void *rastPort, LONG y, LONG x, const 
 
 void ED_DrawSpecialFunctionsMenu(void)
 {
-    _LVOSetAPen(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, 1);
-    _LVOSetDrMd(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, 0);
+    const LONG PEN_PRIMARY = 1;
+    const LONG DRAWMODE_JAM1 = 0;
+    const LONG DRAWMODE_JAM2 = 1;
+    const LONG TEXT_X = 40;
+    const LONG ROW0_Y = 90;
+    const LONG ROW_STEP_Y = 30;
 
-    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, 90, 40, Global_STR_SAVE_ALL_TO_DISK);
-    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, 120, 40, Global_STR_SAVE_DATA_TO_DISK);
-    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, 150, 40, Global_STR_LOAD_TEXT_ADS_FROM_DISK);
-    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, 180, 40, Global_STR_REBOOT_COMPUTER);
+    _LVOSetAPen(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, PEN_PRIMARY);
+    _LVOSetDrMd(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, DRAWMODE_JAM1);
 
-    _LVOSetDrMd(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, 1);
+    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, ROW0_Y + (ROW_STEP_Y * 0), TEXT_X, Global_STR_SAVE_ALL_TO_DISK);
+    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, ROW0_Y + (ROW_STEP_Y * 1), TEXT_X, Global_STR_SAVE_DATA_TO_DISK);
+    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, ROW0_Y + (ROW_STEP_Y * 2), TEXT_X, Global_STR_LOAD_TEXT_ADS_FROM_DISK);
+    DISPLIB_DisplayTextAtPosition(Global_REF_RASTPORT_1, ROW0_Y + (ROW_STEP_Y * 3), TEXT_X, Global_STR_REBOOT_COMPUTER);
+
+    _LVOSetDrMd(Global_REF_GRAPHICS_LIBRARY, Global_REF_RASTPORT_1, DRAWMODE_JAM2);
 }
