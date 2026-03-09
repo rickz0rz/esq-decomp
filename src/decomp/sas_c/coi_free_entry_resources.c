7f5aa946e562a686926bdfce3f22cb0723546db0
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:38:25 -0400

    Decompilation checkpoint: 1773085105

--- src/decomp/sas_c/coi_free_entry_resources.c
+++ src/decomp/sas_c/coi_free_entry_resources.c
@@ -13,27 +13,32 @@ void COI_FreeSubEntryTableEntries(void *entry);
 void COI_ClearAnimObjectStrings(void *entry);
 void GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(const void *tag, LONG line, void *ptr, LONG bytes);
 
+typedef struct COI_Entry {
+    UBYTE pad0[COI_ENTRY_AUX_OFFSET];
+    void *auxPtr;
+} COI_Entry;
+
 void COI_FreeEntryResources(void *entry)
 {
-    UBYTE *e;
-    UBYTE *aux;
+    COI_Entry *e;
+    void *aux;
 
-    e = (UBYTE *)entry;
-    if (e == (UBYTE *)0) {
+    e = (COI_Entry *)entry;
+    if (e == (COI_Entry *)0) {
         return;
     }
 
-    aux = *(UBYTE **)(e + COI_ENTRY_AUX_OFFSET);
+    aux = e->auxPtr;
     COI_FreeSubEntryTableEntries((void *)e);
     COI_ClearAnimObjectStrings((void *)e);
 
-    if (aux != (UBYTE *)0) {
+    if (aux != (void *)0) {
         GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(
             Global_STR_COI_C_3,
             COI_AUX_DEALLOC_LINE,
-            (void *)aux,
+            aux,
             COI_AUX_DEALLOC_BYTES);
     }
 
-    *(void **)(e + COI_ENTRY_AUX_OFFSET) = (void *)0;
+    e->auxPtr = (void *)0;
 }
