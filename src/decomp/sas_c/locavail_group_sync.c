ddbc18aaeb53457617ba9d63876adb9d777819ed
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:20:58 -0400

    Decompilation checkpoint: 1773084058

--- src/decomp/sas_c/locavail_group_sync.c
+++ src/decomp/sas_c/locavail_group_sync.c
@@ -1,9 +1,13 @@
 typedef unsigned char UBYTE;
 
+typedef struct LOCAVAIL_FilterState {
+    UBYTE groupCode;
+} LOCAVAIL_FilterState;
+
 extern UBYTE TEXTDISP_PrimaryGroupCode;
 extern UBYTE TEXTDISP_SecondaryGroupCode;
-extern UBYTE LOCAVAIL_PrimaryFilterState[];
-extern UBYTE LOCAVAIL_SecondaryFilterState[];
+extern LOCAVAIL_FilterState LOCAVAIL_PrimaryFilterState;
+extern LOCAVAIL_FilterState LOCAVAIL_SecondaryFilterState;
 
 extern void LOCAVAIL_CopyFilterStateStructRetainRefs(void *dstState, const void *srcState);
 extern void LOCAVAIL_FreeResourceChain(void *state);
@@ -11,19 +15,19 @@ extern void LOCAVAIL_ResetFilterCursorState(void *statePtr);
 
 void LOCAVAIL_SyncSecondaryFilterForCurrentGroup(void)
 {
-    if (LOCAVAIL_SecondaryFilterState[0] != TEXTDISP_SecondaryGroupCode &&
-        LOCAVAIL_PrimaryFilterState[0] == TEXTDISP_PrimaryGroupCode) {
-        LOCAVAIL_FreeResourceChain(LOCAVAIL_SecondaryFilterState);
-        LOCAVAIL_CopyFilterStateStructRetainRefs(LOCAVAIL_SecondaryFilterState, LOCAVAIL_PrimaryFilterState);
-        LOCAVAIL_SecondaryFilterState[0] = TEXTDISP_SecondaryGroupCode;
+    if (LOCAVAIL_SecondaryFilterState.groupCode != TEXTDISP_SecondaryGroupCode &&
+        LOCAVAIL_PrimaryFilterState.groupCode == TEXTDISP_PrimaryGroupCode) {
+        LOCAVAIL_FreeResourceChain(&LOCAVAIL_SecondaryFilterState);
+        LOCAVAIL_CopyFilterStateStructRetainRefs(&LOCAVAIL_SecondaryFilterState, &LOCAVAIL_PrimaryFilterState);
+        LOCAVAIL_SecondaryFilterState.groupCode = TEXTDISP_SecondaryGroupCode;
     }
 }
 
 void LOCAVAIL_RebuildFilterStateFromCurrentGroup(void)
 {
-    LOCAVAIL_FreeResourceChain(LOCAVAIL_PrimaryFilterState);
-    LOCAVAIL_CopyFilterStateStructRetainRefs(LOCAVAIL_PrimaryFilterState, LOCAVAIL_SecondaryFilterState);
-    LOCAVAIL_FreeResourceChain(LOCAVAIL_SecondaryFilterState);
-    LOCAVAIL_SecondaryFilterState[0] = (UBYTE)(TEXTDISP_PrimaryGroupCode - 1);
-    LOCAVAIL_ResetFilterCursorState(LOCAVAIL_PrimaryFilterState);
+    LOCAVAIL_FreeResourceChain(&LOCAVAIL_PrimaryFilterState);
+    LOCAVAIL_CopyFilterStateStructRetainRefs(&LOCAVAIL_PrimaryFilterState, &LOCAVAIL_SecondaryFilterState);
+    LOCAVAIL_FreeResourceChain(&LOCAVAIL_SecondaryFilterState);
+    LOCAVAIL_SecondaryFilterState.groupCode = (UBYTE)(TEXTDISP_PrimaryGroupCode - 1);
+    LOCAVAIL_ResetFilterCursorState(&LOCAVAIL_PrimaryFilterState);
 }
