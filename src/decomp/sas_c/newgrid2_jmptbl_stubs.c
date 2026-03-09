ba5bcba9746d9ee4783db911074c54906a703e93
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 19:43:13 -0400

    Decompilation checkpoint: 1773099793

--- src/decomp/sas_c/newgrid2_jmptbl_stubs.c
+++ src/decomp/sas_c/newgrid2_jmptbl_stubs.c
@@ -1,3 +1,5 @@
+typedef signed long LONG;
+
 extern void COI_SelectAnimFieldPointer(void);
 extern void DISPTEXT_SetCurrentLineIndex(void);
 extern void DISPTEXT_LayoutAndAppendToBuffer(void);
@@ -16,10 +18,10 @@ extern void DISPTEXT_RenderCurrentLine(void);
 extern void COI_ProcessEntrySelectionState(void);
 extern void CLEANUP_FormatClockFormatEntry(void);
 extern void ESQ_GetHalfHourSlotIndex(void);
-extern void STR_SkipClass3Chars(void);
+extern char *STR_SkipClass3Chars(char *s);
 extern void STRING_AppendN(void);
 extern void ESQDISP_ComputeScheduleOffsetForRow(void);
-extern void PARSE_ReadSignedLongSkipClass3_Alt(void);
+extern LONG PARSE_ReadSignedLongSkipClass3_Alt(char *s);
 extern void CLEANUP_TestEntryFlagYAndBit1(void);
 extern void DISPTEXT_IsCurrentLineLast(void);
 extern void DISPTEXT_IsLastLineSelected(void);
@@ -30,6 +32,8 @@ extern void DISPTEXT_MeasureCurrentLineLength(void);
 extern void DISPTEXT_SetLayoutParams(void);
 extern void DISPTEXT_HasMultipleLines(void);
 extern void BEVEL_DrawHorizontalBevel(void);
+extern LONG MATH_DivS32(LONG a, LONG b);
+extern LONG MATH_Mulu32(LONG a, LONG b);
 
 void NEWGRID2_JMPTBL_COI_SelectAnimFieldPointer(void){COI_SelectAnimFieldPointer();}
 void NEWGRID2_JMPTBL_DISPTEXT_SetCurrentLineIndex(void){DISPTEXT_SetCurrentLineIndex();}
@@ -49,10 +53,10 @@ void NEWGRID2_JMPTBL_DISPTEXT_RenderCurrentLine(void){DISPTEXT_RenderCurrentLine
 void NEWGRID2_JMPTBL_COI_ProcessEntrySelectionState(void){COI_ProcessEntrySelectionState();}
 void NEWGRID2_JMPTBL_CLEANUP_FormatClockFormatEntry(void){CLEANUP_FormatClockFormatEntry();}
 void NEWGRID2_JMPTBL_ESQ_GetHalfHourSlotIndex(void){ESQ_GetHalfHourSlotIndex();}
-void NEWGRID2_JMPTBL_STR_SkipClass3Chars(void){STR_SkipClass3Chars();}
+char *NEWGRID2_JMPTBL_STR_SkipClass3Chars(char *s){return STR_SkipClass3Chars(s);}
 void NEWGRID2_JMPTBL_STRING_AppendN(void){STRING_AppendN();}
 void NEWGRID2_JMPTBL_ESQDISP_ComputeScheduleOffsetForRow(void){ESQDISP_ComputeScheduleOffsetForRow();}
-void NEWGRID2_JMPTBL_PARSE_ReadSignedLongSkipClass3_Alt(void){PARSE_ReadSignedLongSkipClass3_Alt();}
+LONG NEWGRID2_JMPTBL_PARSE_ReadSignedLongSkipClass3_Alt(char *s){return PARSE_ReadSignedLongSkipClass3_Alt(s);}
 void NEWGRID2_JMPTBL_CLEANUP_TestEntryFlagYAndBit1(void){CLEANUP_TestEntryFlagYAndBit1();}
 void NEWGRID2_JMPTBL_DISPTEXT_IsCurrentLineLast(void){DISPTEXT_IsCurrentLineLast();}
 void NEWGRID2_JMPTBL_DISPTEXT_IsLastLineSelected(void){DISPTEXT_IsLastLineSelected();}
@@ -63,3 +67,5 @@ void NEWGRID2_JMPTBL_DISPTEXT_MeasureCurrentLineLength(void){DISPTEXT_MeasureCur
 void NEWGRID2_JMPTBL_DISPTEXT_SetLayoutParams(void){DISPTEXT_SetLayoutParams();}
 void NEWGRID2_JMPTBL_DISPTEXT_HasMultipleLines(void){DISPTEXT_HasMultipleLines();}
 void NEWGRID2_JMPTBL_BEVEL_DrawHorizontalBevel(void){BEVEL_DrawHorizontalBevel();}
+LONG NEWGRID2_JMPTBL_MATH_DivS32(LONG a, LONG b){return MATH_DivS32(a, b);}
+LONG NEWGRID2_JMPTBL_MATH_Mulu32(LONG a, LONG b){return MATH_Mulu32(a, b);}
