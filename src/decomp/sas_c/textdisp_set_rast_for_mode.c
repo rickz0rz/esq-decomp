c8ce8786ad331399930115ed482f8efa3a0e08a9
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:22:46 -0400

    Decompilation checkpoint: 1773084166

--- src/decomp/sas_c/textdisp_set_rast_for_mode.c
+++ src/decomp/sas_c/textdisp_set_rast_for_mode.c
@@ -13,8 +13,15 @@ extern void WDISP_JMPTBL_ESQIFF_RunCopperDropTransition(void);
 extern LONG TLIBA3_BuildDisplayContextForViewMode(LONG viewMode, LONG a1, LONG a2);
 extern LONG _LVOSetRast(void *gfxBase, void *rastPort, LONG pen);
 
+typedef struct TEXTDISP_DisplayContext {
+    UBYTE pad0[2];
+    UBYTE rastPort[1];
+} TEXTDISP_DisplayContext;
+
 void TEXTDISP_SetRastForMode(WORD modeIndex)
 {
+    TEXTDISP_DisplayContext *context;
+
     WDISP_AccumulatorFlushPending = 0;
     WDISP_JMPTBL_ESQIFF_RunCopperDropTransition();
 
@@ -28,9 +35,10 @@ void TEXTDISP_SetRastForMode(WORD modeIndex)
     WDISP_PaletteTriplesRBase[0] = WDISP_PaletteTriplesRBase[(LONG)modeIndex * 3];
     WDISP_PaletteTriplesGBase[0] = WDISP_PaletteTriplesGBase[(LONG)modeIndex * 3];
     WDISP_PaletteTriplesBBase[0] = WDISP_PaletteTriplesBBase[(LONG)modeIndex * 3];
+    context = (TEXTDISP_DisplayContext *)WDISP_DisplayContextBase;
 
     _LVOSetRast(
         Global_REF_GRAPHICS_LIBRARY,
-        (void *)((UBYTE *)WDISP_DisplayContextBase + 2),
+        (void *)context->rastPort,
         (LONG)modeIndex);
 }
