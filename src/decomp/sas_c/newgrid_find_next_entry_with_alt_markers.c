87e3e4c75f9af459f2e5455fd8fceb5c271e7807
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 16:04:22 -0400

    Decompilation checkpoint: 1773086662

--- src/decomp/sas_c/newgrid_find_next_entry_with_alt_markers.c
+++ src/decomp/sas_c/newgrid_find_next_entry_with_alt_markers.c
@@ -15,7 +15,7 @@ typedef struct NEWGRID_AuxData {
     UBYTE pad0[7];
     UBYTE rowFlags[49];
     UBYTE pad1[0x38 - 0x38];
-    void *titleTable[49];
+    UBYTE *titleTable[49];
 } NEWGRID_AuxData;
 
 extern UBYTE TEXTDISP_PrimaryGroupPresentFlag;
