3fbc276320adf49c33895627bf15f64e826f988d
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 14:01:03 -0400

    Decompilation checkpoint: 1773079263

--- src/decomp/sas_c/textdisp2_reset_selection_and_refresh.c
+++ src/decomp/sas_c/textdisp2_reset_selection_and_refresh.c
@@ -9,6 +9,6 @@ extern void TEXTDISP2_JMPTBL_ESQIFF_PlayNextExternalAssetFrame(LONG value);
 void TEXTDISP_ResetSelectionAndRefresh(void)
 {
     SCRIPT_UpdateSerialShadowFromCtrlByte(3);
-    TEXTDISP_CurrentMatchIndex = -1;
-    TEXTDISP2_JMPTBL_ESQIFF_PlayNextExternalAssetFrame(0);
+    TEXTDISP_CurrentMatchIndex = (WORD)-1;
+    TEXTDISP2_JMPTBL_ESQIFF_PlayNextExternalAssetFrame((LONG)0);
 }
