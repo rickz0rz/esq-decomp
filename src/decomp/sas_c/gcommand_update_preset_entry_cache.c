be1b5003f98f1524b0292f20559ae47006a6b280
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:02:23 -0400

    Decompilation checkpoint: 1773082943

--- src/decomp/sas_c/gcommand_update_preset_entry_cache.c
+++ src/decomp/sas_c/gcommand_update_preset_entry_cache.c
@@ -1,21 +1,31 @@
 typedef signed long LONG;
 typedef unsigned char UBYTE;
 
+typedef struct GCOMMAND_PresetEntry {
+    UBYTE pad0[32];
+    LONG value32;
+    LONG cache36[4];
+    UBYTE values55[4];
+} GCOMMAND_PresetEntry;
+
 extern LONG GCOMMAND_ComputePresetIncrement(LONG value, LONG step);
 
 void GCOMMAND_UpdatePresetEntryCache(UBYTE *preset_base)
 {
-    LONG value = *(LONG *)(preset_base + 32);
+    GCOMMAND_PresetEntry *entryView;
+    LONG value;
     LONG i;
     LONG *dst;
     UBYTE *src;
 
+    entryView = (GCOMMAND_PresetEntry *)preset_base;
+    value = entryView->value32;
     if (value < 0) {
         return;
     }
 
-    dst = (LONG *)(preset_base + 36);
-    src = preset_base + 55;
+    dst = entryView->cache36;
+    src = entryView->values55;
 
     for (i = 0; i < 4; ++i) {
         *dst++ = GCOMMAND_ComputePresetIncrement((LONG)(*src++), value);
