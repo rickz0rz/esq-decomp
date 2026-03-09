24025e04516cf8d6b1bed32480c97980d01cf7ce
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 16:25:23 -0400

    Decompilation checkpoint: 1773087923

--- src/decomp/sas_c/newgrid_draw_wrapped_text.c
+++ src/decomp/sas_c/newgrid_draw_wrapped_text.c
@@ -6,27 +6,27 @@ extern const UBYTE NEWGRID_WrapReturnSpacer;
 extern const UBYTE Global_STR_SINGLE_SPACE;
 extern void *Global_REF_GRAPHICS_LIBRARY;
 
-extern UBYTE *NEWGRID2_JMPTBL_STR_SkipClass3Chars(UBYTE *s);
-extern UBYTE *NEWGRID_JMPTBL_STR_CopyUntilAnyDelimN(UBYTE *src, UBYTE *dst, LONG limit, UBYTE *delims);
-extern LONG _LVOTextLength(void *rastport, const UBYTE *text, LONG len);
+extern char *NEWGRID2_JMPTBL_STR_SkipClass3Chars(char *s);
+extern char *NEWGRID_JMPTBL_STR_CopyUntilAnyDelimN(char *src, char *dst, LONG limit, const UBYTE *delims);
+extern LONG _LVOTextLength(void *rastport, const char *text, LONG len);
 extern void _LVOMove(void *rastport, LONG x, LONG y);
-extern void _LVOText(void *rastport, const UBYTE *text, LONG len);
+extern void _LVOText(void *rastport, const char *text, LONG len);
 
-UBYTE *NEWGRID_DrawWrappedText(void *rastport, LONG x, LONG y, LONG max_width, UBYTE *text, LONG draw_enable)
+char *NEWGRID_DrawWrappedText(void *rastport, LONG x, LONG y, LONG max_width, char *text, LONG draw_enable)
 {
-    UBYTE word_buf[50];
+    char word_buf[50];
     LONG drawn_width;
     LONG trim_len;
-    UBYTE *next;
-    UBYTE *word_start;
+    char *next;
+    char *word_start;
     LONG space_w;
 
     drawn_width = 0;
     trim_len = 0;
-    next = (UBYTE *)0;
-    word_start = (UBYTE *)0;
+    next = (char *)0;
+    word_start = (char *)0;
 
-    if (text != (UBYTE *)0) {
+    if (text != (char *)0) {
         next = NEWGRID2_JMPTBL_STR_SkipClass3Chars(text);
     }
 
@@ -34,17 +34,17 @@ UBYTE *NEWGRID_DrawWrappedText(void *rastport, LONG x, LONG y, LONG max_width, U
     space_w = _LVOTextLength(rastport, &Global_STR_SINGLE_SPACE, 1);
     _LVOMove(rastport, x, y);
 
-    while (next != (UBYTE *)0) {
+    while (next != (char *)0) {
         LONG word_len;
         LONG word_w;
         LONG rem_w;
-        UBYTE *scan;
+        char *scan;
 
         next = NEWGRID_JMPTBL_STR_CopyUntilAnyDelimN(next, word_buf, 50, &NEWGRID_WrapWordSpacer);
         next = NEWGRID2_JMPTBL_STR_SkipClass3Chars(next);
 
         if (word_buf[0] == 0) {
-            return (UBYTE *)0;
+            return (char *)0;
         }
 
         scan = word_buf;
