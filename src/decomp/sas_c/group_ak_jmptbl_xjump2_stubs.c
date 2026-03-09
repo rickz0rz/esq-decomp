6fbae0b7384b96e5dd7fa902ba17adafcfd71ffb
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 19:52:55 -0400

    Decompilation checkpoint: 1773100375

--- src/decomp/sas_c/group_ak_jmptbl_xjump2_stubs.c
+++ src/decomp/sas_c/group_ak_jmptbl_xjump2_stubs.c
@@ -1,9 +1,9 @@
 extern void ESQ_SetCopperEffect_AllOn(void);
 extern void GCOMMAND_CopyGfxToWorkIfAvailable(void);
 extern void SCRIPT_AssertCtrlLineNow(void);
-extern void TLIBA3_DrawViewModeGuides(void);
+extern void TLIBA3_DrawViewModeGuides(char *rp);
 
 void GROUP_AK_JMPTBL_ESQ_SetCopperEffect_AllOn(void){ESQ_SetCopperEffect_AllOn();}
 void GROUP_AK_JMPTBL_GCOMMAND_CopyGfxToWorkIfAvailable(void){GCOMMAND_CopyGfxToWorkIfAvailable();}
 void GROUP_AK_JMPTBL_SCRIPT_AssertCtrlLineNow(void){SCRIPT_AssertCtrlLineNow();}
-void GROUP_AK_JMPTBL_TLIBA3_DrawViewModeGuides(void){TLIBA3_DrawViewModeGuides();}
+void GROUP_AK_JMPTBL_TLIBA3_DrawViewModeGuides(char *rp){TLIBA3_DrawViewModeGuides(rp);}
