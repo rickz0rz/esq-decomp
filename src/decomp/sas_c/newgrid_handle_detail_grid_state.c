a8e423af82013a62158ea297cdd47d5afdd4b0fd
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 14:22:41 -0400

    Decompilation checkpoint: 1773080561

--- src/decomp/sas_c/newgrid_handle_detail_grid_state.c
+++ src/decomp/sas_c/newgrid_handle_detail_grid_state.c
@@ -2,6 +2,19 @@ typedef signed long LONG;
 typedef signed short WORD;
 typedef unsigned char UBYTE;
 
+typedef struct NEWGRID_Entry {
+    UBYTE pad0[1];
+    UBYTE shortText[18];
+    UBYTE channelText[1];
+} NEWGRID_Entry;
+
+typedef struct NEWGRID_Context {
+    UBYTE pad0[32];
+    LONG selectedState;
+    UBYTE pad1[24];
+    UBYTE rastPort[1];
+} NEWGRID_Context;
+
 extern LONG NEWGRID_DetailGridStateLatch;
 extern LONG GCOMMAND_MplexDetailLayoutPen;
 extern UBYTE GCOMMAND_MplexDetailLayoutFlag;
@@ -19,8 +32,9 @@ extern LONG NEWGRID2_JMPTBL_DISPTEXT_ComputeVisibleLineCount(LONG mode);
 
 LONG NEWGRID_HandleDetailGridState(UBYTE *ctx, LONG keyIndex, WORD rowIndex)
 {
-    UBYTE *entry;
+    NEWGRID_Entry *entry;
     UBYTE *aux;
+    NEWGRID_Context *ctxView;
     LONG nextState;
     UBYTE channelRow[58];
 
@@ -32,8 +46,9 @@ LONG NEWGRID_HandleDetailGridState(UBYTE *ctx, LONG keyIndex, WORD rowIndex)
         return NEWGRID_DetailGridStateLatch;
     }
 
+    ctxView = (NEWGRID_Context *)ctx;
     if (NEWGRID_DetailGridStateLatch == 4) {
-        rowIndex = (WORD)NEWGRID_UpdatePresetEntry(&entry, &aux, rowIndex, keyIndex);
+        rowIndex = (WORD)NEWGRID_UpdatePresetEntry((UBYTE **)&entry, &aux, rowIndex, keyIndex);
         if (entry == 0 || aux == 0) {
             return NEWGRID_DetailGridStateLatch;
         }
@@ -41,14 +56,14 @@ LONG NEWGRID_HandleDetailGridState(UBYTE *ctx, LONG keyIndex, WORD rowIndex)
         NEWGRID2_JMPTBL_DISPTEXT_SetLayoutParams(612, 20, GCOMMAND_MplexDetailLayoutPen);
 
         if (GCOMMAND_MplexDetailLayoutFlag == (UBYTE)'N') {
-            NEWGRID_DrawGridEntry(ctx + 60, entry, aux, (LONG)rowIndex, 2, 1, 4);
+            NEWGRID_DrawGridEntry(ctxView->rastPort, (UBYTE *)entry, aux, (LONG)rowIndex, 2, 1, 4);
         } else {
-            NEWGRID_DrawGridEntry(ctx + 60, entry, aux, (LONG)rowIndex, 3, 1, 4);
+            NEWGRID_DrawGridEntry(ctxView->rastPort, (UBYTE *)entry, aux, (LONG)rowIndex, 3, 1, 4);
         }
 
         NEWGRID2_JMPTBL_DISPTEXT_SetCurrentLineIndex(GCOMMAND_MplexDetailInitialLineIndex);
-        PARSEINI_JMPTBL_WDISP_SPrintf(channelRow, NEWGRID_ChannelRowFmt, entry + 19, entry + 1);
-        NEWGRID2_JMPTBL_DISPTEXT_LayoutAndAppendToBuffer(ctx + 60, channelRow);
+        PARSEINI_JMPTBL_WDISP_SPrintf(channelRow, NEWGRID_ChannelRowFmt, entry->channelText, entry->shortText);
+        NEWGRID2_JMPTBL_DISPTEXT_LayoutAndAppendToBuffer(ctxView->rastPort, channelRow);
 
         if (NEWGRID_DrawGridFrameVariant2(ctx) != 0) {
             nextState = 4;
@@ -57,7 +72,7 @@ LONG NEWGRID_HandleDetailGridState(UBYTE *ctx, LONG keyIndex, WORD rowIndex)
         }
 
         NEWGRID_DetailGridStateLatch = nextState;
-        *(LONG *)(ctx + 32) = NEWGRID2_JMPTBL_DISPTEXT_ComputeVisibleLineCount(2);
+        ctxView->selectedState = NEWGRID2_JMPTBL_DISPTEXT_ComputeVisibleLineCount(2);
         return NEWGRID_DetailGridStateLatch;
     }
 
@@ -67,7 +82,7 @@ LONG NEWGRID_HandleDetailGridState(UBYTE *ctx, LONG keyIndex, WORD rowIndex)
         } else {
             nextState = 5;
         }
-        *(LONG *)(ctx + 32) = -1;
+        ctxView->selectedState = -1;
         NEWGRID_DetailGridStateLatch = nextState;
         return NEWGRID_DetailGridStateLatch;
     }
