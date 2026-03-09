152ef9783c971451850dba9e1e8274b923b6e2c0
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 19:04:21 -0400

    Decompilation checkpoint: 1773097461

--- src/decomp/sas_c/unknown39_graphics_bltbitmaprastport.c
+++ src/decomp/sas_c/unknown39_graphics_bltbitmaprastport.c
@@ -1,9 +1,9 @@
 typedef signed long LONG;
 
 extern void *Global_GraphicsLibraryBase_A4;
-extern LONG _LVOBltBitMapRastPort(void *gfxBase, void *bitMap, LONG sx, LONG sy, void *rastPort, LONG dx, LONG dy, LONG width, LONG height, LONG minterm, LONG mask);
+extern LONG _LVOBltBitMapRastPort(void *gfxBase, void *bitMap, LONG sx, LONG sy, char *rastPort, LONG dx, LONG dy, LONG width, LONG height, LONG minterm, LONG mask);
 
-LONG GRAPHICS_BltBitMapRastPort(void *bitMap, LONG sx, LONG sy, void *rastPort, LONG dx, LONG dy, LONG width, LONG height, LONG minterm, LONG mask)
+LONG GRAPHICS_BltBitMapRastPort(void *bitMap, LONG sx, LONG sy, char *rastPort, LONG dx, LONG dy, LONG width, LONG height, LONG minterm, LONG mask)
 {
     return _LVOBltBitMapRastPort(Global_GraphicsLibraryBase_A4, bitMap, sx, sy, rastPort, dx, dy, width, height, minterm, mask);
 }
