20447192e478538f8c6baa55739986961469f71a
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:24:54 -0400

    Decompilation checkpoint: 1773084294

--- src/decomp/sas_c/esqfunc_update_disk_warning_and_refresh_tick.c
+++ src/decomp/sas_c/esqfunc_update_disk_warning_and_refresh_tick.c
@@ -3,10 +3,15 @@ typedef signed short WORD;
 
 typedef unsigned char UBYTE;
 
+typedef struct ESQFUNC_RastPortHolder {
+    UBYTE pad0[2];
+    UBYTE rastPort[1];
+} ESQFUNC_RastPortHolder;
+
 extern LONG DISKIO_Drive0WriteProtectedCode;
 extern LONG DISKIO_DriveMediaStatusCodeTable;
 extern WORD Global_RefreshTickCounter;
-extern UBYTE Global_REF_RASTPORT_2[];
+extern ESQFUNC_RastPortHolder Global_REF_RASTPORT_2;
 extern char Global_STR_DISK_0_IS_WRITE_PROTECTED[];
 extern char Global_STR_YOU_MUST_REINSERT_SYSTEM_DISK_INTO_DRIVE_0[];
 
@@ -20,7 +25,7 @@ void ESQFUNC_UpdateDiskWarningAndRefreshTick(void)
     if (DISKIO_Drive0WriteProtectedCode != 0) {
         Global_RefreshTickCounter = -1;
         ESQFUNC_JMPTBL_TLIBA3_DrawCenteredWrappedTextLines(
-            (void *)(Global_REF_RASTPORT_2 + 2),
+            (void *)Global_REF_RASTPORT_2.rastPort,
             Global_STR_YOU_MUST_REINSERT_SYSTEM_DISK_INTO_DRIVE_0,
             90);
         return;
@@ -29,7 +34,7 @@ void ESQFUNC_UpdateDiskWarningAndRefreshTick(void)
     if (DISKIO_DriveMediaStatusCodeTable != 0) {
         Global_RefreshTickCounter = -1;
         ESQFUNC_JMPTBL_TLIBA3_DrawCenteredWrappedTextLines(
-            (void *)(Global_REF_RASTPORT_2 + 2),
+            (void *)Global_REF_RASTPORT_2.rastPort,
             Global_STR_DISK_0_IS_WRITE_PROTECTED,
             90);
         return;
