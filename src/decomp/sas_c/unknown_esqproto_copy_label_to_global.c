fa7a3fa4b83f76ced588ab7788a0d1dbb012cc5a
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 17:13:49 -0400

    Decompilation checkpoint: 1773090829

--- src/decomp/sas_c/unknown_esqproto_copy_label_to_global.c
+++ src/decomp/sas_c/unknown_esqproto_copy_label_to_global.c
@@ -1,15 +1,15 @@
 typedef unsigned char UBYTE;
 typedef unsigned short UWORD;
 
-extern UBYTE WDISP_StatusListMatchPattern[];
+extern char WDISP_StatusListMatchPattern[];
 
-void ESQPROTO_CopyLabelToGlobal(const UBYTE *in)
+void ESQPROTO_CopyLabelToGlobal(const char *in)
 {
-    UBYTE local[16];
+    char local[16];
     UWORD i = 0;
 
     for (;;) {
-        UBYTE c = *in++;
+        char c = *in++;
         local[i] = c;
         if (c == 0x12 || i >= 10) {
             break;
@@ -20,8 +20,8 @@ void ESQPROTO_CopyLabelToGlobal(const UBYTE *in)
     local[i] = 0;
 
     {
-        const UBYTE *src = local;
-        UBYTE *dst = WDISP_StatusListMatchPattern;
+        const char *src = local;
+        char *dst = WDISP_StatusListMatchPattern;
         while ((*dst++ = *src++) != 0) {
         }
     }
