fa837422c83004cf04650b178411b9af4e798fe0
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 13:14:07 -0400

    Decompilation checkpoint: 1773076447

---
+++ src/decomp/sas_c/tliba3_select_next_view_mode.c
@@ -0,0 +1,17 @@
+typedef signed long LONG;
+
+extern LONG TLIBA1_CurrentViewModeIndex;
+extern LONG WDISP_DisplayContextBase;
+
+extern LONG TLIBA3_BuildDisplayContextForViewMode(LONG viewMode, LONG a1, LONG a2);
+extern void TLIBA3_DrawViewModeOverlay(LONG viewMode);
+
+void TLIBA3_SelectNextViewMode(void)
+{
+    TLIBA1_CurrentViewModeIndex = (TLIBA1_CurrentViewModeIndex + 1) % 9;
+    WDISP_DisplayContextBase = TLIBA3_BuildDisplayContextForViewMode(
+        TLIBA1_CurrentViewModeIndex,
+        0,
+        -1);
+    TLIBA3_DrawViewModeOverlay(TLIBA1_CurrentViewModeIndex);
+}
