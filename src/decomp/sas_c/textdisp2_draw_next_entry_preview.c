9c95df0e5a871552c8cd729d0e968cb85fa8f20b
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 13:59:07 -0400

    Decompilation checkpoint: 1773079147

--- src/decomp/sas_c/textdisp2_draw_next_entry_preview.c
+++ src/decomp/sas_c/textdisp2_draw_next_entry_preview.c
@@ -1,5 +1,11 @@
 typedef signed long LONG;
 typedef unsigned short UWORD;
+typedef unsigned char UBYTE;
+
+typedef struct LADFUNC_PreviewEntry {
+    UBYTE pad0[4];
+    UWORD state;
+} LADFUNC_PreviewEntry;
 
 extern UWORD LADFUNC_EntryCount;
 extern void *LADFUNC_EntryPtrTable[];
@@ -9,17 +15,14 @@ extern void TEXTDISP2_JMPTBL_LADFUNC_DrawEntryPreview(LONG entryIndex);
 
 void TEXTDISP_DrawNextEntryPreview(void)
 {
-    void *entry;
-
     for (;;) {
-        entry = LADFUNC_EntryPtrTable[(LONG)LADFUNC_EntryCount];
-        if (*(UWORD *)((unsigned char *)entry + 4) == 1) {
+        if (((LADFUNC_PreviewEntry *)LADFUNC_EntryPtrTable[(LONG)(UWORD)LADFUNC_EntryCount])->state == 1) {
             break;
         }
 
-        LADFUNC_EntryCount = (UWORD)MATH_DivS32((LONG)LADFUNC_EntryCount + 1, 46);
+        LADFUNC_EntryCount = (UWORD)MATH_DivS32((LONG)(UWORD)LADFUNC_EntryCount + 1, 46);
     }
 
-    TEXTDISP2_JMPTBL_LADFUNC_DrawEntryPreview((LONG)LADFUNC_EntryCount);
-    LADFUNC_EntryCount = (UWORD)(LADFUNC_EntryCount + 1);
+    TEXTDISP2_JMPTBL_LADFUNC_DrawEntryPreview((LONG)(UWORD)LADFUNC_EntryCount);
+    LADFUNC_EntryCount = (UWORD)((UWORD)LADFUNC_EntryCount + 1);
 }
