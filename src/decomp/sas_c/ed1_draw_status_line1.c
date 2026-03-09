7b90920a0ad4c498424b22cce24b351f02588f6b
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:23:11 -0400

    Decompilation checkpoint: 1773084191

--- src/decomp/sas_c/ed1_draw_status_line1.c
+++ src/decomp/sas_c/ed1_draw_status_line1.c
@@ -9,14 +9,21 @@ extern const char ED2_FMT_SCRSPD_PCT_D[];
 extern LONG GROUP_AM_JMPTBL_WDISP_SPrintf(char *dst, const char *fmt, ...);
 extern LONG ESQFUNC_JMPTBL_TLIBA3_DrawCenteredWrappedTextLines(void *rastPort, const char *text, LONG y);
 
+typedef struct ED1_DisplayContext {
+    unsigned char pad0[2];
+    unsigned char rastPort[1];
+} ED1_DisplayContext;
+
 void ED1_DrawStatusLine1(void)
 {
+    ED1_DisplayContext *context;
     char statusLine[41];
 
     GROUP_AM_JMPTBL_WDISP_SPrintf(statusLine, ED2_FMT_SCRSPD_PCT_D, (LONG)ESQPARS2_StateIndex);
+    context = (ED1_DisplayContext *)WDISP_DisplayContextBase;
 
     ESQFUNC_JMPTBL_TLIBA3_DrawCenteredWrappedTextLines(
-        (void *)((unsigned char *)WDISP_DisplayContextBase + 2),
+        (void *)context->rastPort,
         statusLine,
         210
     );
