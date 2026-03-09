9f9338fe93422984ed2de62dd847c3efe39b544d
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:06:05 -0400

    Decompilation checkpoint: 1773083165

--- src/decomp/sas_c/tliba3_get_view_mode_height.c
+++ src/decomp/sas_c/tliba3_get_view_mode_height.c
@@ -2,13 +2,18 @@ typedef signed long LONG;
 typedef signed short WORD;
 typedef unsigned char UBYTE;
 
+typedef struct TLIBA3_ViewModeRuntimeEntry {
+    UBYTE pad0[4];
+    WORD height4;
+} TLIBA3_ViewModeRuntimeEntry;
+
 extern UBYTE TLIBA3_VmArrayRuntimeTable[];
 extern LONG MATH_Mulu32(LONG a, LONG b);
 
 LONG TLIBA3_GetViewModeHeight(LONG viewModeIndex)
 {
-    UBYTE *viewMode;
+    TLIBA3_ViewModeRuntimeEntry *viewMode;
 
-    viewMode = TLIBA3_VmArrayRuntimeTable + MATH_Mulu32(viewModeIndex, 154);
-    return (LONG)*(WORD *)(viewMode + 4);
+    viewMode = (TLIBA3_ViewModeRuntimeEntry *)(TLIBA3_VmArrayRuntimeTable + MATH_Mulu32(viewModeIndex, 154));
+    return (LONG)viewMode->height4;
 }
--- src/decomp/sas_c/tliba3_get_view_mode_rast_port.c
+++ src/decomp/sas_c/tliba3_get_view_mode_rast_port.c
@@ -1,13 +1,18 @@
 typedef signed long LONG;
 typedef unsigned char UBYTE;
 
+typedef struct TLIBA3_ViewModeRuntimeEntry {
+    UBYTE pad0[10];
+    void *rastPort10;
+} TLIBA3_ViewModeRuntimeEntry;
+
 extern UBYTE TLIBA3_VmArrayRuntimeTable[];
 extern LONG MATH_Mulu32(LONG a, LONG b);
 
 void *TLIBA3_GetViewModeRastPort(LONG viewModeIndex)
 {
-    UBYTE *viewMode;
+    TLIBA3_ViewModeRuntimeEntry *viewMode;
 
-    viewMode = TLIBA3_VmArrayRuntimeTable + MATH_Mulu32(viewModeIndex, 154);
-    return (void *)(viewMode + 10);
+    viewMode = (TLIBA3_ViewModeRuntimeEntry *)(TLIBA3_VmArrayRuntimeTable + MATH_Mulu32(viewModeIndex, 154));
+    return viewMode->rastPort10;
 }
