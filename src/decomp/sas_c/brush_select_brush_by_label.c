964508f928315a0029101de4de7c86fdb2783a6f
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:47:32 -0400

    Decompilation checkpoint: 1773085652

--- src/decomp/sas_c/brush_select_brush_by_label.c
+++ src/decomp/sas_c/brush_select_brush_by_label.c
@@ -3,10 +3,8 @@ typedef long LONG;
 
 enum {
     BRUSH_NULL_BYTE = 0,
-    BRUSH_COMPARE_EQUAL = 0,
     BRUSH_LABEL_COMPARE_LEN = 2,
     BRUSH_CODE_BUFFER_LEN = 3,
-    BRUSH_CODE_TERMINATOR_INDEX = 2,
     BRUSH_NODE_LABEL_OFFSET = 0x21,
     BRUSH_NODE_NEXT_OFFSET = 368
 };
@@ -23,11 +21,17 @@ extern const UBYTE BRUSH_STR_FALLBACK_DITHER[];
 
 LONG GROUP_AA_JMPTBL_STRING_CompareN(const void *lhs, const void *rhs, LONG n);
 void GROUP_AG_JMPTBL_STRING_CopyPadNul(void *dst, const void *src, LONG n);
-void *BRUSH_FindBrushByPredicate(void *key, void *list_head_ptr);
+void *BRUSH_FindBrushByPredicate(const void *key, void *list_head_ptr);
+
+typedef struct BRUSH_Node {
+    UBYTE pad0[BRUSH_NODE_LABEL_OFFSET];
+    UBYTE label[BRUSH_NODE_NEXT_OFFSET - BRUSH_NODE_LABEL_OFFSET];
+    struct BRUSH_Node *next;
+} BRUSH_Node;
 
 void BRUSH_SelectBrushByLabel(const UBYTE *brushLabel)
 {
-    void *nodeCursor;
+    BRUSH_Node *nodeCursor;
     UBYTE aliasCode[BRUSH_CODE_BUFFER_LEN];
     const UBYTE *labelCursor;
     UBYTE *scratchCursor;
@@ -38,28 +42,25 @@ void BRUSH_SelectBrushByLabel(const UBYTE *brushLabel)
         *scratchCursor++ = *labelCursor;
     } while (*labelCursor++ != (UBYTE)BRUSH_NULL_BYTE);
 
-    nodeCursor = ESQIFF_BrushIniListHead;
+    nodeCursor = (BRUSH_Node *)ESQIFF_BrushIniListHead;
     BRUSH_SelectedNode = (void *)BRUSH_NULL_BYTE;
 
     if (GROUP_AA_JMPTBL_STRING_CompareN(brushLabel, BRUSH_STR_ALIAS_CODE_00, BRUSH_LABEL_COMPARE_LEN) ==
-            BRUSH_COMPARE_EQUAL ||
+            0 ||
         GROUP_AA_JMPTBL_STRING_CompareN(brushLabel, BRUSH_STR_ALIAS_CODE_11, BRUSH_LABEL_COMPARE_LEN) ==
-            BRUSH_COMPARE_EQUAL) {
+            0) {
         GROUP_AG_JMPTBL_STRING_CopyPadNul(aliasCode, BRUSH_STR_ALIAS_CODE_DT, BRUSH_LABEL_COMPARE_LEN);
     } else {
         GROUP_AG_JMPTBL_STRING_CopyPadNul(aliasCode, brushLabel, BRUSH_LABEL_COMPARE_LEN);
     }
-    aliasCode[BRUSH_CODE_TERMINATOR_INDEX] = BRUSH_NULL_BYTE;
+    aliasCode[2] = BRUSH_NULL_BYTE;
 
-    while (nodeCursor != (void *)BRUSH_NULL_BYTE) {
-        UBYTE *node;
-
-        node = (UBYTE *)nodeCursor;
-        if (GROUP_AA_JMPTBL_STRING_CompareN(node + BRUSH_NODE_LABEL_OFFSET, aliasCode, BRUSH_LABEL_COMPARE_LEN) ==
-            BRUSH_COMPARE_EQUAL) {
-            BRUSH_SelectedNode = nodeCursor;
+    while (nodeCursor != (BRUSH_Node *)BRUSH_NULL_BYTE) {
+        if (GROUP_AA_JMPTBL_STRING_CompareN(nodeCursor->label, aliasCode, BRUSH_LABEL_COMPARE_LEN) ==
+            0) {
+            BRUSH_SelectedNode = (void *)nodeCursor;
         }
-        nodeCursor = *(void **)(node + BRUSH_NODE_NEXT_OFFSET);
+        nodeCursor = nodeCursor->next;
     }
 
     if (BRUSH_SelectedNode == (void *)BRUSH_NULL_BYTE) {
