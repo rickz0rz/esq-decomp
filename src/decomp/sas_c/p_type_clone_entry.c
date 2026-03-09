fc80907ada93687ca80ffb0427a09cb19729c23a
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 14:58:45 -0400

    Decompilation checkpoint: 1773082725

--- src/decomp/sas_c/p_type_clone_entry.c
+++ src/decomp/sas_c/p_type_clone_entry.c
@@ -1,6 +1,13 @@
 typedef signed long LONG;
 typedef unsigned char UBYTE;
 
+typedef struct P_TYPE_Entry {
+    UBYTE typeByte;
+    UBYTE subtypeByte;
+    LONG payloadLength;
+    UBYTE *payload;
+} P_TYPE_Entry;
+
 extern void P_TYPE_FreeEntry(UBYTE *entry);
 extern UBYTE *P_TYPE_AllocateEntry(UBYTE typeByte, LONG length, UBYTE *dataPtr);
 
@@ -8,6 +15,7 @@ UBYTE *P_TYPE_CloneEntry(UBYTE *dstEntry, UBYTE *srcEntry)
 {
     UBYTE scratch[104];
     UBYTE *result;
+    P_TYPE_Entry *srcEntryView;
     LONG i;
     LONG len;
 
@@ -15,14 +23,15 @@ UBYTE *P_TYPE_CloneEntry(UBYTE *dstEntry, UBYTE *srcEntry)
     result = (UBYTE *)0;
 
     if (srcEntry != (UBYTE *)0) {
+        srcEntryView = (P_TYPE_Entry *)srcEntry;
         i = 0;
-        len = *(LONG *)(srcEntry + 2);
+        len = srcEntryView->payloadLength;
         while (i < len) {
-            scratch[i] = (*(UBYTE **)(srcEntry + 6))[i];
+            scratch[i] = srcEntryView->payload[i];
             i += 1;
         }
         scratch[i] = 0;
-        result = P_TYPE_AllocateEntry(srcEntry[0], len, scratch);
+        result = P_TYPE_AllocateEntry(srcEntryView->typeByte, len, scratch);
     }
 
     return result;
--- src/decomp/sas_c/p_type_get_subtype_if_type20.c
+++ src/decomp/sas_c/p_type_get_subtype_if_type20.c
@@ -1,14 +1,21 @@
 typedef signed long LONG;
 typedef unsigned char UBYTE;
 
+typedef struct P_TYPE_Entry {
+    UBYTE typeByte;
+    UBYTE subtypeByte;
+} P_TYPE_Entry;
+
 LONG P_TYPE_GetSubtypeIfType20(UBYTE *entry)
 {
     LONG subtype;
+    P_TYPE_Entry *entryView;
 
     subtype = 0;
     if (entry != (UBYTE *)0) {
-        if (entry[0] == (UBYTE)20 && entry[1] != (UBYTE)0) {
-            subtype = (LONG)entry[1];
+        entryView = (P_TYPE_Entry *)entry;
+        if (entryView->typeByte == (UBYTE)20 && entryView->subtypeByte != (UBYTE)0) {
+            subtype = (LONG)entryView->subtypeByte;
         }
     }
 
