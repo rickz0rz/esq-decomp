07c8e6f4781ea875770de09973a2961149b3dda1
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 14:23:24 -0400

    Decompilation checkpoint: 1773080604

--- src/decomp/sas_c/newgrid_handle_showtimes_state.c
+++ src/decomp/sas_c/newgrid_handle_showtimes_state.c
@@ -2,6 +2,20 @@ typedef signed long LONG;
 typedef signed short WORD;
 typedef unsigned char UBYTE;
 
+typedef struct NEWGRID_Context {
+    UBYTE pad0[32];
+    LONG selectedState;
+    UBYTE pad1[24];
+    UBYTE rastPort[1];
+} NEWGRID_Context;
+
+typedef struct NEWGRID_ShowtimesEntryState {
+    void *entryPtr;
+    void *auxPtr;
+    UBYTE pad0[12];
+    WORD rowIndex;
+} NEWGRID_ShowtimesEntryState;
+
 extern LONG NEWGRID_ShowtimesWorkflowStateLatch;
 extern LONG GCOMMAND_PpvShowtimesLayoutPen;
 extern LONG GCOMMAND_PpvShowtimesInitialLineIndex;
@@ -17,6 +31,8 @@ extern LONG NEWGRID2_JMPTBL_DISPTEXT_ComputeVisibleLineCount(LONG mode);
 
 LONG NEWGRID_HandleShowtimesState(UBYTE *gridCtx, UBYTE *entryState)
 {
+    NEWGRID_Context *ctxView;
+    NEWGRID_ShowtimesEntryState *stateView;
     UBYTE text[130];
     LONG row;
     LONG nextState;
@@ -26,27 +42,29 @@ LONG NEWGRID_HandleShowtimesState(UBYTE *gridCtx, UBYTE *entryState)
         return NEWGRID_ShowtimesWorkflowStateLatch;
     }
 
+    ctxView = (NEWGRID_Context *)gridCtx;
+    stateView = (NEWGRID_ShowtimesEntryState *)entryState;
     if (NEWGRID_ShowtimesWorkflowStateLatch == 4) {
-        if (*(LONG *)(entryState + 0) == 0 || *(LONG *)(entryState + 4) == 0) {
+        if (stateView->entryPtr == 0 || stateView->auxPtr == 0) {
             return NEWGRID_ShowtimesWorkflowStateLatch;
         }
 
         NEWGRID2_JMPTBL_DISPTEXT_SetLayoutParams(612, 20, GCOMMAND_PpvShowtimesLayoutPen);
 
-        row = (LONG)(*(WORD *)(entryState + 20));
+        row = (LONG)stateView->rowIndex;
         if (row > 48) {
             row -= 48;
         }
 
         if (GCOMMAND_PpvDetailLayoutFlag == (UBYTE)78) {
-            NEWGRID_DrawGridEntry((UBYTE *)gridCtx + 60, *(void **)(entryState + 0), *(void **)(entryState + 4), row, 2, 1, -1);
+            NEWGRID_DrawGridEntry(ctxView->rastPort, stateView->entryPtr, stateView->auxPtr, row, 2, 1, -1);
         } else {
-            NEWGRID_DrawGridEntry((UBYTE *)gridCtx + 60, *(void **)(entryState + 0), *(void **)(entryState + 4), row, 3, 1, -1);
+            NEWGRID_DrawGridEntry(ctxView->rastPort, stateView->entryPtr, stateView->auxPtr, row, 3, 1, -1);
         }
 
         NEWGRID2_JMPTBL_DISPTEXT_SetCurrentLineIndex(GCOMMAND_PpvShowtimesInitialLineIndex);
         NEWGRID_BuildShowtimesText(gridCtx, entryState, text);
-        NEWGRID2_JMPTBL_DISPTEXT_LayoutAndAppendToBuffer((UBYTE *)gridCtx + 60, text);
+        NEWGRID2_JMPTBL_DISPTEXT_LayoutAndAppendToBuffer(ctxView->rastPort, text);
 
         if (NEWGRID_DrawGridFrameVariant3(gridCtx) != 0) {
             NEWGRID_ShowtimesWorkflowStateLatch = 4;
@@ -54,7 +72,7 @@ LONG NEWGRID_HandleShowtimesState(UBYTE *gridCtx, UBYTE *entryState)
             NEWGRID_ShowtimesWorkflowStateLatch = 5;
         }
 
-        *(LONG *)(gridCtx + 32) = NEWGRID2_JMPTBL_DISPTEXT_ComputeVisibleLineCount(2);
+        ctxView->selectedState = NEWGRID2_JMPTBL_DISPTEXT_ComputeVisibleLineCount(2);
         return NEWGRID_ShowtimesWorkflowStateLatch;
     }
 
@@ -64,7 +82,7 @@ LONG NEWGRID_HandleShowtimesState(UBYTE *gridCtx, UBYTE *entryState)
         } else {
             nextState = 5;
         }
-        *(LONG *)(gridCtx + 32) = -1;
+        ctxView->selectedState = -1;
         NEWGRID_ShowtimesWorkflowStateLatch = nextState;
         return NEWGRID_ShowtimesWorkflowStateLatch;
     }
