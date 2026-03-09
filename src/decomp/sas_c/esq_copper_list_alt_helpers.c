366a4cc075b860790fbd5a2a8f7c255deaa49875
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:56:12 -0400

    Decompilation checkpoint: 1773086172

--- src/decomp/sas_c/esq_copper_list_alt_helpers.c
+++ src/decomp/sas_c/esq_copper_list_alt_helpers.c
@@ -2,10 +2,23 @@ typedef unsigned char UBYTE;
 typedef unsigned short UWORD;
 typedef signed short WORD;
 
-extern UBYTE ESQ_CopperListBannerA[];
-extern UBYTE ESQ_CopperListBannerB[];
-extern UWORD ESQ_BannerPaletteWordsA[];
-extern UWORD ESQ_BannerPaletteWordsB[];
+typedef struct ESQ_CopperListHeader {
+    UBYTE flag0;
+} ESQ_CopperListHeader;
+
+typedef struct ESQ_BannerPaletteSlot {
+    UWORD color;
+    UWORD pad2;
+} ESQ_BannerPaletteSlot;
+
+typedef struct ESQ_BannerPaletteTable {
+    ESQ_BannerPaletteSlot slots[8];
+} ESQ_BannerPaletteTable;
+
+extern ESQ_CopperListHeader ESQ_CopperListBannerA;
+extern ESQ_CopperListHeader ESQ_CopperListBannerB;
+extern ESQ_BannerPaletteTable ESQ_BannerPaletteWordsA;
+extern ESQ_BannerPaletteTable ESQ_BannerPaletteWordsB;
 extern UBYTE WDISP_PaletteTriplesRBase[];
 
 extern UWORD ESQ_DecColorStep(UWORD color);
@@ -13,45 +26,39 @@ extern UWORD ESQ_BumpColorTowardTargets(UWORD color, UBYTE *targets);
 
 void ESQ_ClearCopperListFlags(void)
 {
-    ESQ_CopperListBannerA[0] = 0;
-    ESQ_CopperListBannerB[0] = 0;
+    ESQ_CopperListBannerA.flag0 = 0;
+    ESQ_CopperListBannerB.flag0 = 0;
 }
 
 void ESQ_DecCopperListsAltSkipIndex4(void)
 {
-    WORD off;
     WORD i;
 
-    off = 0;
     for (i = 0; i <= 7; ++i) {
-        if (off != 4) {
-            UWORD value = *(UWORD *)((UBYTE *)ESQ_BannerPaletteWordsA + off);
+        if (i != 1) {
+            UWORD value = ESQ_BannerPaletteWordsA.slots[i].color;
 
             value = ESQ_DecColorStep(value);
-            *(UWORD *)((UBYTE *)ESQ_BannerPaletteWordsA + off) = value;
-            *(UWORD *)((UBYTE *)ESQ_BannerPaletteWordsB + off) = value;
+            ESQ_BannerPaletteWordsA.slots[i].color = value;
+            ESQ_BannerPaletteWordsB.slots[i].color = value;
         }
-        off = (WORD)(off + 4);
     }
 }
 
 void ESQ_IncCopperListsAltSkipIndex4(void)
 {
     UBYTE *targets;
-    WORD off;
     WORD i;
 
     targets = WDISP_PaletteTriplesRBase;
-    off = 0;
     for (i = 0; i <= 7; ++i) {
-        if (off != 4) {
-            UWORD value = *(UWORD *)((UBYTE *)ESQ_BannerPaletteWordsA + off);
+        if (i != 1) {
+            UWORD value = ESQ_BannerPaletteWordsA.slots[i].color;
 
             value = ESQ_BumpColorTowardTargets(value, targets);
-            *(UWORD *)((UBYTE *)ESQ_BannerPaletteWordsA + off) = value;
-            *(UWORD *)((UBYTE *)ESQ_BannerPaletteWordsB + off) = value;
+            ESQ_BannerPaletteWordsA.slots[i].color = value;
+            ESQ_BannerPaletteWordsB.slots[i].color = value;
         }
-        off = (WORD)(off + 4);
         targets += 3;
     }
 }
