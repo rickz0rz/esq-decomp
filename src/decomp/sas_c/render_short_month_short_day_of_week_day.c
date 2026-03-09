32ac2601f2e6326842eea7d9f233627b92762460
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:03:06 -0400

    Decompilation checkpoint: 1773082986

--- src/decomp/sas_c/render_short_month_short_day_of_week_day.c
+++ src/decomp/sas_c/render_short_month_short_day_of_week_day.c
@@ -2,6 +2,13 @@ typedef unsigned short UWORD;
 typedef short WORD;
 typedef long LONG;
 
+typedef struct RENDER_RastPort {
+    LONG textWidth0;
+    LONG bitmap4;
+    UWORD pad8[12];
+    UWORD drawMode32;
+} RENDER_RastPort;
+
 extern LONG Global_REF_RASTPORT_1;
 extern LONG Global_REF_696_400_BITMAP;
 extern char *Global_JMPTBL_SHORT_DAYS_OF_WEEK[];
@@ -22,26 +29,28 @@ void GROUP_AD_JMPTBL_GRAPHICS_BltBitMapRastPort(void);
 
 void RENDER_SHORT_MONTH_SHORT_DAY_OF_WEEK_DAY(void)
 {
+    RENDER_RastPort *rp;
     char text[32];
     char *dow;
     char *mon;
     LONG text_w;
     LONG x;
 
-    *(LONG *)(Global_REF_RASTPORT_1 + 4) = (LONG)&Global_REF_696_400_BITMAP;
+    rp = (RENDER_RastPort *)Global_REF_RASTPORT_1;
+    rp->bitmap4 = (LONG)&Global_REF_696_400_BITMAP;
 
     dow = Global_JMPTBL_SHORT_DAYS_OF_WEEK[(LONG)CLOCK_CurrentDayOfWeekIndex];
     mon = Global_JMPTBL_SHORT_MONTHS[(LONG)CLOCK_CurrentMonthIndex];
     GROUP_AE_JMPTBL_WDISP_SPrintf(text, Global_STR_SHORT_MONTH_SHORT_DAY_OF_WEEK_FORMATTED, dow, mon, (LONG)CLOCK_CurrentDayOfMonth);
 
     _LVOSetAPen();
-    *(UWORD *)(Global_REF_RASTPORT_1 + 32) = (UWORD)(*(UWORD *)(Global_REF_RASTPORT_1 + 32) & 0xFFF7);
+    rp->drawMode32 = (UWORD)(rp->drawMode32 & 0xFFF7);
     _LVOSetDrMd();
     _LVORectFill();
     _LVOSetAPen();
 
     _LVOTextLength();
-    text_w = *(LONG *)(Global_REF_RASTPORT_1);
+    text_w = rp->textWidth0;
     x = (216 - text_w);
     if (x < 0) {
         x++;
