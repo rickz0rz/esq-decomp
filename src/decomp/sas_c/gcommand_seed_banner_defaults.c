8b71ecf442e8baeae7019e594b851dc0348dc097
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:33:15 -0400

    Decompilation checkpoint: 1773084795

--- src/decomp/sas_c/gcommand_seed_banner_defaults.c
+++ src/decomp/sas_c/gcommand_seed_banner_defaults.c
@@ -2,24 +2,37 @@ typedef unsigned char UBYTE;
 typedef signed short WORD;
 
 extern void GCOMMAND_BuildBannerTables(UBYTE arg0, WORD arg1, UBYTE arg2);
-extern UBYTE ESQ_CopperListBannerA[];
-extern UBYTE ESQ_CopperListBannerB[];
+
+typedef struct GCOMMAND_CopperHeader {
+    UBYTE head0;
+    UBYTE head1;
+    WORD terminator2;
+} GCOMMAND_CopperHeader;
+
+typedef struct GCOMMAND_BannerCopperList {
+    GCOMMAND_CopperHeader start;
+    UBYTE pad4[3912];
+    GCOMMAND_CopperHeader end;
+} GCOMMAND_BannerCopperList;
+
+extern GCOMMAND_BannerCopperList ESQ_CopperListBannerA;
+extern GCOMMAND_BannerCopperList ESQ_CopperListBannerB;
 
 void GCOMMAND_SeedBannerDefaults(void)
 {
     GCOMMAND_BuildBannerTables(32, (WORD)0xFFFE, 1);
 
-    ESQ_CopperListBannerA[0] = (UBYTE)31;
-    ESQ_CopperListBannerA[1] = (UBYTE)0xD9;
-    *(WORD *)(ESQ_CopperListBannerA + 2) = (WORD)0xFFFE;
-    ESQ_CopperListBannerA[3916] = (UBYTE)0xF8;
-    ESQ_CopperListBannerA[3917] = (UBYTE)0xD9;
-    *(WORD *)(ESQ_CopperListBannerA + 3918) = (WORD)0xFFFE;
+    ESQ_CopperListBannerA.start.head0 = (UBYTE)31;
+    ESQ_CopperListBannerA.start.head1 = (UBYTE)0xD9;
+    ESQ_CopperListBannerA.start.terminator2 = (WORD)0xFFFE;
+    ESQ_CopperListBannerA.end.head0 = (UBYTE)0xF8;
+    ESQ_CopperListBannerA.end.head1 = (UBYTE)0xD9;
+    ESQ_CopperListBannerA.end.terminator2 = (WORD)0xFFFE;
 
-    ESQ_CopperListBannerB[0] = (UBYTE)31;
-    ESQ_CopperListBannerB[1] = (UBYTE)0xD9;
-    *(WORD *)(ESQ_CopperListBannerB + 2) = (WORD)0xFFFE;
-    ESQ_CopperListBannerB[3916] = (UBYTE)0xF8;
-    ESQ_CopperListBannerB[3917] = (UBYTE)0xD9;
-    *(WORD *)(ESQ_CopperListBannerB + 3918) = (WORD)0xFFFE;
+    ESQ_CopperListBannerB.start.head0 = (UBYTE)31;
+    ESQ_CopperListBannerB.start.head1 = (UBYTE)0xD9;
+    ESQ_CopperListBannerB.start.terminator2 = (WORD)0xFFFE;
+    ESQ_CopperListBannerB.end.head0 = (UBYTE)0xF8;
+    ESQ_CopperListBannerB.end.head1 = (UBYTE)0xD9;
+    ESQ_CopperListBannerB.end.terminator2 = (WORD)0xFFFE;
 }
--- src/decomp/sas_c/gcommand_seed_banner_from_prefs.c
+++ src/decomp/sas_c/gcommand_seed_banner_from_prefs.c
@@ -2,19 +2,26 @@ typedef unsigned char UBYTE;
 typedef signed short WORD;
 
 extern void GCOMMAND_BuildBannerTables(UBYTE arg0, WORD arg1, UBYTE arg2);
-extern UBYTE ESQ_CopperListBannerA[];
-extern UBYTE ESQ_CopperListBannerB[];
 extern WORD CONFIG_BannerCopperHeadByte;
+
+typedef struct GCOMMAND_CopperHeader {
+    UBYTE head0;
+    UBYTE head1;
+    WORD terminator2;
+} GCOMMAND_CopperHeader;
+
+extern GCOMMAND_CopperHeader ESQ_CopperListBannerA;
+extern GCOMMAND_CopperHeader ESQ_CopperListBannerB;
 
 void GCOMMAND_SeedBannerFromPrefs(void)
 {
     GCOMMAND_BuildBannerTables(128, (WORD)0x80FE, 0);
 
-    ESQ_CopperListBannerA[0] = (UBYTE)CONFIG_BannerCopperHeadByte;
-    ESQ_CopperListBannerA[1] = (UBYTE)0xD9;
-    *(WORD *)(ESQ_CopperListBannerA + 2) = (WORD)0xFFFE;
+    ESQ_CopperListBannerA.head0 = (UBYTE)CONFIG_BannerCopperHeadByte;
+    ESQ_CopperListBannerA.head1 = (UBYTE)0xD9;
+    ESQ_CopperListBannerA.terminator2 = (WORD)0xFFFE;
 
-    ESQ_CopperListBannerB[0] = (UBYTE)CONFIG_BannerCopperHeadByte;
-    ESQ_CopperListBannerB[1] = (UBYTE)0xD9;
-    *(WORD *)(ESQ_CopperListBannerB + 2) = (WORD)0xFFFE;
+    ESQ_CopperListBannerB.head0 = (UBYTE)CONFIG_BannerCopperHeadByte;
+    ESQ_CopperListBannerB.head1 = (UBYTE)0xD9;
+    ESQ_CopperListBannerB.terminator2 = (WORD)0xFFFE;
 }
