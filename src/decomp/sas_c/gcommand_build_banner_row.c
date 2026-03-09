4588ae9277a04a7151afd4a527d5153495e753e1
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:55:28 -0400

    Decompilation checkpoint: 1773086128

--- src/decomp/sas_c/gcommand_build_banner_row.c
+++ src/decomp/sas_c/gcommand_build_banner_row.c
@@ -20,6 +20,31 @@ typedef struct GCOMMAND_BannerRowColors {
     UWORD color3;
 } GCOMMAND_BannerRowColors;
 
+typedef struct GCOMMAND_PresetRow {
+    UWORD values[64];
+} GCOMMAND_PresetRow;
+
+typedef struct GCOMMAND_BannerTable {
+    UBYTE pad0[694];
+    UWORD plane1Hi;
+    UWORD pad696;
+    UWORD plane1Lo;
+    UWORD pad700;
+    UWORD plane2Hi;
+    UWORD pad704;
+    UWORD plane2Lo;
+    UBYTE pad708[6];
+    UWORD plane0Hi;
+    UWORD pad716;
+    UWORD plane0Lo;
+    UBYTE pad720[10];
+    UWORD rowPtrHi;
+    UWORD pad732;
+    UWORD rowPtrLo;
+    UBYTE pad736[10];
+    GCOMMAND_BannerRowColors rowColors[17];
+} GCOMMAND_BannerTable;
+
 extern LONG GCOMMAND_PresetWorkEntryTable;
 extern LONG GCOMMAND_PresetWorkEntry1;
 extern LONG GCOMMAND_PresetWorkEntry2;
@@ -37,39 +62,39 @@ extern void GCOMMAND_UpdateBannerRowPointers(void *tablePtr);
 
 static UWORD GCOMMAND_LoadPresetWord(LONG presetBase, LONG valueIndex)
 {
-    UBYTE *p = GCOMMAND_PresetValueTable + (presetBase << 7) + (valueIndex << 1);
-    return *(UWORD *)p;
+    return ((GCOMMAND_PresetRow *)GCOMMAND_PresetValueTable)[presetBase].values[valueIndex];
 }
 
 void GCOMMAND_BuildBannerRow(UBYTE *bitmapPtr, UBYTE *tablePtr, LONG rowIndex, LONG fallbackIndex, LONG baseOffset)
 {
+    GCOMMAND_BannerTable *tableView;
     GCOMMAND_Bitmap *bitmapView;
     LONG selected = (rowIndex > 0) ? rowIndex : fallbackIndex;
     LONG d4 = selected - 1;
     LONG rowOff = rowIndex << 5;
-    UBYTE *table = tablePtr;
     ULONG addr;
     GCOMMAND_BannerRowColors *rowColors;
 
     bitmapView = (GCOMMAND_Bitmap *)bitmapPtr;
+    tableView = (GCOMMAND_BannerTable *)tablePtr;
 
-    addr = (ULONG)(table + rowOff + 744);
-    *(UWORD *)(table + 730) = (UWORD)((addr >> 16) & 0xFFFF);
-    *(UWORD *)(table + 734) = (UWORD)(addr & 0xFFFF);
+    addr = (ULONG)(tablePtr + rowOff + 744);
+    tableView->rowPtrHi = (UWORD)((addr >> 16) & 0xFFFF);
+    tableView->rowPtrLo = (UWORD)(addr & 0xFFFF);
 
     addr = (ULONG)(bitmapView->plane0Ptr + (ULONG)baseOffset);
-    *(UWORD *)(table + 714) = (UWORD)((addr >> 16) & 0xFFFF);
-    *(UWORD *)(table + 718) = (UWORD)(addr & 0xFFFF);
+    tableView->plane0Hi = (UWORD)((addr >> 16) & 0xFFFF);
+    tableView->plane0Lo = (UWORD)(addr & 0xFFFF);
 
     addr = (ULONG)(bitmapView->plane1Ptr + (ULONG)baseOffset);
-    *(UWORD *)(table + 694) = (UWORD)((addr >> 16) & 0xFFFF);
-    *(UWORD *)(table + 698) = (UWORD)(addr & 0xFFFF);
+    tableView->plane1Hi = (UWORD)((addr >> 16) & 0xFFFF);
+    tableView->plane1Lo = (UWORD)(addr & 0xFFFF);
 
     addr = (ULONG)(bitmapView->plane2Ptr + (ULONG)baseOffset);
-    *(UWORD *)(table + 702) = (UWORD)((addr >> 16) & 0xFFFF);
-    *(UWORD *)(table + 706) = (UWORD)(addr & 0xFFFF);
+    tableView->plane2Hi = (UWORD)((addr >> 16) & 0xFFFF);
+    tableView->plane2Lo = (UWORD)(addr & 0xFFFF);
 
-    rowColors = (GCOMMAND_BannerRowColors *)(table + (d4 << 5) + 746);
+    rowColors = &tableView->rowColors[d4];
 
     if (GCOMMAND_BannerRowFallbackOnFirstRowFlag != 0 && d4 <= 0) {
         rowColors->color0 = 0x00F0;
@@ -83,5 +108,5 @@ void GCOMMAND_BuildBannerRow(UBYTE *bitmapPtr, UBYTE *tablePtr, LONG rowIndex, L
         rowColors->color3 = GCOMMAND_LoadPresetWord(GCOMMAND_PresetWorkEntry3, GCOMMAND_PresetWorkEntry3_ValueIndex);
     }
 
-    GCOMMAND_UpdateBannerRowPointers(table);
+    GCOMMAND_UpdateBannerRowPointers(tablePtr);
 }
