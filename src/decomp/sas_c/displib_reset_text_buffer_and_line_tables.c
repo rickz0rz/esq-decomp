55b5fc35e61589038af7aabd6b8f9f4c6b2676b1
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 19:12:01 -0400

    Decompilation checkpoint: 1773097921

--- src/decomp/sas_c/displib_reset_text_buffer_and_line_tables.c
+++ src/decomp/sas_c/displib_reset_text_buffer_and_line_tables.c
@@ -1,5 +1,5 @@
 extern char *DISPTEXT_TextBufferPtr;
-extern char *GROUP_AE_JMPTBL_ESQPARS_ReplaceOwnedString(char *oldText, const char *newText);
+extern char *GROUP_AE_JMPTBL_ESQPARS_ReplaceOwnedString(const char *newText, char *oldText);
 extern void DISPLIB_ResetLineTables(void);
 
 void DISPLIB_ResetTextBufferAndLineTables(void)
@@ -7,6 +7,6 @@ void DISPLIB_ResetTextBufferAndLineTables(void)
     const char *EMPTY_TEXT_PTR = (const char *)0;
 
     DISPTEXT_TextBufferPtr =
-        GROUP_AE_JMPTBL_ESQPARS_ReplaceOwnedString(DISPTEXT_TextBufferPtr, EMPTY_TEXT_PTR);
+        GROUP_AE_JMPTBL_ESQPARS_ReplaceOwnedString(EMPTY_TEXT_PTR, DISPTEXT_TextBufferPtr);
     DISPLIB_ResetLineTables();
 }
