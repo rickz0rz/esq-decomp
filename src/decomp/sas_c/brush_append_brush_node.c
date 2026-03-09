fa7b51a785324af57afee2e4091a60e160c9f989
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 15:46:20 -0400

    Decompilation checkpoint: 1773085580

--- src/decomp/sas_c/brush_append_brush_node.c
+++ src/decomp/sas_c/brush_append_brush_node.c
@@ -3,19 +3,24 @@ enum {
     BRUSH_NODE_NEXT_OFFSET = 368
 };
 
+typedef struct BRUSH_Node {
+    unsigned char pad0[BRUSH_NODE_NEXT_OFFSET];
+    struct BRUSH_Node *next;
+} BRUSH_Node;
+
 void *BRUSH_AppendBrushNode(void *head, void *node)
 {
-    unsigned char *tailCursor;
+    BRUSH_Node *tailCursor;
 
     if (head == (void *)BRUSH_NULL) {
         return node;
     }
 
-    tailCursor = (unsigned char *)head;
-    while (*(void **)(tailCursor + BRUSH_NODE_NEXT_OFFSET) != (void *)BRUSH_NULL) {
-        tailCursor = *(unsigned char **)(tailCursor + BRUSH_NODE_NEXT_OFFSET);
+    tailCursor = (BRUSH_Node *)head;
+    while (tailCursor->next != (BRUSH_Node *)BRUSH_NULL) {
+        tailCursor = tailCursor->next;
     }
 
-    *(void **)(tailCursor + BRUSH_NODE_NEXT_OFFSET) = node;
+    tailCursor->next = (BRUSH_Node *)node;
     return head;
 }
