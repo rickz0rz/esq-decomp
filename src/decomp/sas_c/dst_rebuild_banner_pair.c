275cefda4e680e415dd1c38106902ae74ed3a72a
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 05:15:51 -0400

    Decompilation checkpoint: 1773134151

--- src/decomp/sas_c/dst_rebuild_banner_pair.c
+++ src/decomp/sas_c/dst_rebuild_banner_pair.c
@@ -17,9 +17,9 @@ LONG DST_RebuildBannerPair(void *pair)
     DST_FreeBannerPair(p);
 
     p->primaryBanner = DST_AllocateBannerStruct(p->primaryBanner);
-    if (p->primaryBanner != 0) {
+    if (p->primaryBanner) {
         p->secondaryBanner = DST_AllocateBannerStruct(p->secondaryBanner);
-        if (p->primaryBanner != 0) {
+        if (p->primaryBanner) {
             ok = 1;
         }
     }
