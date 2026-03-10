83d89b2480badb68d0bd9bca8ed196b58095c26b
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 02:46:24 -0400

    Decompilation checkpoint: 1773125184

--- src/decomp/sas_c/tliba2_resolve_entry_window_with_default_range.c
+++ src/decomp/sas_c/tliba2_resolve_entry_window_with_default_range.c
@@ -1,8 +1,26 @@
 typedef signed long LONG;
 
-extern LONG TLIBA2_ResolveEntryWindowAndSlotCount(void *entryTable, void *entryState, LONG entryIndex, LONG outStart, LONG outEnd);
+extern LONG TLIBA2_ResolveEntryWindowAndSlotCount(
+    void *entryTable,
+    void *entryState,
+    LONG entryIndex,
+    LONG *outRange,
+    LONG flags,
+    LONG *outSlotCount,
+    LONG *outStart,
+    LONG *outEnd,
+    LONG wildcardMode);
 
 LONG TLIBA2_ResolveEntryWindowWithDefaultRange(void *entryTable, void *entryState, LONG entryIndex)
 {
-    return TLIBA2_ResolveEntryWindowAndSlotCount(entryTable, entryState, entryIndex, 0, 0);
+    return TLIBA2_ResolveEntryWindowAndSlotCount(
+        entryTable,
+        entryState,
+        entryIndex,
+        (LONG *)0,
+        0,
+        (LONG *)0,
+        (LONG *)0,
+        (LONG *)0,
+        0);
 }
