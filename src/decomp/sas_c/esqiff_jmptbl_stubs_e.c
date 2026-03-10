8440be6f25d7f407afa2e5b2c8081a5831ec8052
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:53:50 -0400

    Decompilation checkpoint: 1773104030

--- src/decomp/sas_c/esqiff_jmptbl_stubs_e.c
+++ src/decomp/sas_c/esqiff_jmptbl_stubs_e.c
@@ -1,9 +1,11 @@
-extern void BRUSH_PopBrushHead(void);
-extern void BRUSH_AllocBrushNode(void);
-extern void ESQ_NoOp_006A(void);
-extern void NEWGRID_ValidateSelectionCode(void);
+typedef signed long LONG;
 
-void ESQIFF_JMPTBL_BRUSH_PopBrushHead(void){BRUSH_PopBrushHead();}
-void ESQIFF_JMPTBL_BRUSH_AllocBrushNode(void){BRUSH_AllocBrushNode();}
+extern void *BRUSH_PopBrushHead(void *head);
+extern void *BRUSH_AllocBrushNode(const char *brushLabel, void *prevTail);
+extern void ESQ_NoOp_006A(void);
+extern void NEWGRID_ValidateSelectionCode(char *gridCtx, LONG code);
+
+void *ESQIFF_JMPTBL_BRUSH_PopBrushHead(void *head){return BRUSH_PopBrushHead(head);}
+void *ESQIFF_JMPTBL_BRUSH_AllocBrushNode(const char *brushLabel, void *prevTail){return BRUSH_AllocBrushNode(brushLabel, prevTail);}
 void ESQIFF_JMPTBL_ESQ_NoOp_006A(void){ESQ_NoOp_006A();}
-void ESQIFF_JMPTBL_NEWGRID_ValidateSelectionCode(void){NEWGRID_ValidateSelectionCode();}
+void ESQIFF_JMPTBL_NEWGRID_ValidateSelectionCode(char *gridCtx, LONG code){NEWGRID_ValidateSelectionCode(gridCtx, code);}
