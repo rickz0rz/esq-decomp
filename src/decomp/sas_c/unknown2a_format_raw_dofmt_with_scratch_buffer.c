3fe12a1819adeb1756a0fcff5eed4ce42888bcdf
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 17:23:33 -0400

    Decompilation checkpoint: 1773177813

--- src/decomp/sas_c/unknown2a_format_raw_dofmt_with_scratch_buffer.c
+++ src/decomp/sas_c/unknown2a_format_raw_dofmt_with_scratch_buffer.c
@@ -2,14 +2,15 @@ typedef signed long LONG;
 
 extern char FORMAT_ScratchBuffer[];
 extern LONG FORMAT_FormatToBuffer2(char *outBuf, char *fmt, void *args);
-extern LONG PARALLEL_RawDoFmtStackArgs(char *buffer);
+extern void PARALLEL_RawDoFmtStackArgs(char *buffer);
 
 LONG FORMAT_RawDoFmtWithScratchBuffer(char *fmt, ...)
 {
     void *args = (void *)(&fmt + 1);
 
     FORMAT_FormatToBuffer2(FORMAT_ScratchBuffer, fmt, args);
-    return PARALLEL_RawDoFmtStackArgs(FORMAT_ScratchBuffer);
+    PARALLEL_RawDoFmtStackArgs(FORMAT_ScratchBuffer);
+    return 0;
 }
 
 void UNKNOWN2A_Stub0(void)
