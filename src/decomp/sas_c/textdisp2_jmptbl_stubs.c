78ad312a7e6757238dbac186351174865062b1e4
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 21:00:17 -0400

    Decompilation checkpoint: 1773104417

--- src/decomp/sas_c/textdisp2_jmptbl_stubs.c
+++ src/decomp/sas_c/textdisp2_jmptbl_stubs.c
@@ -1,9 +1,12 @@
-extern void LOCAVAIL_GetFilterWindowHalfSpan(void);
-extern void LADFUNC_DrawEntryPreview(void);
-extern void ESQIFF_RunPendingCopperAnimations(void);
-extern void ESQIFF_PlayNextExternalAssetFrame(void);
+typedef signed long LONG;
+typedef signed short WORD;
 
-void TEXTDISP2_JMPTBL_LOCAVAIL_GetFilterWindowHalfSpan(void){LOCAVAIL_GetFilterWindowHalfSpan();}
-void TEXTDISP2_JMPTBL_LADFUNC_DrawEntryPreview(void){LADFUNC_DrawEntryPreview();}
+extern LONG LOCAVAIL_GetFilterWindowHalfSpan(void);
+extern void LADFUNC_DrawEntryPreview(LONG entryIndex);
+extern void ESQIFF_RunPendingCopperAnimations(void);
+extern void ESQIFF_PlayNextExternalAssetFrame(WORD arg);
+
+LONG TEXTDISP2_JMPTBL_LOCAVAIL_GetFilterWindowHalfSpan(void){return LOCAVAIL_GetFilterWindowHalfSpan();}
+void TEXTDISP2_JMPTBL_LADFUNC_DrawEntryPreview(LONG entryIndex){LADFUNC_DrawEntryPreview(entryIndex);}
 void TEXTDISP2_JMPTBL_ESQIFF_RunPendingCopperAnimations(void){ESQIFF_RunPendingCopperAnimations();}
-void TEXTDISP2_JMPTBL_ESQIFF_PlayNextExternalAssetFrame(void){ESQIFF_PlayNextExternalAssetFrame();}
+void TEXTDISP2_JMPTBL_ESQIFF_PlayNextExternalAssetFrame(LONG value){ESQIFF_PlayNextExternalAssetFrame((WORD)value);}
