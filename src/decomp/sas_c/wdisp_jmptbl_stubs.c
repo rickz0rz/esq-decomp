23c2ed1059e11b59b1c8d7bbf2b4bb16527fdf8d
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:32:04 -0400

    Decompilation checkpoint: 1773102723

--- src/decomp/sas_c/wdisp_jmptbl_stubs.c
+++ src/decomp/sas_c/wdisp_jmptbl_stubs.c
@@ -5,7 +5,7 @@ extern void ESQIFF_QueueIffBrushLoad(void);
 extern void ESQIFF_RunCopperDropTransition(void);
 extern void BRUSH_FindBrushByPredicate(void);
 extern void BRUSH_FreeBrushList(void);
-extern void BRUSH_PlaneMaskForIndex(void);
+extern unsigned long BRUSH_PlaneMaskForIndex(long planeIndex);
 extern void ESQ_SetCopperEffect_OnEnableHighlight(void);
 extern void ESQIFF_RenderWeatherStatusBrushSlice(void);
 extern void BRUSH_SelectBrushSlot(void);
@@ -19,7 +19,7 @@ void WDISP_JMPTBL_ESQIFF_QueueIffBrushLoad(void){ESQIFF_QueueIffBrushLoad();}
 void WDISP_JMPTBL_ESQIFF_RunCopperDropTransition(void){ESQIFF_RunCopperDropTransition();}
 void WDISP_JMPTBL_BRUSH_FindBrushByPredicate(void){BRUSH_FindBrushByPredicate();}
 void WDISP_JMPTBL_BRUSH_FreeBrushList(void){BRUSH_FreeBrushList();}
-void WDISP_JMPTBL_BRUSH_PlaneMaskForIndex(void){BRUSH_PlaneMaskForIndex();}
+unsigned long WDISP_JMPTBL_BRUSH_PlaneMaskForIndex(long planeIndex){return BRUSH_PlaneMaskForIndex(planeIndex);}
 void WDISP_JMPTBL_ESQ_SetCopperEffect_OnEnableHighlight(void){ESQ_SetCopperEffect_OnEnableHighlight();}
 void WDISP_JMPTBL_ESQIFF_RenderWeatherStatusBrushSlice(void){ESQIFF_RenderWeatherStatusBrushSlice();}
 void WDISP_JMPTBL_BRUSH_SelectBrushSlot(void){BRUSH_SelectBrushSlot();}
