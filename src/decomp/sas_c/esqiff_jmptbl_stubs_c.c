ccd38fccc7b229995dcb0cef633257446b8ce000
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 23:03:02 -0400

    Decompilation checkpoint: 1773111782

--- src/decomp/sas_c/esqiff_jmptbl_stubs_c.c
+++ src/decomp/sas_c/esqiff_jmptbl_stubs_c.c
@@ -1,11 +1,11 @@
 typedef signed long LONG;
 
 extern void ESQ_MoveCopperEntryTowardStart(LONG dst_index, LONG src_index);
-extern void MEMORY_DeallocateMemory(void);
+extern void MEMORY_DeallocateMemory(void *ptr, LONG size);
 extern void DISKIO_ForceUiRefreshIfIdle(void);
 extern void *BRUSH_CloneBrushRecord(void *srcRec);
 
 void ESQIFF_JMPTBL_ESQ_MoveCopperEntryTowardStart(LONG dst_index, LONG src_index){ESQ_MoveCopperEntryTowardStart(dst_index, src_index);}
-void ESQIFF_JMPTBL_MEMORY_DeallocateMemory(void){MEMORY_DeallocateMemory();}
+void ESQIFF_JMPTBL_MEMORY_DeallocateMemory(const void *tag, LONG line, void *ptr, LONG size){MEMORY_DeallocateMemory(ptr, size);}
 void ESQIFF_JMPTBL_DISKIO_ForceUiRefreshIfIdle(void){DISKIO_ForceUiRefreshIfIdle();}
 void *ESQIFF_JMPTBL_BRUSH_CloneBrushRecord(void *srcRec){return BRUSH_CloneBrushRecord(srcRec);}
