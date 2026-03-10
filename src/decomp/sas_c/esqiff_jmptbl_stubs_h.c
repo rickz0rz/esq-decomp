6e1c3b271a43691cc71f1c0e76f002155944ab5c
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:34:09 -0400

    Decompilation checkpoint: 1773102848

--- src/decomp/sas_c/esqiff_jmptbl_stubs_h.c
+++ src/decomp/sas_c/esqiff_jmptbl_stubs_h.c
@@ -1,9 +1,9 @@
 extern void ESQ_IncCopperListsTowardsTargets(void);
 extern void ESQ_DecCopperListsPrimary(void);
-extern void BRUSH_SelectBrushSlot(void);
+extern long BRUSH_SelectBrushSlot(unsigned char *brush, long srcX0, long srcY0, long srcX1, long srcY1, char *dstRp, long forcedDstY);
 extern void BRUSH_SelectBrushByLabel(void);
 
 void ESQIFF_JMPTBL_ESQ_IncCopperListsTowardsTargets(void){ESQ_IncCopperListsTowardsTargets();}
 void ESQIFF_JMPTBL_ESQ_DecCopperListsPrimary(void){ESQ_DecCopperListsPrimary();}
-void ESQIFF_JMPTBL_BRUSH_SelectBrushSlot(void){BRUSH_SelectBrushSlot();}
+long ESQIFF_JMPTBL_BRUSH_SelectBrushSlot(unsigned char *brush, long srcX0, long srcY0, long srcX1, long srcY1, char *dstRp, long forcedDstY){return BRUSH_SelectBrushSlot(brush, srcX0, srcY0, srcX1, srcY1, dstRp, forcedDstY);}
 void ESQIFF_JMPTBL_BRUSH_SelectBrushByLabel(void){BRUSH_SelectBrushByLabel();}
--- src/decomp/sas_c/wdisp_jmptbl_stubs.c
+++ src/decomp/sas_c/wdisp_jmptbl_stubs.c
@@ -8,7 +8,7 @@ extern void BRUSH_FreeBrushList(void);
 extern unsigned long BRUSH_PlaneMaskForIndex(long planeIndex);
 extern void ESQ_SetCopperEffect_OnEnableHighlight(void);
 extern void ESQIFF_RenderWeatherStatusBrushSlice(void);
-extern void BRUSH_SelectBrushSlot(void);
+extern long BRUSH_SelectBrushSlot(unsigned char *brush, long srcX0, long srcY0, long srcX1, long srcY1, char *dstRp, long forcedDstY);
 extern char *NEWGRID_DrawWrappedText(char *rastport, long x, long y, long max_width, char *text, long draw_enable);
 extern void NEWGRID_ResetRowTable(void);
 
@@ -22,6 +22,6 @@ void WDISP_JMPTBL_BRUSH_FreeBrushList(void){BRUSH_FreeBrushList();}
 unsigned long WDISP_JMPTBL_BRUSH_PlaneMaskForIndex(long planeIndex){return BRUSH_PlaneMaskForIndex(planeIndex);}
 void WDISP_JMPTBL_ESQ_SetCopperEffect_OnEnableHighlight(void){ESQ_SetCopperEffect_OnEnableHighlight();}
 void WDISP_JMPTBL_ESQIFF_RenderWeatherStatusBrushSlice(void){ESQIFF_RenderWeatherStatusBrushSlice();}
-void WDISP_JMPTBL_BRUSH_SelectBrushSlot(void){BRUSH_SelectBrushSlot();}
+long WDISP_JMPTBL_BRUSH_SelectBrushSlot(unsigned char *brush, long srcX0, long srcY0, long srcX1, long srcY1, char *dstRp, long forcedDstY){return BRUSH_SelectBrushSlot(brush, srcX0, srcY0, srcX1, srcY1, dstRp, forcedDstY);}
 char *WDISP_JMPTBL_NEWGRID_DrawWrappedText(char *rastport, long x, long y, long max_width, char *text, long draw_enable){return NEWGRID_DrawWrappedText(rastport, x, y, max_width, text, draw_enable);}
 void WDISP_JMPTBL_NEWGRID_ResetRowTable(void){NEWGRID_ResetRowTable();}
