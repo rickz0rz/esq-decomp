942d17174473d200ee4c252f976d8ec2ad358866
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:32:48 -0400

    Decompilation checkpoint: 1773102768

--- src/decomp/sas_c/wdisp_jmptbl_stubs.c
+++ src/decomp/sas_c/wdisp_jmptbl_stubs.c
@@ -3,13 +3,13 @@ extern void ESQFUNC_TrimTextToPixelWidthWordBoundary(void);
 extern void GCOMMAND_ExpandPresetBlock(void);
 extern void ESQIFF_QueueIffBrushLoad(void);
 extern void ESQIFF_RunCopperDropTransition(void);
-extern void BRUSH_FindBrushByPredicate(void);
+extern void *BRUSH_FindBrushByPredicate(void *searchKey, void *listHeadPtr);
 extern void BRUSH_FreeBrushList(void);
 extern unsigned long BRUSH_PlaneMaskForIndex(long planeIndex);
 extern void ESQ_SetCopperEffect_OnEnableHighlight(void);
 extern void ESQIFF_RenderWeatherStatusBrushSlice(void);
 extern void BRUSH_SelectBrushSlot(void);
-extern void NEWGRID_DrawWrappedText(void);
+extern char *NEWGRID_DrawWrappedText(char *rastport, long x, long y, long max_width, char *text, long draw_enable);
 extern void NEWGRID_ResetRowTable(void);
 
 void WDISP_JMPTBL_ESQIFF_RestoreBasePaletteTriples(void){ESQIFF_RestoreBasePaletteTriples();}
@@ -17,11 +17,11 @@ void WDISP_JMPTBL_ESQFUNC_TrimTextToPixelWidthWordBoundary(void){ESQFUNC_TrimTex
 void WDISP_JMPTBL_GCOMMAND_ExpandPresetBlock(void){GCOMMAND_ExpandPresetBlock();}
 void WDISP_JMPTBL_ESQIFF_QueueIffBrushLoad(void){ESQIFF_QueueIffBrushLoad();}
 void WDISP_JMPTBL_ESQIFF_RunCopperDropTransition(void){ESQIFF_RunCopperDropTransition();}
-void WDISP_JMPTBL_BRUSH_FindBrushByPredicate(void){BRUSH_FindBrushByPredicate();}
+void *WDISP_JMPTBL_BRUSH_FindBrushByPredicate(void *searchKey, void *listHeadPtr){return BRUSH_FindBrushByPredicate(searchKey, listHeadPtr);}
 void WDISP_JMPTBL_BRUSH_FreeBrushList(void){BRUSH_FreeBrushList();}
 unsigned long WDISP_JMPTBL_BRUSH_PlaneMaskForIndex(long planeIndex){return BRUSH_PlaneMaskForIndex(planeIndex);}
 void WDISP_JMPTBL_ESQ_SetCopperEffect_OnEnableHighlight(void){ESQ_SetCopperEffect_OnEnableHighlight();}
 void WDISP_JMPTBL_ESQIFF_RenderWeatherStatusBrushSlice(void){ESQIFF_RenderWeatherStatusBrushSlice();}
 void WDISP_JMPTBL_BRUSH_SelectBrushSlot(void){BRUSH_SelectBrushSlot();}
-void WDISP_JMPTBL_NEWGRID_DrawWrappedText(void){NEWGRID_DrawWrappedText();}
+char *WDISP_JMPTBL_NEWGRID_DrawWrappedText(char *rastport, long x, long y, long max_width, char *text, long draw_enable){return NEWGRID_DrawWrappedText(rastport, x, y, max_width, text, draw_enable);}
 void WDISP_JMPTBL_NEWGRID_ResetRowTable(void){NEWGRID_ResetRowTable();}
