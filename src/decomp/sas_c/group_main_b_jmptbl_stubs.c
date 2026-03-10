184375f306a9491684dc2e2724774bf0f44b6792
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 16:54:58 -0400

    Decompilation checkpoint: 1773176098

--- src/decomp/sas_c/group_main_b_jmptbl_stubs.c
+++ src/decomp/sas_c/group_main_b_jmptbl_stubs.c
@@ -1,14 +1,14 @@
 typedef signed long LONG;
 typedef unsigned char UBYTE;
 
-extern void DOS_Delay(LONG ticks);
+extern LONG DOS_Delay(LONG ticks);
 extern void STREAM_BufferedWriteString(const char *s);
 extern unsigned long MATH_Mulu32(unsigned long a, unsigned long b);
 extern LONG BUFFER_FlushAllAndCloseWithCode(LONG code);
 
-void GROUP_MAIN_B_JMPTBL_DOS_Delay(LONG ticks)
+LONG GROUP_MAIN_B_JMPTBL_DOS_Delay(LONG ticks)
 {
-    DOS_Delay(ticks);
+    return DOS_Delay(ticks);
 }
 
 void GROUP_MAIN_B_JMPTBL_STREAM_BufferedWriteString(const char *s)
