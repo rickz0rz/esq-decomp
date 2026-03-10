107309be56d47a9835dcff437a5beb11c92fac0c
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 18:32:45 -0400

    Decompilation checkpoint: 1773181965

--- src/decomp/sas_c/dst_build_banner_time_word.c
+++ src/decomp/sas_c/dst_build_banner_time_word.c
@@ -3,12 +3,12 @@ typedef signed short WORD;
 typedef unsigned char UBYTE;
 typedef unsigned long ULONG;
 
-extern LONG DST_BuildBannerTimeEntry(WORD arg0, UBYTE arg1, WORD *out_word, ULONG arg3);
+extern LONG DST_BuildBannerTimeEntry(WORD lane, UBYTE slot_hint, WORD *out_word, ULONG out_dt);
 
-LONG DST_BuildBannerTimeWord(WORD arg_1, LONG arg_2, UBYTE arg_3)
+LONG DST_BuildBannerTimeWord(WORD lane, LONG unused, UBYTE slot_hint)
 {
     WORD out_word = 0;
-    DST_BuildBannerTimeEntry((LONG)arg_1, (LONG)arg_3, &out_word, 0);
-    (void)arg_2;
+    DST_BuildBannerTimeEntry(lane, slot_hint, &out_word, 0);
+    (void)unused;
     return (LONG)out_word;
 }
