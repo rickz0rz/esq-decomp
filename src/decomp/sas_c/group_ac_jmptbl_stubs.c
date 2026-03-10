3bf8ca287a9dde20fa28c22125131570695f20f5
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 22:43:39 -0400

    Decompilation checkpoint: 1773110619

--- src/decomp/sas_c/group_ac_jmptbl_stubs.c
+++ src/decomp/sas_c/group_ac_jmptbl_stubs.c
@@ -1,12 +1,13 @@
+typedef signed short WORD;
+
 extern void DST_RefreshBannerBuffer(void);
 extern long DST_UpdateBannerQueue(void *pair);
 extern void ESQDISP_DrawStatusBanner(void);
 extern void ESQFUNC_DrawDiagnosticsScreen(void);
 extern void ESQFUNC_DrawEscMenuVersion(void);
 extern void ESQFUNC_DrawMemoryStatusScreen(void);
-extern void ESQFUNC_FreeExtraTitleTextPointers(void);
-extern void GCOMMAND_UpdateBannerBounds(void);
-typedef signed short WORD;
+extern void ESQFUNC_FreeExtraTitleTextPointers(WORD max_index);
+extern void GCOMMAND_UpdateBannerBounds(long left, long top, long right, long bottom);
 
 extern long PARSEINI_AdjustHoursTo24HrFormat(WORD hour, WORD amPmFlag);
 extern void PARSEINI_UpdateClockFromRtc(void);
@@ -19,8 +20,8 @@ void GROUP_AC_JMPTBL_ESQDISP_DrawStatusBanner(void){ESQDISP_DrawStatusBanner();}
 void GROUP_AC_JMPTBL_ESQFUNC_DrawDiagnosticsScreen(void){ESQFUNC_DrawDiagnosticsScreen();}
 void GROUP_AC_JMPTBL_ESQFUNC_DrawEscMenuVersion(void){ESQFUNC_DrawEscMenuVersion();}
 void GROUP_AC_JMPTBL_ESQFUNC_DrawMemoryStatusScreen(void){ESQFUNC_DrawMemoryStatusScreen();}
-void GROUP_AC_JMPTBL_ESQFUNC_FreeExtraTitleTextPointers(void){ESQFUNC_FreeExtraTitleTextPointers();}
-void GROUP_AC_JMPTBL_GCOMMAND_UpdateBannerBounds(void){GCOMMAND_UpdateBannerBounds();}
+void GROUP_AC_JMPTBL_ESQFUNC_FreeExtraTitleTextPointers(WORD max_index){ESQFUNC_FreeExtraTitleTextPointers(max_index);}
+void GROUP_AC_JMPTBL_GCOMMAND_UpdateBannerBounds(long left, long top, long right, long bottom){GCOMMAND_UpdateBannerBounds(left, top, right, bottom);}
 long GROUP_AC_JMPTBL_PARSEINI_AdjustHoursTo24HrFormat(WORD hour, WORD amPmFlag){return PARSEINI_AdjustHoursTo24HrFormat(hour, amPmFlag);}
 void GROUP_AC_JMPTBL_PARSEINI_UpdateClockFromRtc(void){PARSEINI_UpdateClockFromRtc();}
 void GROUP_AC_JMPTBL_SCRIPT_ClearCtrlLineIfEnabled(void){SCRIPT_ClearCtrlLineIfEnabled();}
