bd08dbc31ba8b8dae965eef85a9879a3bbefbedc
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 04:12:12 -0400

    Decompilation checkpoint: 1773130332

--- src/decomp/sas_c/newgrid_get_entry_state_code.c
+++ src/decomp/sas_c/newgrid_get_entry_state_code.c
@@ -11,7 +11,7 @@ typedef struct NEWGRID_AuxData {
     UBYTE pad0[7];
     UBYTE rowFlags[49];
     UBYTE pad1[0x38 - 0x38];
-    char *titleTable[49];
+    const char *titleTable[49];
 } NEWGRID_AuxData;
 
 extern LONG NEWGRID2_JMPTBL_ESQ_TestBit1Based(const UBYTE *bitset, LONG bitIndex);
