d5e786ac7fa2c17e2fdb3be9fb6852055e4bc0e7
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 18:07:54 -0400

    Decompilation checkpoint: 1773180474

--- src/decomp/sas_c/esq_invoke_gcommand_init.c
+++ src/decomp/sas_c/esq_invoke_gcommand_init.c
@@ -1,9 +1,14 @@
 typedef signed long LONG;
 typedef unsigned char UBYTE;
 
+typedef struct GCOMMAND_CtrlPacket {
+    UBYTE pad0[4];
+    UBYTE type4;
+} GCOMMAND_CtrlPacket;
+
 extern LONG GCOMMAND_ProcessCtrlCommand(const UBYTE *cmdPtr);
 
-void ESQ_InvokeGcommandInit(const void *cmdPtr, void *unusedA1)
+void ESQ_InvokeGcommandInit(const GCOMMAND_CtrlPacket *cmdPtr, void *unusedA1)
 {
     (void)unusedA1;
     GCOMMAND_ProcessCtrlCommand((const UBYTE *)cmdPtr);
--- src/decomp/sas_c/group_av_jmptbl_stubs.c
+++ src/decomp/sas_c/group_av_jmptbl_stubs.c
@@ -1,9 +1,14 @@
 typedef signed long LONG;
 
+typedef struct GCOMMAND_CtrlPacket {
+    unsigned char pad0[4];
+    unsigned char type4;
+} GCOMMAND_CtrlPacket;
+
 extern void *ALLOCATE_AllocAndInitializeIOStdReq(void *replyPort);
 extern void *SIGNAL_CreateMsgPortWithSignal(const char *name, LONG pri);
 extern void DISKIO_ProbeDrivesAndAssignPaths(void);
-extern void ESQ_InvokeGcommandInit(const void *cmdPtr, void *unusedA1);
+extern void ESQ_InvokeGcommandInit(const GCOMMAND_CtrlPacket *cmdPtr, void *unusedA1);
 extern LONG EXEC_CallVector_48(void *a0, void *a1, LONG d1, void *a2);
 
 void *GROUP_AV_JMPTBL_ALLOCATE_AllocAndInitializeIOStdReq(void *replyPort)
@@ -21,7 +26,7 @@ void GROUP_AV_JMPTBL_DISKIO_ProbeDrivesAndAssignPaths(void)
     DISKIO_ProbeDrivesAndAssignPaths();
 }
 
-void GROUP_AV_JMPTBL_ESQ_InvokeGcommandInit(const void *cmdPtr, void *unusedA1)
+void GROUP_AV_JMPTBL_ESQ_InvokeGcommandInit(const GCOMMAND_CtrlPacket *cmdPtr, void *unusedA1)
 {
     ESQ_InvokeGcommandInit(cmdPtr, unusedA1);
 }
