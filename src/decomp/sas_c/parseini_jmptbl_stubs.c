cea607391f9bfcea73c9fa81d4d6e88691829733
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 22:51:35 -0400

    Decompilation checkpoint: 1773111095

--- src/decomp/sas_c/parseini_jmptbl_stubs.c
+++ src/decomp/sas_c/parseini_jmptbl_stubs.c
@@ -1,4 +1,5 @@
 typedef signed long LONG;
+typedef unsigned short UWORD;
 typedef unsigned char UBYTE;
 
 extern long STRING_CompareNoCase(const char *a, const char *b);
@@ -8,16 +9,16 @@ extern char *STR_FindCharPtr(char *s, long ch);
 extern void *HANDLE_OpenWithMode(const char *path, const char *mode, char *unused);
 extern void ESQIFF_QueueIffBrushLoad(void);
 extern void ESQIFF_HandleBrushIniReloadHotkey(void);
-extern void BRUSH_FreeBrushResources(void);
+extern void BRUSH_FreeBrushResources(void **headPtr);
 extern void ESQFUNC_RebuildPwBrushListFromTagTable(void);
 extern char *GCOMMAND_FindPathSeparator(char *path);
 extern char *DISKIO_ConsumeLineFromWorkBuffer(void);
 extern void ED1_DrawDiagnosticsScreen(void);
 extern void BRUSH_FreeBrushList(void **headPtr, LONG freeAll);
-extern void GCOMMAND_ValidatePresetTable(void);
+extern void GCOMMAND_ValidatePresetTable(UWORD *presetTable);
 extern void *BRUSH_AllocBrushNode(const char *brushLabel, void *prevTail);
-extern void UNKNOWN36_FinalizeRequest(void);
-extern void GCOMMAND_InitPresetTableFromPalette(void);
+extern LONG UNKNOWN36_FinalizeRequest(void *req);
+extern void GCOMMAND_InitPresetTableFromPalette(UWORD *presetTable);
 extern char *STRING_AppendAtNull(char *dst, const char *src);
 extern long DISKIO_LoadFileToWorkBuffer(const char *path);
 extern void WDISP_SPrintf(void);
@@ -35,16 +36,16 @@ char *PARSEINI_JMPTBL_STR_FindCharPtr(const char *s, long ch){return STR_FindCha
 void *PARSEINI_JMPTBL_HANDLE_OpenWithMode(const char *path, const char *mode, char *unused){return HANDLE_OpenWithMode(path, mode, unused);}
 void PARSEINI_JMPTBL_ESQIFF_QueueIffBrushLoad(void){ESQIFF_QueueIffBrushLoad();}
 void PARSEINI_JMPTBL_ESQIFF_HandleBrushIniReloadHotkey(void){ESQIFF_HandleBrushIniReloadHotkey();}
-void PARSEINI_JMPTBL_BRUSH_FreeBrushResources(void){BRUSH_FreeBrushResources();}
+void PARSEINI_JMPTBL_BRUSH_FreeBrushResources(void **headPtr){BRUSH_FreeBrushResources(headPtr);}
 void PARSEINI_JMPTBL_ESQFUNC_RebuildPwBrushListFromTagTableFromTagTable(void){ESQFUNC_RebuildPwBrushListFromTagTable();}
 char *PARSEINI_JMPTBL_GCOMMAND_FindPathSeparator(char *path){return GCOMMAND_FindPathSeparator(path);}
 char *PARSEINI_JMPTBL_DISKIO_ConsumeLineFromWorkBuffer(void){return DISKIO_ConsumeLineFromWorkBuffer();}
 void PARSEINI_JMPTBL_ED1_DrawDiagnosticsScreen(void){ED1_DrawDiagnosticsScreen();}
 void PARSEINI_JMPTBL_BRUSH_FreeBrushList(void **headPtr, LONG freeAll){BRUSH_FreeBrushList(headPtr, freeAll);}
-void PARSEINI_JMPTBL_GCOMMAND_ValidatePresetTable(void){GCOMMAND_ValidatePresetTable();}
+void PARSEINI_JMPTBL_GCOMMAND_ValidatePresetTable(UWORD *presetTable){GCOMMAND_ValidatePresetTable(presetTable);}
 void *PARSEINI_JMPTBL_BRUSH_AllocBrushNode(const char *brushLabel, void *prevTail){return BRUSH_AllocBrushNode(brushLabel, prevTail);}
-void PARSEINI_JMPTBL_UNKNOWN36_FinalizeRequest(void){UNKNOWN36_FinalizeRequest();}
-void PARSEINI_JMPTBL_GCOMMAND_InitPresetTableFromPalette(void){GCOMMAND_InitPresetTableFromPalette();}
+LONG PARSEINI_JMPTBL_UNKNOWN36_FinalizeRequest(void *req){return UNKNOWN36_FinalizeRequest(req);}
+void PARSEINI_JMPTBL_GCOMMAND_InitPresetTableFromPalette(UWORD *presetTable){GCOMMAND_InitPresetTableFromPalette(presetTable);}
 char *PARSEINI_JMPTBL_STRING_AppendAtNull(char *dst, const char *src){return STRING_AppendAtNull(dst, src);}
 long PARSEINI_JMPTBL_DISKIO_LoadFileToWorkBuffer(const char *path){return DISKIO_LoadFileToWorkBuffer(path);}
 void PARSEINI_JMPTBL_WDISP_SPrintf(void){WDISP_SPrintf();}
