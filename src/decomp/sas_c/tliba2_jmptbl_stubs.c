5d1042eafd82f5465a821cca77479556db85f7f5
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 02:48:20 -0400

    Decompilation checkpoint: 1773125300

--- src/decomp/sas_c/tliba2_jmptbl_stubs.c
+++ src/decomp/sas_c/tliba2_jmptbl_stubs.c
@@ -1,9 +1,10 @@
 typedef signed long LONG;
+typedef signed short WORD;
 typedef unsigned long ULONG;
 typedef unsigned char UBYTE;
 
-extern void DST_AddTimeOffset(void);
+extern void DST_AddTimeOffset(void *dt, WORD hours, WORD minutes);
 extern LONG ESQ_TestBit1Based(const UBYTE *base, ULONG bitIndex);
 
-void TLIBA2_JMPTBL_DST_AddTimeOffset(void){DST_AddTimeOffset();}
+void TLIBA2_JMPTBL_DST_AddTimeOffset(void *dt, WORD hours, WORD minutes){DST_AddTimeOffset(dt, hours, minutes);}
 LONG TLIBA2_JMPTBL_ESQ_TestBit1Based(const UBYTE *base, ULONG bitIndex){return ESQ_TestBit1Based(base, bitIndex);}
