da9c4fa6d3429a113ebbb4efcfb3160709a67ab5
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:55:04 -0400

    Decompilation checkpoint: 1773104104

--- src/decomp/sas_c/esqiff_jmptbl_stubs_d.c
+++ src/decomp/sas_c/esqiff_jmptbl_stubs_d.c
@@ -1,9 +1,11 @@
-extern void ESQ_MoveCopperEntryTowardEnd(void);
-extern void *BRUSH_FindBrushByPredicate(void *searchKey, void *listHeadPtr);
-extern void BRUSH_FreeBrushList(void);
-extern void BRUSH_FindType3Brush(void);
+typedef signed long LONG;
 
-void ESQIFF_JMPTBL_ESQ_MoveCopperEntryTowardEnd(void){ESQ_MoveCopperEntryTowardEnd();}
+extern void ESQ_MoveCopperEntryTowardEnd(LONG src_index, LONG dst_index);
+extern void *BRUSH_FindBrushByPredicate(void *searchKey, void *listHeadPtr);
+extern void BRUSH_FreeBrushList(void **headPtr, LONG freeAll);
+extern void *BRUSH_FindType3Brush(void *listHeadPtr);
+
+void ESQIFF_JMPTBL_ESQ_MoveCopperEntryTowardEnd(LONG src_index, LONG dst_index){ESQ_MoveCopperEntryTowardEnd(src_index, dst_index);}
 void *ESQIFF_JMPTBL_BRUSH_FindBrushByPredicate(void *searchKey, void *listHeadPtr){return BRUSH_FindBrushByPredicate(searchKey, listHeadPtr);}
-void ESQIFF_JMPTBL_BRUSH_FreeBrushList(void){BRUSH_FreeBrushList();}
-void ESQIFF_JMPTBL_BRUSH_FindType3Brush(void){BRUSH_FindType3Brush();}
+void ESQIFF_JMPTBL_BRUSH_FreeBrushList(void **headPtr, LONG freeAll){BRUSH_FreeBrushList(headPtr, freeAll);}
+void *ESQIFF_JMPTBL_BRUSH_FindType3Brush(void *listHeadPtr){return BRUSH_FindType3Brush(listHeadPtr);}
