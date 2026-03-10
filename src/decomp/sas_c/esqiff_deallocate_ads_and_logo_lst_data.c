d836e34e4c7892e5cb86740326f601772c090f6a
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 00:39:36 -0400

    Decompilation checkpoint: 1773117576

--- src/decomp/sas_c/esqiff_deallocate_ads_and_logo_lst_data.c
+++ src/decomp/sas_c/esqiff_deallocate_ads_and_logo_lst_data.c
@@ -4,8 +4,8 @@ extern LONG Global_REF_LONG_GFX_G_ADS_DATA;
 extern LONG Global_REF_LONG_GFX_G_ADS_FILESIZE;
 extern LONG Global_REF_LONG_DF0_LOGO_LST_DATA;
 extern LONG Global_REF_LONG_DF0_LOGO_LST_FILESIZE;
-extern const char Global_STR_ESQIFF_C_7;
-extern const char Global_STR_ESQIFF_C_8;
+extern const char Global_STR_ESQIFF_C_7[];
+extern const char Global_STR_ESQIFF_C_8[];
 
 extern void ESQIFF_JMPTBL_MEMORY_DeallocateMemory(const void *tag, LONG width, void *ptr, LONG size);
 
@@ -13,7 +13,7 @@ void ESQIFF_DeallocateAdsAndLogoLstData(void)
 {
     if (Global_REF_LONG_GFX_G_ADS_DATA != 0 && Global_REF_LONG_GFX_G_ADS_FILESIZE != 0) {
         ESQIFF_JMPTBL_MEMORY_DeallocateMemory(
-            &Global_STR_ESQIFF_C_7,
+            Global_STR_ESQIFF_C_7,
             1988,
             (void *)Global_REF_LONG_GFX_G_ADS_DATA,
             Global_REF_LONG_GFX_G_ADS_FILESIZE + 1);
@@ -23,7 +23,7 @@ void ESQIFF_DeallocateAdsAndLogoLstData(void)
 
     if (Global_REF_LONG_DF0_LOGO_LST_DATA != 0 && Global_REF_LONG_DF0_LOGO_LST_FILESIZE != 0) {
         ESQIFF_JMPTBL_MEMORY_DeallocateMemory(
-            &Global_STR_ESQIFF_C_8,
+            Global_STR_ESQIFF_C_8,
             1994,
             (void *)Global_REF_LONG_DF0_LOGO_LST_DATA,
             Global_REF_LONG_DF0_LOGO_LST_FILESIZE + 1);
