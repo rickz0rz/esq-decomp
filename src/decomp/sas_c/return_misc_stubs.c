9330a7de70d18e520bbec548074d248a9dd144f4
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 16:48:47 -0400

    Decompilation checkpoint: 1773175727

--- src/decomp/sas_c/return_misc_stubs.c
+++ src/decomp/sas_c/return_misc_stubs.c
@@ -1,4 +1,4 @@
-void ESQ_ShutdownAndReturn(void) {}
+long ESQ_ShutdownAndReturn(long exit_code) { return exit_code; }
 void ESQIFF_ShowExternalAssetWithCopperFx_Return(void) {}
 void ESQIFF_PlayNextExternalAssetFrame_Return(void) {}
 void ESQSHARED4_ApplyBannerColorStep_Return(void) {}
