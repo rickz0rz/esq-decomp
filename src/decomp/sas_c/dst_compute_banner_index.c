0c76887672c486a497ff620b7c5054ca7299b658
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 18:31:29 -0400

    Decompilation checkpoint: 1773181889

--- src/decomp/sas_c/dst_compute_banner_index.c
+++ src/decomp/sas_c/dst_compute_banner_index.c
@@ -14,6 +14,12 @@ typedef struct DST_BannerTimeContext {
 
 LONG DST_ComputeBannerIndex(void *ctx, WORD arg_2, UBYTE arg_3)
 {
+    const LONG HOURS_PER_HALF_DAY = 12;
+    const LONG PM_HOUR_OFFSET = 12;
+    const WORD LATE_MONTH_DAY_THRESHOLD = 0x1d;
+    const WORD BANNER_INDEX_BASE = 0x26;
+    const WORD BANNER_INDEX_MODULUS = 48;
+    const WORD BANNER_INDEX_BIAS = 1;
     DST_BannerTimeContext *p = (DST_BannerTimeContext *)ctx;
     WORD out_word = 0;
     LONG rem;
@@ -23,18 +29,18 @@ LONG DST_ComputeBannerIndex(void *ctx, WORD arg_2, UBYTE arg_3)
 
     DST_BuildBannerTimeEntry((LONG)arg_2, (LONG)arg_3, &out_word, ctx);
 
-    rem = (LONG)((WORD)(p->hourWord8) % 12);
+    rem = (LONG)((WORD)(p->hourWord8) % HOURS_PER_HALF_DAY);
     if (p->pmFlag18) {
-        rem += 12;
+        rem += PM_HOUR_OFFSET;
     }
 
     idx = rem + rem;
-    if ((WORD)(p->dayWord10) > 0x1d) {
+    if ((WORD)(p->dayWord10) > LATE_MONTH_DAY_THRESHOLD) {
         idx += 1;
     }
 
     nonzero = (idx != 0);
-    folded = (WORD)((WORD)(nonzero + 0x26) % 48);
+    folded = (WORD)((WORD)(nonzero + BANNER_INDEX_BASE) % BANNER_INDEX_MODULUS);
 
-    return (LONG)(WORD)(folded + 1);
+    return (LONG)(WORD)(folded + BANNER_INDEX_BIAS);
 }
