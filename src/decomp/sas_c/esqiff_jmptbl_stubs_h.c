6f5187b05308b6d29e539e06397c1e9013f920c0
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:55:39 -0400

    Decompilation checkpoint: 1773104139

--- src/decomp/sas_c/esqiff_jmptbl_stubs_h.c
+++ src/decomp/sas_c/esqiff_jmptbl_stubs_h.c
@@ -1,9 +1,9 @@
 extern void ESQ_IncCopperListsTowardsTargets(void);
 extern void ESQ_DecCopperListsPrimary(void);
 extern long BRUSH_SelectBrushSlot(unsigned char *brush, long srcX0, long srcY0, long srcX1, long srcY1, char *dstRp, long forcedDstY);
-extern void BRUSH_SelectBrushByLabel(void);
+extern void BRUSH_SelectBrushByLabel(const char *brushLabel);
 
 void ESQIFF_JMPTBL_ESQ_IncCopperListsTowardsTargets(void){ESQ_IncCopperListsTowardsTargets();}
 void ESQIFF_JMPTBL_ESQ_DecCopperListsPrimary(void){ESQ_DecCopperListsPrimary();}
 long ESQIFF_JMPTBL_BRUSH_SelectBrushSlot(unsigned char *brush, long srcX0, long srcY0, long srcX1, long srcY1, char *dstRp, long forcedDstY){return BRUSH_SelectBrushSlot(brush, srcX0, srcY0, srcX1, srcY1, dstRp, forcedDstY);}
-void ESQIFF_JMPTBL_BRUSH_SelectBrushByLabel(void){BRUSH_SelectBrushByLabel();}
+void ESQIFF_JMPTBL_BRUSH_SelectBrushByLabel(const char *brushLabel){BRUSH_SelectBrushByLabel(brushLabel);}
