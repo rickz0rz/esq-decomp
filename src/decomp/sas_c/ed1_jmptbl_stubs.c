f887c258fbb777facc7e0b929ccc46e2eefb553f
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 21:00:52 -0400

    Decompilation checkpoint: 1773104452

--- src/decomp/sas_c/ed1_jmptbl_stubs.c
+++ src/decomp/sas_c/ed1_jmptbl_stubs.c
@@ -1,25 +1,28 @@
+typedef signed long LONG;
+typedef unsigned char UBYTE;
+
 extern void NEWGRID_DrawTopBorderLine(void);
 extern void LOCAVAIL_ResetFilterCursorState(void);
 extern void GCOMMAND_ResetHighlightMessages(void);
-extern void LADFUNC_SetPackedPenLowNibble(void);
+extern LONG LADFUNC_SetPackedPenLowNibble(UBYTE packed, UBYTE lowNibble);
 extern void LADFUNC_SaveTextAdsToFile(void);
 extern void ESQ_ColdReboot(void);
 extern void ESQSHARED4_LoadDefaultPaletteToCopper_NoOp(void);
 extern void GCOMMAND_SeedBannerDefaults(void);
-extern void MEM_Move(void);
+extern LONG MEM_Move(UBYTE *src, UBYTE *dst, LONG length);
 extern void GCOMMAND_SeedBannerFromPrefs(void);
 extern void CLEANUP_DrawDateTimeBannerRow(void);
-extern void LADFUNC_SetPackedPenHighNibble(void);
+extern LONG LADFUNC_SetPackedPenHighNibble(UBYTE highNibble, UBYTE lowNibble);
 
 void ED1_JMPTBL_NEWGRID_DrawTopBorderLine(void){NEWGRID_DrawTopBorderLine();}
 void ED1_JMPTBL_LOCAVAIL_ResetFilterCursorState(void){LOCAVAIL_ResetFilterCursorState();}
 void ED1_JMPTBL_GCOMMAND_ResetHighlightMessages(void){GCOMMAND_ResetHighlightMessages();}
-void ED1_JMPTBL_LADFUNC_MergeHighLowNibbles(void){LADFUNC_SetPackedPenLowNibble();}
+LONG ED1_JMPTBL_LADFUNC_MergeHighLowNibbles(UBYTE packed, UBYTE lowNibble){return LADFUNC_SetPackedPenLowNibble(packed, lowNibble);}
 void ED1_JMPTBL_LADFUNC_SaveTextAdsToFile(void){LADFUNC_SaveTextAdsToFile();}
 void ED1_JMPTBL_ESQ_ColdReboot(void){ESQ_ColdReboot();}
 void ED1_JMPTBL_ESQSHARED4_LoadDefaultPaletteToCopper_NoOp(void){ESQSHARED4_LoadDefaultPaletteToCopper_NoOp();}
 void ED1_JMPTBL_GCOMMAND_SeedBannerDefaults(void){GCOMMAND_SeedBannerDefaults();}
-void ED1_JMPTBL_MEM_Move(void){MEM_Move();}
+LONG ED1_JMPTBL_MEM_Move(UBYTE *src, UBYTE *dst, LONG length){return MEM_Move(src, dst, length);}
 void ED1_JMPTBL_GCOMMAND_SeedBannerFromPrefs(void){GCOMMAND_SeedBannerFromPrefs();}
 void ED1_JMPTBL_CLEANUP_DrawDateTimeBannerRow(void){CLEANUP_DrawDateTimeBannerRow();}
-void ED1_JMPTBL_LADFUNC_PackNibblesToByte(void){LADFUNC_SetPackedPenHighNibble();}
+LONG ED1_JMPTBL_LADFUNC_PackNibblesToByte(UBYTE highNibble, UBYTE lowNibble){return LADFUNC_SetPackedPenHighNibble(highNibble, lowNibble);}
