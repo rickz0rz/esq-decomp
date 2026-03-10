0f80389117ec04dc799bb7f9d945f1a9b0fdb923
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 23:04:51 -0400

    Decompilation checkpoint: 1773111891

--- src/decomp/sas_c/newgrid_jmptbl_stubs.c
+++ src/decomp/sas_c/newgrid_jmptbl_stubs.c
@@ -1,11 +1,12 @@
 typedef signed long LONG;
+typedef unsigned long ULONG;
 
 extern LONG MATH_DivS32(LONG a, LONG b);
 extern void *DATETIME_SecondsToStruct(LONG seconds, void *dt);
 extern void GENERATE_GRID_DATE_STRING(char *outText);
-extern void MEMORY_DeallocateMemory(void);
+extern void MEMORY_DeallocateMemory(void *ptr, LONG size);
 extern void DISPTEXT_FreeBuffers(void);
-extern void MEMORY_AllocateMemory(void);
+extern void *MEMORY_AllocateMemory(ULONG byteSize, ULONG attributes);
 extern void DISPTEXT_InitBuffers(void);
 extern LONG DATETIME_NormalizeStructToSeconds(void *dt);
 extern char *STR_CopyUntilAnyDelimN(char *src, char *dst, LONG maxLen, char *delims);
@@ -15,9 +16,9 @@ extern LONG MATH_Mulu32(LONG a, LONG b);
 LONG NEWGRID_JMPTBL_MATH_DivS32(LONG a, LONG b){return MATH_DivS32(a, b);}
 void *NEWGRID_JMPTBL_DATETIME_SecondsToStruct(LONG seconds, void *dt){return DATETIME_SecondsToStruct(seconds, dt);}
 void NEWGRID_JMPTBL_GENERATE_GRID_DATE_STRING(char *outText){GENERATE_GRID_DATE_STRING(outText);}
-void NEWGRID_JMPTBL_MEMORY_DeallocateMemory(void){MEMORY_DeallocateMemory();}
+void NEWGRID_JMPTBL_MEMORY_DeallocateMemory(const char *tagName, LONG line, void *ptr, LONG bytes){MEMORY_DeallocateMemory(ptr, bytes);}
 void NEWGRID_JMPTBL_DISPTEXT_FreeBuffers(void){DISPTEXT_FreeBuffers();}
-void NEWGRID_JMPTBL_MEMORY_AllocateMemory(void){MEMORY_AllocateMemory();}
+void *NEWGRID_JMPTBL_MEMORY_AllocateMemory(const char *tagName, LONG line, LONG size, LONG flags){return MEMORY_AllocateMemory(size, flags);}
 void NEWGRID_JMPTBL_DISPTEXT_InitBuffers(void){DISPTEXT_InitBuffers();}
 LONG NEWGRID_JMPTBL_DATETIME_NormalizeStructToSeconds(void *dt){return DATETIME_NormalizeStructToSeconds(dt);}
 char *NEWGRID_JMPTBL_STR_CopyUntilAnyDelimN(char *src, char *dst, LONG maxLen, const char *delims){return STR_CopyUntilAnyDelimN(src, dst, maxLen, (char *)delims);}
