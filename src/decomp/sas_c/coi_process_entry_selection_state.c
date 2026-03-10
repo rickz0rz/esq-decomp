5c414209bbb38ed1f96f6b6263df5d69da85f71d
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-10 17:47:26 -0400

    Decompilation checkpoint: 1773179246

--- src/decomp/sas_c/coi_process_entry_selection_state.c
+++ src/decomp/sas_c/coi_process_entry_selection_state.c
@@ -1,11 +1,9 @@
 typedef signed long LONG;
+typedef signed short WORD;
+
+extern LONG COI_TestEntryWithinTimeWindow(const void *entry, const void *aux, WORD slot, LONG win, LONG tol);
 
 LONG COI_ProcessEntrySelectionState(void *entry, void *aux, LONG idx, LONG win, LONG tol)
 {
-    (void)entry;
-    (void)aux;
-    (void)idx;
-    (void)win;
-    (void)tol;
-    return 0;
+    return COI_TestEntryWithinTimeWindow(entry, aux, (WORD)idx, win, tol);
 }
