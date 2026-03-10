0be0a1227742f54257f68a2da6dbcf111e9dabe8
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:37:54 -0400

    Decompilation checkpoint: 1773103074

--- src/decomp/sas_c/group_aw_jmptbl_stubs_core.c
+++ src/decomp/sas_c/group_aw_jmptbl_stubs_core.c
@@ -1,22 +1,24 @@
 typedef signed long LONG;
+typedef unsigned long ULONG;
+typedef unsigned char UBYTE;
 
 extern LONG TLIBA3_BuildDisplayContextForViewMode(LONG viewMode, LONG a1, LONG a2);
-extern void DISPLIB_ApplyInlineAlignmentPadding(void);
+extern void DISPLIB_ApplyInlineAlignmentPadding(char *text, UBYTE alignCode);
 extern void ESQIFF_RunCopperRiseTransition(void);
 extern void DISPLIB_DisplayTextAtPosition(char *rastPort, LONG x, LONG y, const char *text);
 extern void MEM_Move(void);
 extern void WDISP_SPrintf(void);
 extern void ESQ_SetCopperEffect_OffDisableHighlight(void);
-extern void STRING_CopyPadNul(void);
+extern char *STRING_CopyPadNul(char *dst, const char *src, ULONG maxLen);
 
 LONG GROUP_AW_JMPTBL_TLIBA3_BuildDisplayContextForViewMode(LONG viewMode, LONG a1, LONG a2)
 {
     return TLIBA3_BuildDisplayContextForViewMode(viewMode, a1, a2);
 }
 
-void GROUP_AW_JMPTBL_DISPLIB_ApplyInlineAlignmentPadding(void)
+void GROUP_AW_JMPTBL_DISPLIB_ApplyInlineAlignmentPadding(char *text, UBYTE alignCode)
 {
-    DISPLIB_ApplyInlineAlignmentPadding();
+    DISPLIB_ApplyInlineAlignmentPadding(text, alignCode);
 }
 
 void GROUP_AW_JMPTBL_ESQIFF_RunCopperRiseTransition(void)
@@ -44,7 +46,7 @@ void GROUP_AW_JMPTBL_ESQ_SetCopperEffect_OffDisableHighlight(void)
     ESQ_SetCopperEffect_OffDisableHighlight();
 }
 
-void GROUP_AW_JMPTBL_STRING_CopyPadNul(void)
+char *GROUP_AW_JMPTBL_STRING_CopyPadNul(char *dst, const char *src, ULONG maxLen)
 {
-    STRING_CopyPadNul();
+    return STRING_CopyPadNul(dst, src, maxLen);
 }
