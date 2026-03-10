d4641695a75fed854909218135d64a319cb2c50f
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 22:58:49 -0400

    Decompilation checkpoint: 1773111529

--- src/decomp/sas_c/group_au_jmptbl_stubs.c
+++ src/decomp/sas_c/group_au_jmptbl_stubs.c
@@ -1,12 +1,14 @@
-extern void BRUSH_AppendBrushNode(void);
-extern void BRUSH_PopulateBrushList(void);
+typedef signed long LONG;
 
-void GROUP_AU_JMPTBL_BRUSH_AppendBrushNode(void)
+extern void *BRUSH_AppendBrushNode(void *head, void *node);
+extern void BRUSH_PopulateBrushList(void *descriptorList, void **outHeadPtr);
+
+LONG GROUP_AU_JMPTBL_BRUSH_AppendBrushNode(LONG head, LONG newNode)
 {
-    BRUSH_AppendBrushNode();
+    return (LONG)BRUSH_AppendBrushNode((void *)head, (void *)newNode);
 }
 
-void GROUP_AU_JMPTBL_BRUSH_PopulateBrushList(void)
+void GROUP_AU_JMPTBL_BRUSH_PopulateBrushList(void *ctx, LONG *outList)
 {
-    BRUSH_PopulateBrushList();
+    BRUSH_PopulateBrushList(ctx, (void **)outList);
 }
