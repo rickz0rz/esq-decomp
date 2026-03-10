386c2517c37c5a3931212f042032bbd7bfab7c5f
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 22:59:20 -0400

    Decompilation checkpoint: 1773111560

--- src/decomp/sas_c/group_at_jmptbl_stubs_core.c
+++ src/decomp/sas_c/group_at_jmptbl_stubs_core.c
@@ -1,12 +1,14 @@
+typedef signed long LONG;
+
 extern void ED1_WaitForFlagAndClearBit0(void);
-extern void DOS_SystemTagList(void);
+extern LONG DOS_SystemTagList(LONG arg1, LONG arg2);
 
 void GROUP_AT_JMPTBL_ED1_WaitForFlagAndClearBit0(void)
 {
     ED1_WaitForFlagAndClearBit0();
 }
 
-void GROUP_AT_JMPTBL_DOS_SystemTagList(void)
+LONG GROUP_AT_JMPTBL_DOS_SystemTagList(LONG arg1, LONG arg2)
 {
-    DOS_SystemTagList();
+    return DOS_SystemTagList(arg1, arg2);
 }
