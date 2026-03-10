0d0ed3ebc71abf375a8d2022cd484af8e78ed0ef
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-09 20:48:41 -0400

    Decompilation checkpoint: 1773103721

--- src/decomp/sas_c/group_al_jmptbl_stubs.c
+++ src/decomp/sas_c/group_al_jmptbl_stubs.c
@@ -1,36 +1,39 @@
-extern void LADFUNC_ComposePackedPenByte(void);
-extern void LADFUNC_GetPackedPenLowNibble(void);
-extern void LADFUNC_UpdateEntryFromTextAndAttrBuffers(void);
-extern void ESQ_WriteDecFixedWidth(void);
-extern void LADFUNC_BuildEntryBuffersOrDefault(void);
-extern void LADFUNC_GetPackedPenHighNibble(void);
+typedef signed long LONG;
+typedef unsigned char UBYTE;
 
-void GROUP_AL_JMPTBL_LADFUNC_PackNibblesToByte(void)
+extern LONG LADFUNC_ComposePackedPenByte(UBYTE highNibble, UBYTE lowNibble);
+extern LONG LADFUNC_GetPackedPenLowNibble(UBYTE packed);
+extern void LADFUNC_UpdateEntryFromTextAndAttrBuffers(LONG entryIndex, char *textBuf, UBYTE *attrBuf);
+extern void ESQ_WriteDecFixedWidth(char *outBuf, LONG value, LONG digits);
+extern void LADFUNC_BuildEntryBuffersOrDefault(LONG entryIndex, char *outText, UBYTE *outAttr);
+extern LONG LADFUNC_GetPackedPenHighNibble(UBYTE packed);
+
+LONG GROUP_AL_JMPTBL_LADFUNC_PackNibblesToByte(UBYTE highNibble, UBYTE lowNibble)
 {
-    LADFUNC_ComposePackedPenByte();
+    return LADFUNC_ComposePackedPenByte(highNibble, lowNibble);
 }
 
-void GROUP_AL_JMPTBL_LADFUNC_ExtractLowNibble(void)
+LONG GROUP_AL_JMPTBL_LADFUNC_ExtractLowNibble(UBYTE packed)
 {
-    LADFUNC_GetPackedPenLowNibble();
+    return LADFUNC_GetPackedPenLowNibble(packed);
 }
 
-void GROUP_AL_JMPTBL_LADFUNC_UpdateEntryBuffersForAdIndex(void)
+void GROUP_AL_JMPTBL_LADFUNC_UpdateEntryBuffersForAdIndex(LONG entryIndex, char *textBuf, UBYTE *attrBuf)
 {
-    LADFUNC_UpdateEntryFromTextAndAttrBuffers();
+    LADFUNC_UpdateEntryFromTextAndAttrBuffers(entryIndex, textBuf, attrBuf);
 }
 
-void GROUP_AL_JMPTBL_ESQ_WriteDecFixedWidth(void)
+void GROUP_AL_JMPTBL_ESQ_WriteDecFixedWidth(char *outBuf, LONG value, LONG digits)
 {
-    ESQ_WriteDecFixedWidth();
+    ESQ_WriteDecFixedWidth(outBuf, value, digits);
 }
 
-void GROUP_AL_JMPTBL_LADFUNC_BuildEntryBuffersOrDefault(void)
+void GROUP_AL_JMPTBL_LADFUNC_BuildEntryBuffersOrDefault(LONG entryIndex, char *outText, UBYTE *outAttr)
 {
-    LADFUNC_BuildEntryBuffersOrDefault();
+    LADFUNC_BuildEntryBuffersOrDefault(entryIndex, outText, outAttr);
 }
 
-void GROUP_AL_JMPTBL_LADFUNC_ExtractHighNibble(void)
+LONG GROUP_AL_JMPTBL_LADFUNC_ExtractHighNibble(UBYTE packed)
 {
-    LADFUNC_GetPackedPenHighNibble();
+    return LADFUNC_GetPackedPenHighNibble(packed);
 }
