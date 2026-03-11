630f42fcd984337b55d7df341dabc7ec3531ea94
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-11 19:58:09 -0400

    Decompilation checkpoint: 1773273489

--- src/decomp/sas_c/unknown40_dos_delay.c
+++ src/decomp/sas_c/unknown40_dos_delay.c
@@ -1,8 +1,10 @@
 typedef long LONG;
 
-LONG _LVODelay(LONG ticks);
+extern void *Global_REF_DOS_LIBRARY_2;
+#pragma libcall Global_REF_DOS_LIBRARY_2 Delay c6 101
+extern LONG Delay(LONG ticks);
 
 LONG DOS_Delay(LONG ticks)
 {
-    return _LVODelay(ticks);
+    return Delay(ticks);
 }
