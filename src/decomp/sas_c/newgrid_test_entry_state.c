b4570394408c803df524fcfa7af728d060232bd7
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-11 21:01:04 -0400

    Decompilation checkpoint: 1773277264

--- src/decomp/sas_c/newgrid_test_entry_state.c
+++ src/decomp/sas_c/newgrid_test_entry_state.c
@@ -7,58 +7,36 @@ extern LONG NEWGRID_GetEntryStateCode(const void *entryPtr, const void *entryAux
 
 LONG NEWGRID_TestEntryState(LONG mode, LONG primaryIndex, LONG secondaryIndex, WORD selector)
 {
-    const LONG MODE_PRIMARY = 1;
-    const LONG MODE_SECONDARY = 2;
-    const WORD SLOT_WRAP = 48;
-    const WORD SLOT_PRIMARY_FIRST = 1;
-    const LONG RESULT_FALSE = 0;
-    const LONG RESULT_TRUE = 1;
-    const LONG RESULT_MATCH = -1;
-    const LONG TEST_MODE_UNSET = 0;
-    const LONG TEST_MODE_PRESENT = 1;
-    const LONG TEST_MODE_BLOCKED_A = 2;
-    const LONG TEST_MODE_BLOCKED_B = 3;
-    const LONG STATE_UNSET = 0;
-    const LONG STATE_PRESENT = 1;
-    const LONG STATE_BLOCKED = 3;
     const void *entryPtr;
     const void *entryAuxPtr;
     LONG state;
-    LONG result;
+    LONG result = 0;
 
-    result = RESULT_FALSE;
+    if (selector > 48 || selector == 1) {
+        entryPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryPointerByMode(secondaryIndex, 2);
+        entryAuxPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryAuxPointerByMode(secondaryIndex, 2);
 
-    if (selector > SLOT_WRAP || selector == SLOT_PRIMARY_FIRST) {
-        entryPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryPointerByMode(secondaryIndex, MODE_SECONDARY);
-        entryAuxPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryAuxPointerByMode(secondaryIndex, MODE_SECONDARY);
-
-        while (selector > SLOT_WRAP) {
-            selector -= SLOT_WRAP;
+        while (selector > 48) {
+            selector -= 48;
         }
     } else {
-        entryPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryPointerByMode(primaryIndex, MODE_PRIMARY);
-        entryAuxPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryAuxPointerByMode(primaryIndex, MODE_PRIMARY);
+        entryPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryPointerByMode(primaryIndex, 1);
+        entryAuxPtr = NEWGRID2_JMPTBL_ESQDISP_GetEntryAuxPointerByMode(primaryIndex, 1);
     }
 
     state = NEWGRID_GetEntryStateCode(entryPtr, entryAuxPtr, selector);
 
-    if (mode == TEST_MODE_UNSET) {
-        if (state == STATE_UNSET) {
-            result = RESULT_MATCH;
-        } else {
-            result = RESULT_FALSE;
+    if (mode == 0) {
+        if (state == 0) {
+            result = -1;
         }
-    } else if (mode == TEST_MODE_PRESENT) {
-        if (state == STATE_PRESENT || state == STATE_BLOCKED) {
-            result = RESULT_TRUE;
-        } else {
-            result = RESULT_FALSE;
+    } else if (mode == 1) {
+        if (state == 1 || state == 3) {
+            result = 1;
         }
-    } else if (mode == TEST_MODE_BLOCKED_A || mode == TEST_MODE_BLOCKED_B) {
-        if (state == STATE_BLOCKED) {
-            result = RESULT_MATCH;
-        } else {
-            result = RESULT_FALSE;
+    } else if (mode == 2 || mode == 3) {
+        if (state == 3) {
+            result = -1;
         }
     }
 
