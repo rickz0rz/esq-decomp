cfd3e31ecadbb122d9b4f72d8ae57625822cc736
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-12 18:11:44 -0400

    Decompilation checkpoint: 1773353503

--- src/decomp/sas_c/esqfunc_update_disk_warning_and_refresh_tick.c
+++ src/decomp/sas_c/esqfunc_update_disk_warning_and_refresh_tick.c
@@ -15,16 +15,16 @@ extern ESQFUNC_RastPortHolder Global_REF_RASTPORT_2;
 extern const char Global_STR_DISK_0_IS_WRITE_PROTECTED[];
 extern const char Global_STR_YOU_MUST_REINSERT_SYSTEM_DISK_INTO_DRIVE_0[];
 
-extern void ESQFUNC_JMPTBL_DISKIO_ProbeDrivesAndAssignPaths(void);
-extern void ESQFUNC_JMPTBL_TLIBA3_DrawCenteredWrappedTextLines(char *rastPort, const char *text, LONG y);
+extern void DISKIO_ProbeDrivesAndAssignPaths(void);
+extern void TLIBA3_DrawCenteredWrappedTextLines(char *rastPort, const char *text, LONG y);
 
 void ESQFUNC_UpdateDiskWarningAndRefreshTick(void)
 {
-    ESQFUNC_JMPTBL_DISKIO_ProbeDrivesAndAssignPaths();
+    DISKIO_ProbeDrivesAndAssignPaths();
 
     if (DISKIO_Drive0WriteProtectedCode != 0) {
         Global_RefreshTickCounter = -1;
-        ESQFUNC_JMPTBL_TLIBA3_DrawCenteredWrappedTextLines(
+        TLIBA3_DrawCenteredWrappedTextLines(
             (char *)Global_REF_RASTPORT_2.rastPort,
             Global_STR_YOU_MUST_REINSERT_SYSTEM_DISK_INTO_DRIVE_0,
             90);
@@ -33,7 +33,7 @@ void ESQFUNC_UpdateDiskWarningAndRefreshTick(void)
 
     if (DISKIO_DriveMediaStatusCodeTable != 0) {
         Global_RefreshTickCounter = -1;
-        ESQFUNC_JMPTBL_TLIBA3_DrawCenteredWrappedTextLines(
+        TLIBA3_DrawCenteredWrappedTextLines(
             (char *)Global_REF_RASTPORT_2.rastPort,
             Global_STR_DISK_0_IS_WRITE_PROTECTED,
             90);
