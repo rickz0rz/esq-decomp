c3ca322e9888cf5995a2f5d7b64f988ca8ec9be6
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-12 15:16:11 -0400

    Decompilation checkpoint: 1773342971

--- src/decomp/sas_c/cleanup_shutdown_input_devices.c
+++ src/decomp/sas_c/cleanup_shutdown_input_devices.c
@@ -1,43 +1,46 @@
 typedef long LONG;
+typedef unsigned short UWORD;
 
-enum {
-    IOSTDREQ_COMMAND_OFFSET = 28,
-    IOSTDREQ_BUFFERPTR_OFFSET = 40
-};
+typedef struct IOStdReqApprox {
+    char pad0[28];
+    UWORD io_Command;
+    char pad30[10];
+    void *io_Data;
+} IOSTDREQ;
 
-extern LONG Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE;
-extern LONG Global_REF_DATA_INPUT_BUFFER;
-extern LONG Global_REF_IOSTDREQ_STRUCT_CONSOLE_DEVICE;
-extern LONG Global_REF_INPUTDEVICE_MSGPORT;
-extern LONG Global_REF_CONSOLEDEVICE_MSGPORT;
+extern void *AbsExecBase;
+extern IOSTDREQ *Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE;
+extern void *Global_REF_DATA_INPUT_BUFFER;
+extern IOSTDREQ *Global_REF_IOSTDREQ_STRUCT_CONSOLE_DEVICE;
+extern void *Global_REF_INPUTDEVICE_MSGPORT;
+extern void *Global_REF_CONSOLEDEVICE_MSGPORT;
 extern const char Global_STR_CLEANUP_C_5[];
 
-void _LVODoIO(void);
+LONG _LVODoIO(void *execBase, void *ioReq);
 void GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(const char *file, LONG line, void *ptr, LONG size);
-void _LVOCloseDevice(void);
+LONG _LVOCloseDevice(void *execBase, void *ioReq);
 void GROUP_AG_JMPTBL_IOSTDREQ_CleanupSignalAndMsgport(void *port);
 void GROUP_AB_JMPTBL_IOSTDREQ_Free(void *req);
 
 void CLEANUP_ShutdownInputDevices(void)
 {
-    *(unsigned short *)(Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE + IOSTDREQ_COMMAND_OFFSET) = 10;
-    *(LONG *)(Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE + IOSTDREQ_BUFFERPTR_OFFSET) = Global_REF_DATA_INPUT_BUFFER;
-
-    _LVODoIO();
+    Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE->io_Command = 10;
+    Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE->io_Data = Global_REF_DATA_INPUT_BUFFER;
+    _LVODoIO(AbsExecBase, Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE);
 
     GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(
         Global_STR_CLEANUP_C_5,
         127,
-        (void *)Global_REF_DATA_INPUT_BUFFER,
+        Global_REF_DATA_INPUT_BUFFER,
         20
     );
 
-    _LVOCloseDevice();
-    _LVOCloseDevice();
+    _LVOCloseDevice(AbsExecBase, Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE);
+    _LVOCloseDevice(AbsExecBase, Global_REF_IOSTDREQ_STRUCT_CONSOLE_DEVICE);
 
-    GROUP_AG_JMPTBL_IOSTDREQ_CleanupSignalAndMsgport((void *)Global_REF_INPUTDEVICE_MSGPORT);
-    GROUP_AG_JMPTBL_IOSTDREQ_CleanupSignalAndMsgport((void *)Global_REF_CONSOLEDEVICE_MSGPORT);
+    GROUP_AG_JMPTBL_IOSTDREQ_CleanupSignalAndMsgport(Global_REF_INPUTDEVICE_MSGPORT);
+    GROUP_AG_JMPTBL_IOSTDREQ_CleanupSignalAndMsgport(Global_REF_CONSOLEDEVICE_MSGPORT);
 
-    GROUP_AB_JMPTBL_IOSTDREQ_Free((void *)Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE);
-    GROUP_AB_JMPTBL_IOSTDREQ_Free((void *)Global_REF_IOSTDREQ_STRUCT_CONSOLE_DEVICE);
+    GROUP_AB_JMPTBL_IOSTDREQ_Free(Global_REF_IOSTDREQ_STRUCT_INPUT_DEVICE);
+    GROUP_AB_JMPTBL_IOSTDREQ_Free(Global_REF_IOSTDREQ_STRUCT_CONSOLE_DEVICE);
 }
