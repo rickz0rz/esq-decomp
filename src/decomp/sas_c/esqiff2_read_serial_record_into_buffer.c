bbb0ad334f1d23dc38ad301ff4b0259f251523d8
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-12 19:54:25 -0400

    Decompilation checkpoint: 1773359665

--- src/decomp/sas_c/esqiff2_read_serial_record_into_buffer.c
+++ src/decomp/sas_c/esqiff2_read_serial_record_into_buffer.c
@@ -1,4 +1,5 @@
 typedef unsigned char UBYTE;
+typedef unsigned short UWORD;
 typedef signed short WORD;
 typedef signed long LONG;
 
@@ -9,59 +10,50 @@ extern UBYTE ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte(void);
 
 LONG ESQIFF2_ReadSerialRecordIntoBuffer(UBYTE *dst, WORD record_mode, WORD extension_count)
 {
-    const UWORD RECORD_MAX = 0x2328;
-    const UBYTE CH_NUL = 0;
-    const UBYTE TOKEN_EXT_RECORD = 20;
-    const UBYTE TOKEN_COPY_NEXT = 18;
-    const WORD MODE_PLAIN = 0;
-    const WORD MODE_EXTENDED = 1;
-    const UWORD MIN_BREAK_INDEX = 1;
-    const UWORD COPY_NEXT_LIMIT = 0x12e;
-    const LONG RESULT_FAIL = 0;
     WORD d4 = 0;
     WORD local_0x12_counter = 0;
 
     for (;;) {
-        if ((unsigned short)d4 >= RECORD_MAX) {
+        if ((UWORD)d4 >= (UWORD)0x2328) {
             break;
         }
 
         ESQFUNC_WaitForClockChangeAndServiceUi();
-        dst[(unsigned short)d4] = ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte();
+        dst[(UWORD)d4] = ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte();
 
-        if (dst[(unsigned short)d4] == CH_NUL) {
-            if (record_mode == MODE_PLAIN) {
+        if (dst[(UWORD)d4] == 0) {
+            if (record_mode == 0) {
                 break;
             }
-            if ((unsigned short)d4 > MIN_BREAK_INDEX) {
+            if ((UWORD)d4 > (UWORD)1) {
                 break;
             }
         }
 
-        if (dst[(unsigned short)d4] == TOKEN_EXT_RECORD && record_mode == MODE_EXTENDED) {
+        if (dst[(UWORD)d4] == 20 && record_mode == 1) {
             WORD d5 = 0;
             d4 = (WORD)(d4 + 1);
-            while ((unsigned short)d5 < (unsigned short)extension_count) {
+            while ((UWORD)d5 < (UWORD)extension_count) {
                 WORD index;
                 ESQFUNC_WaitForClockChangeAndServiceUi();
                 index = d4;
                 d4 = (WORD)(d4 + 1);
-                dst[(unsigned short)index] = ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte();
+                dst[(UWORD)index] = ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte();
                 d5 = (WORD)(d5 + 1);
             }
             continue;
         }
 
-        if (dst[(unsigned short)d4] == TOKEN_COPY_NEXT && record_mode == MODE_EXTENDED) {
+        if (dst[(UWORD)d4] == 18 && record_mode == 1) {
             WORD index;
             d4 = (WORD)(d4 + 1);
             ESQFUNC_WaitForClockChangeAndServiceUi();
             index = d4;
             d4 = (WORD)(d4 + 1);
-            dst[(unsigned short)index] = ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte();
+            dst[(UWORD)index] = ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte();
             local_0x12_counter = (WORD)(local_0x12_counter + 1);
-            if ((unsigned short)local_0x12_counter >= COPY_NEXT_LIMIT) {
-                return RESULT_FAIL;
+            if ((UWORD)local_0x12_counter >= (UWORD)0x12e) {
+                return 0;
             }
             continue;
         }
@@ -71,5 +63,5 @@ LONG ESQIFF2_ReadSerialRecordIntoBuffer(UBYTE *dst, WORD record_mode, WORD exten
 
     ESQFUNC_WaitForClockChangeAndServiceUi();
     ESQIFF_RecordChecksumByte = ESQPARS_JMPTBL_SCRIPT_ReadSerialRbfByte();
-    return (LONG)(unsigned short)d4;
+    return (LONG)(UWORD)d4;
 }
