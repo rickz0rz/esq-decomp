cf361cc36f5084140ebfec2cd823b09ccfa85cbc
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-12 10:38:02 -0400

    Decompilation checkpoint: 1773326282

--- src/decomp/sas_c/unknown21_dos_open_new_file_if_missing.c
+++ src/decomp/sas_c/unknown21_dos_open_new_file_if_missing.c
@@ -1,43 +1,39 @@
 typedef signed long LONG;
 
-enum {
-    ACCESS_READ = -2,
-    MODE_NEWFILE = 1006
-};
-
 extern LONG Global_SignalCallbackPtr;
 extern LONG Global_DosIoErr;
 extern LONG Global_AppErrorCode;
 extern void *Global_DosLibrary;
 
 extern void SIGNAL_PollAndDispatch(void);
-extern LONG _LVOLock(void *dosBase, char *name, LONG mode);
+extern LONG _LVOLock(void *dosBase, const char *name, LONG mode);
 extern void _LVOUnLock(void *dosBase, LONG lock);
-extern LONG _LVOOpen(void *dosBase, char *name, LONG mode);
+extern LONG _LVOOpen(void *dosBase, const char *name, LONG mode);
 extern LONG _LVOIoErr(void *dosBase);
 
-LONG DOS_OpenNewFileIfMissing(char *filename)
+LONG DOS_OpenNewFileIfMissing(const char *filename)
 {
-    LONG lock;
-    LONG handle;
+    LONG lockHandle;
+    LONG fileHandle;
 
     if (Global_SignalCallbackPtr != 0) {
         SIGNAL_PollAndDispatch();
     }
 
     Global_DosIoErr = 0;
-    lock = _LVOLock(Global_DosLibrary, filename, ACCESS_READ);
-    if (lock != 0) {
-        _LVOUnLock(Global_DosLibrary, lock);
+
+    lockHandle = _LVOLock(Global_DosLibrary, filename, -2);
+    if (lockHandle != 0) {
+        _LVOUnLock(Global_DosLibrary, lockHandle);
         return -1;
     }
 
-    handle = _LVOOpen(Global_DosLibrary, filename, MODE_NEWFILE);
-    if (handle == 0) {
+    fileHandle = _LVOOpen(Global_DosLibrary, filename, 1006);
+    if (fileHandle == 0) {
         Global_DosIoErr = _LVOIoErr(Global_DosLibrary);
         Global_AppErrorCode = 2;
         return -1;
     }
 
-    return handle;
+    return fileHandle;
 }
