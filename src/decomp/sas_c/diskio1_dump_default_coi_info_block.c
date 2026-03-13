4dcf093606e89ce4385da4f4323191e4555438bb
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-13 11:02:50 -0400

    Decompilation checkpoint: 1773414170

--- src/decomp/sas_c/diskio1_dump_default_coi_info_block.c
+++ src/decomp/sas_c/diskio1_dump_default_coi_info_block.c
@@ -32,7 +32,7 @@ struct DiskioProgramSourceRecord {
     struct DiskioDefaultCoiInfo *defaultCoi;
 };
 
-extern void GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(const char *fmt, ...);
+extern LONG FORMAT_RawDoFmtWithScratchBuffer(char *fmt, ...);
 
 extern const char DISKIO_FMT_FLAG1_0X_PCT_02X_FLAG2_0X_PCT_04X_BG_DefaultCoiDump[];
 extern const char DISKIO_FMT_COI_DASH_PTR_PCT_08LX[];
@@ -53,14 +53,14 @@ void DISKIO1_DumpDefaultCoiInfoBlock(void)
     const struct DiskioProgramSourceRecord *rec = gDiskio1CurrentSourceRecord;
     const struct DiskioDefaultCoiInfo *coi = rec->defaultCoi;
 
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_FLAG1_0X_PCT_02X_FLAG2_0X_PCT_04X_BG_DefaultCoiDump,
         (ULONG)rec->sourceFlagsByte,
         (ULONG)rec->sourceFlagsWord,
         (ULONG)rec->backgroundColor0,
         (ULONG)rec->backgroundColor1,
         rec->backgroundText);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_COI_DASH_PTR_PCT_08LX,
         coi);
 
@@ -68,35 +68,35 @@ void DISKIO1_DumpDefaultCoiInfoBlock(void)
         return;
     }
 
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_STR_DEF_COI_INFORMATION_FOLLOWS_COLON);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_STR_DEF_DEFAULT,
         coi);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_DEF_CITY_PCT_08LX_STAR_DEF_CITY,
         coi->city,
         coi->city);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_DEF_ORDER_PCT_08LX_STAR_DEF_ORDER,
         coi->order,
         coi->order);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_DEF_PRICE_PCT_08LX_STAR_DEF_PRICE,
         coi->price,
         coi->price);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_DEF_TELE_PCT_08LX_STAR_DEF_TELE,
         coi->tele,
         coi->tele);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_DEF_EVENT_PCT_08LX_STAR_DEF_EVENT,
         coi->event,
         coi->event);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_EXCEPTION_COUNT_IS_PCT_LD,
         (long)coi->exceptionCount);
-    GROUP_AJ_JMPTBL_FORMAT_RawDoFmtWithScratchBuffer(
+    FORMAT_RawDoFmtWithScratchBuffer(
         DISKIO_FMT_EXCEPTION_BLOCK_PCT_08LX,
         coi->exceptionBlock);
 }
