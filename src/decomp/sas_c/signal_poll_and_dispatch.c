ac4be730c52b3b98a74f57ace405a2d105959a1f
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-13 07:41:41 -0400

    Decompilation checkpoint: 1773402101

--- src/decomp/sas_c/signal_poll_and_dispatch.c
+++ src/decomp/sas_c/signal_poll_and_dispatch.c
@@ -12,18 +12,14 @@ LONG SIGNAL_PollAndDispatch(void)
     LONG pending;
 
     pending = _LVOSetSignal(AbsExecBase, 0, 0x3000);
-    if ((pending & 0x3000) == 0) {
-        return pending;
+    if ((pending & 0x3000) != 0) {
+        if (Global_SignalCallbackPtr != 0) {
+            if (((SignalCallback)Global_SignalCallbackPtr)() != 0) {
+                Global_SignalCallbackPtr = 0;
+                return HANDLE_CloseAllAndReturnWithCode(20);
+            }
+        }
     }
 
-    if (Global_SignalCallbackPtr == 0) {
-        return pending;
-    }
-
-    if (((SignalCallback)Global_SignalCallbackPtr)() == 0) {
-        return 0;
-    }
-
-    Global_SignalCallbackPtr = 0;
-    return HANDLE_CloseAllAndReturnWithCode(20);
+    return pending;
 }
