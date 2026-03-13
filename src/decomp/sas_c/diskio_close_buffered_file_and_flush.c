c7733bec04b65604ed49a96257ffd3d8339f0071
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-13 09:35:20 -0400

    Decompilation checkpoint: 1773408920

--- src/decomp/sas_c/diskio_close_buffered_file_and_flush.c
+++ src/decomp/sas_c/diskio_close_buffered_file_and_flush.c
@@ -23,17 +23,17 @@ extern WORD CTASKS_CloseTaskCompletionFlag;
 extern WORD Global_UIBusyFlag;
 extern WORD ESQPARS2_ReadModeFlags;
 
-extern void GROUP_AG_JMPTBL_ESQFUNC_ServiceUiTickIfRunning(void);
+extern void ESQFUNC_ServiceUiTickIfRunning(void);
 extern LONG _LVOWrite(void *dosBase, LONG fileHandle, const void *buffer, LONG length);
 extern LONG _LVODelay(void *dosBase, LONG ticks);
 extern void CTASKS_StartCloseTaskProcess(LONG fileHandle);
-extern void GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(const char *file, LONG line, void *ptr, ULONG size);
+extern void MEMORY_DeallocateMemory(void *ptr, ULONG size);
 
 LONG DISKIO_CloseBufferedFileAndFlush(LONG fileHandle)
 {
     LONG openCountBeforeClose;
 
-    GROUP_AG_JMPTBL_ESQFUNC_ServiceUiTickIfRunning();
+    ESQFUNC_ServiceUiTickIfRunning();
 
     if (fileHandle != 0) {
         LONG bytesPending;
@@ -48,21 +48,19 @@ LONG DISKIO_CloseBufferedFileAndFlush(LONG fileHandle)
                 bytesPending);
         }
 
-        GROUP_AG_JMPTBL_ESQFUNC_ServiceUiTickIfRunning();
+        ESQFUNC_ServiceUiTickIfRunning();
         CTASKS_StartCloseTaskProcess(fileHandle);
 
         while (CTASKS_CloseTaskCompletionFlag == 0) {
-            GROUP_AG_JMPTBL_ESQFUNC_ServiceUiTickIfRunning();
+            ESQFUNC_ServiceUiTickIfRunning();
             _LVODelay(Global_REF_DOS_LIBRARY_2, 5);
-            GROUP_AG_JMPTBL_ESQFUNC_ServiceUiTickIfRunning();
+            ESQFUNC_ServiceUiTickIfRunning();
         }
 
-        GROUP_AG_JMPTBL_MEMORY_DeallocateMemory(
-            Global_STR_DISKIO_C_2,
-            353,
+        MEMORY_DeallocateMemory(
             DISKIO_BufferControl.BufferBase,
             (ULONG)DISKIO_BufferState.BufferSize);
-        GROUP_AG_JMPTBL_ESQFUNC_ServiceUiTickIfRunning();
+        ESQFUNC_ServiceUiTickIfRunning();
     }
 
     openCountBeforeClose = DISKIO_OpenCount;
--- src/decomp/sas_c/flib_append_clock_stamped_log_entry.c
+++ src/decomp/sas_c/flib_append_clock_stamped_log_entry.c
@@ -21,11 +21,11 @@ extern char *NEWGRID2_ErrorLogEntryPtr;
 extern const char Global_STR_FLIB_C_1[];
 extern const char Global_STR_FLIB_C_2[];
 
-extern LONG NEWGRID_JMPTBL_MATH_DivS32(LONG value, LONG divisor);
-extern LONG GROUP_AW_JMPTBL_WDISP_SPrintf(char *dst, const char *fmt, ...);
-extern char *GROUP_AR_JMPTBL_STRING_AppendAtNull(char *dst, const char *src);
-extern void *NEWGRID_JMPTBL_MEMORY_AllocateMemory(const char *tag, LONG line, ULONG flags, ULONG size);
-extern void NEWGRID_JMPTBL_MEMORY_DeallocateMemory(const char *tag, LONG line, void *ptr, ULONG size);
+extern LONG MATH_DivS32(LONG value, LONG divisor);
+extern LONG WDISP_SPrintf(char *dst, const char *fmt, ...);
+extern char *STRING_AppendAtNull(char *dst, const char *src);
+extern void *MEMORY_AllocateMemory(ULONG size, ULONG flags);
+extern void MEMORY_DeallocateMemory(void *ptr, ULONG size);
 extern char *ESQPARS_ReplaceOwnedString(const char *new_src, char *old_owned);
 
 LONG FLIB_AppendClockStampedLogEntry(char *entry_text)
@@ -59,25 +59,23 @@ LONG FLIB_AppendClockStampedLogEntry(char *entry_text)
         entry_text[99] = '\0';
     }
 
-    hour2 = NEWGRID_JMPTBL_MATH_DivS32((LONG)CLOCK_CacheHour, 100);
-    min2 = NEWGRID_JMPTBL_MATH_DivS32((LONG)CLOCK_CacheMinuteOrSecond, 100);
-    date2 = NEWGRID_JMPTBL_MATH_DivS32((LONG)Global_REF_CLOCKDATA_STRUCT, 100);
+    hour2 = MATH_DivS32((LONG)CLOCK_CacheHour, 100);
+    min2 = MATH_DivS32((LONG)CLOCK_CacheMinuteOrSecond, 100);
+    date2 = MATH_DivS32((LONG)Global_REF_CLOCKDATA_STRUCT, 100);
     ampm_tag = (CLOCK_CacheAmPmFlag != 0) ? ESQPARS2_LogTagPm : ESQPARS2_LogTagAm;
 
-    GROUP_AW_JMPTBL_WDISP_SPrintf(scratch, ESQPARS2_LogTimestampFmt, hour2, min2, date2, ampm_tag);
+    WDISP_SPrintf(scratch, ESQPARS2_LogTimestampFmt, hour2, min2, date2, ampm_tag);
     msg_len += 14;
 
-    GROUP_AR_JMPTBL_STRING_AppendAtNull(scratch, ESQPARS2_LogFieldTab);
-    GROUP_AR_JMPTBL_STRING_AppendAtNull(scratch, entry_text);
-    GROUP_AR_JMPTBL_STRING_AppendAtNull(scratch, ESQPARS2_LogLineTerminator);
+    STRING_AppendAtNull(scratch, ESQPARS2_LogFieldTab);
+    STRING_AppendAtNull(scratch, entry_text);
+    STRING_AppendAtNull(scratch, ESQPARS2_LogLineTerminator);
 
     prior_total = FLIB_LogEntryByteCount;
     new_total = (UWORD)(prior_total + (UWORD)msg_len);
     FLIB_LogEntryByteCount = new_total;
 
-    tmp = (char *)NEWGRID_JMPTBL_MEMORY_AllocateMemory(
-        Global_STR_FLIB_C_1, 173, MEMF_PUBLIC, (ULONG)new_total + 1
-    );
+    tmp = (char *)MEMORY_AllocateMemory((ULONG)new_total + 1, MEMF_PUBLIC);
 
     if (prior_total != (UWORD)msg_len) {
         char *src = NEWGRID2_ErrorLogEntryPtr;
@@ -91,13 +89,11 @@ LONG FLIB_AppendClockStampedLogEntry(char *entry_text)
         tmp[0] = '\0';
     }
 
-    GROUP_AR_JMPTBL_STRING_AppendAtNull(tmp, scratch);
+    STRING_AppendAtNull(tmp, scratch);
 
     NEWGRID2_ErrorLogEntryPtr = ESQPARS_ReplaceOwnedString(tmp, NEWGRID2_ErrorLogEntryPtr);
 
-    NEWGRID_JMPTBL_MEMORY_DeallocateMemory(
-        Global_STR_FLIB_C_2, 198, tmp, (ULONG)FLIB_LogEntryByteCount + 1
-    );
+    MEMORY_DeallocateMemory(tmp, (ULONG)FLIB_LogEntryByteCount + 1);
 
     ESQPARS2_LogAppendSpinlock = 0;
     return 0;
