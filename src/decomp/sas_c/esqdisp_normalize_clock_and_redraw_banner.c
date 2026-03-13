c2ac377ff8520ee7df486ad27e4d602b61ae55ad
Ricky Leverence Jr. <ricky.leverence@gmail.com>
2026-03-13 01:08:17 -0400

    Decompilation checkpoint: 1773378497

--- src/decomp/sas_c/esqdisp_normalize_clock_and_redraw_banner.c
+++ src/decomp/sas_c/esqdisp_normalize_clock_and_redraw_banner.c
@@ -13,7 +13,7 @@ extern UBYTE Global_REF_696_400_BITMAP;
 extern void PARSEINI_NormalizeClockData(UBYTE *slot_index, void *clock_data);
 extern long DST_UpdateBannerQueue(void *banner_window);
 extern void DST_RefreshBannerBuffer(void);
-extern void ESQFUNC_JMPTBL_CLEANUP_DrawClockBanner(void);
+extern void CLEANUP_DrawClockBanner(void);
 extern void ESQDISP_DrawStatusBanner_Impl(short highlight_flag);
 
 void ESQDISP_NormalizeClockAndRedrawBanner(void *clock_data)
@@ -27,7 +27,7 @@ void ESQDISP_NormalizeClockAndRedrawBanner(void *clock_data)
 
     saved_bitmap = Global_REF_RASTPORT_1->bitmap;
     Global_REF_RASTPORT_1->bitmap = &Global_REF_696_400_BITMAP;
-    ESQFUNC_JMPTBL_CLEANUP_DrawClockBanner();
+    CLEANUP_DrawClockBanner();
     Global_REF_RASTPORT_1->bitmap = saved_bitmap;
     ESQDISP_DrawStatusBanner_Impl(1);
 }
